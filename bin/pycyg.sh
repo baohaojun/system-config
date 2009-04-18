82daa76ea4f3d4e0c84b28b8223d27df1adb5314
Bao Haojun <baohaojun@gmail.com>
2009-04-18 10:40:39 +0000

    no commit message provided

--- bin/pycyg.sh
+++ bin/pycyg.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+
+debug=false
+if [[ "$1" == -x ]]
+then
+    debug=true
+    shift
+fi
 pyfile="$1"
+ if ! [[ ${pyfile:0:1} == / ]];
+ then
+     pyfile="`which \"$pyfile\"`"
+ fi
 shift
 pyfile="`cygpath -alw \"$pyfile\"`"
 /c/python25/python "$pyfile" "$@"
+if [[ $debug == true ]]
+then
+    echo -n Press any key to exit ...
+    read
+fi
--- bin/regrep.py
+++ bin/regrep.py
@@ -1,3 +1,5 @@
+#!/q/bin/pycyg.exe -x
+# -*- coding: gbk -*-
 from win32api import *
 from win32con import *
 
@@ -9,83 +11,87 @@ rootKeysMap = {HKEY_CLASSES_ROOT: "HKEY_CLASSES_ROOT",
                HKEY_USERS: "HKEY_USERS",
                }
 
+if len(sys.argv) != 2:
+    print 'Usage: regsub.py pattern'
+    sys.exit(-1)
 
 reobj = re.compile(sys.argv[1], re.I)
-print 'Windows Registry Editor Version 5.00'
 
-lastPrintKeyName = ""
-currentKeyName = ""
 
-fileIndex = 0
-File = None
-def printValue(valueName, value):
-    global lastPrintKeyName
-    global currentKeyName
-    global fileIndex
-    global File
-    if lastPrintKeyName != currentKeyName:
-        if File:
-            File.close()
-        File = open(str(fileIndex)+".reg", "w")
-        fileIndex = fileIndex+1
-        lastPrintKeyName = currentKeyName
-        File.write('Windows Registry Editor Version 5.00\n')
-        File.write("\n")
-        File.write('[%s]\n' % currentKeyName)
-
-    valueName = r'\\'.join(valueName.split('\\')) #replace '\\' with r'\\'
-    valueName = r'\"'.join(valueName.split('"')) #replace '"' with r'\"'
-
-    value = r'\\'.join(value.split('\\'))
-    value = r'\"'.join(value.split('"'))
-
-    if valueName:
-        File.write('"%s"="%s"\n' % (valueName, value))
-    else:
-        File.write('@="%s"\n' % value)
+def traverseKey(rootKey, rootKeyName):
         
-    File.flush()
-                           
-def traverseKey(key, keyName):
-    global currentKeyName
-    currentKeyName = keyName
-    if reobj.search(keyName):
-        print
-        print '[%s]' % keyName
-       
-        
+    #replace the values of the rootKey first
+
+    ## get all the values
+    allValues = []
     try:
         index = 0
         while True: 
-            (valueName, value, type_) = RegEnumValue(key, index)
+            (valueName, value, type_) = RegEnumValue(rootKey, index)
             index = index+1
-            if type_ == 1: # 1 is REG_SZ, we only want to handle this type
-                if reobj.search(valueName):
-                    printValue(valueName, value)
-                    continue #no need to print it twice if the value also matches
-                if reobj.search(value):
-                    printValue(valueName, value)
-
-    except:
+            allValues.append((valueName, value, type_))
+    except: #this will always occur when there are no more values in the key
         sys.stdout.flush()
 
+    ## handle the values according to their value / valueName / type
+    
+    for (valueName, value, type_) in allValues:
+        ### if value contains a '?', then probably not a Unicode char not encodable by our coding (gbk)
+        if type_ in (REG_SZ, REG_EXPAND_SZ) and '?' in value:
+            sys.stderr.write('key %s\nname %s\ntype %s\nvalue %s\n\n' % (rootKeyName, valueName, `type_`, value))
+            continue
+
+        if type_ == REG_MULTI_SZ and any([('?' in value) for x in value]):
+            sys.stderr.write('key %s\nname %s\ntype %s\nvalue %s\n\n' % (rootKeyName, valueName, `type_`, `value`))
+            continue
+        
+        needReplaceName = False 
+        needReplaceValue = False
+
+        ### if we need sub the name?
+        if reobj.search(valueName):
+            needReplaceName = True
+
+        ### do we need sub the value string?
+        if type_ in (REG_SZ, REG_EXPAND_SZ) and reobj.search(value):
+            needReplaceValue = True
+            
+        ### do we need sub the strings in value?
+        if type_ == REG_MULTI_SZ and any([reobj.search(x) for x in value]):
+            needReplaceValue = True 
+
+        ### load the new value
+        if needReplaceValue or needReplaceName:
+                sys.stdout.write('matched [%s]: "%s"="%s"\n' % (rootKeyName, valueName, value))
+
+    #now do the tree walking
     try:
-        leafKeys = RegEnumKeyEx(key)
+        subKeys = RegEnumKeyEx(rootKey) #get all the keys
     except:
-        sys.stderr.write("Exception: RegEnumKeyEx %s\n" % keyName)
+        sys.stderr.write("Exception: RegEnumKeyEx %s\n" % rootKeyName)
     else:
-        for leaf in leafKeys:
+        for subK in subKeys:
             try:
-                leafKey = RegOpenKeyEx(key, leaf[0])
-                traverseKey(leafKey, "%s\\%s" % (keyName, leaf[0]))
+                leafKey = RegOpenKeyEx(rootKey, subK[0], 0, KEY_ALL_ACCESS)
             except:
-                sys.stderr.write("Exception: RegOpenKeyEx %s\n" % (keyName+'\\'+leaf[0]))
+                sys.stderr.write("Exception: RegOpenKeyEx %s\n" % (rootKeyName+'\\'+subK[0]))
+            else:
+                traverseKey(leafKey, "%s\\%s" % (rootKeyName, subK[0]))
+
         
 
                 
         
-for x in rootKeysMap:
-    traverseKey(x, rootKeysMap[x])
+for rootKey in rootKeysMap:
+    #rootKey need to be opened with all access
+    rootKeyName = rootKeysMap[rootKey]
+    try:
+        rootKey = RegOpenKeyEx(rootKey, None, 0, KEY_ALL_ACCESS)
+    except:
+        sys.stderr.write('Error openning %s\n' % rootKeysMap[rootKey])
+        continue
+    else:
+        traverseKey(rootKey, rootKeyName)
 
 sys.stderr.write('\n')
 sys.stderr.write('Every thing has been successively dumped!\n')
