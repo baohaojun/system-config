2b89325a67e5c82323cb1753c4c706bf98492b62
Bao Haojun <baohaojun@gmail.com>
2009-05-03 10:46:26 +0000

    no commit message provided

---
+++ bin/mktags.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PATH=/d/tools/emacswin/bin/:/bin:/usr/bin:"$PATH"
+
+find . \( '-path' '*/CVS' \
+    '-o' '-path' '*/.svn' \
+    '-o' '-path' '*/autom4te.cache' \
+    '-o' '-path' '*/{arch}' \
+    '-o' '-path' '*/.hg' \
+    '-o' '-path' '*/_darcs' \
+    '-o' '-path' '*/.git' \
+    '-o' '-path' '*/.bzr' \
+    '-o' '-path' '*~*' \
+    '-o' '-path' '*#' \
+    '-o' '-path' '*/TAGS' \
+    '-o' '-path' '*/semantic.cache' \
+    '-o' '-iname' '*.o' \
+    '-o' '-iname' '*.class' \
+    '-o' '-iname' '*.obj' \
+    '-o' '-iname' '*.pyc' \
+    '-o' '-path' '*/.ignore' \) -prune \
+    -o -type f \( -iname '*.cpp' \
+    -o -iname '*.hpp' \
+    -o -iname '*.s' \
+    -o -iname '*.h' \
+    -o -iname '*.c' \
+    -o -iname '*.py' \
+    -o -iname '*.el' \
+    -o -iname '*.pl' \
+    \) -print |etags -
--- bin/windows/mktags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=/d/tools/emacswin/bin/:/bin:/usr/bin:"$PATH"
-
-if [[ -z $1 ]]; 
-    then (find . -type f -iname "*.[hc]pp"; find . -type f -iname "*.[sch]")|etags -;
-    else (find . -type f -iname "$1")|etags -;
-fi
