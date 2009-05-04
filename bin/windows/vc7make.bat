c40befe3fbcd62f7d36c8c0a0d718804cce06486
Bao Haojun <baohaojun@gmail.com>
2009-05-04 05:02:36 +0000

    no commit message provided

---
+++ bin/windows/vc7make.bat
@@ -0,0 +1,2 @@
+call "c:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\bin\vcvars32.bat"
+bash --rcfile /dev/null
--- bin/windows/w3m.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for x in "$@"; do echo -n \""$x"\"\ ; done >>~/w3m.log
-
-/usr/bin/w3m "$@"
