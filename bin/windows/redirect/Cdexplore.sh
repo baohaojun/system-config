549d02c49a9486939ca01a6460d3cb1fd20f32fc
Bao Haojun <baohaojun@gmail.com>
2009-05-19 09:40:57 +0000

    no commit message provided

--- bin/windows/redirect/Cdexplore.sh
+++ bin/windows/redirect/Cdexplore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
 cp /c/windows/hh 'q:\bin\windows\redirect\dexplore.exe'
-/c/windows/apppatch/apploc 'q:\bin\windows\redirect\dexplore.exe' "$@" /L0409
+/c/windows/apppatch/apploc 'q:\bin\windows\redirect\dexplore.exe' "`cygpath -alw \"$1\"`" /L0409
+
