8664adc6d85ff9fa8d457e2802875873ea39af19
Bao Haojun <baohaojun@gmail.com>
2009-05-23 13:27:29 +0000

    no commit message provided

---
+++ gcode/open-context/aboutdlg.h
@@ -0,0 +1,39 @@
+/**************************************************************************
+   THIS CODE AND INFORMATION IS PROVIDED 'AS IS' WITHOUT WARRANTY OF
+   ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+   PARTICULAR PURPOSE.
+   Author: Leon Finker  01/2001
+**************************************************************************/
+// aboutdlg.h : interface of the CAboutDlg class
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __ABOUTDLG_H__
+#define __ABOUTDLG_H__
+
+#include "resource.h"
+class CAboutDlg : public CDialogImpl<CAboutDlg>
+{
+public:
+	enum { IDD = IDD_ABOUTBOX };
+
+	BEGIN_MSG_MAP(CAboutDlg)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		COMMAND_ID_HANDLER(IDOK, OnCloseCmd)
+		COMMAND_ID_HANDLER(IDCANCEL, OnCloseCmd)
+	END_MSG_MAP()
+
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		CenterWindow(GetParent());
+		return TRUE;
+	}
+
+	LRESULT OnCloseCmd(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		EndDialog(wID);
+		return 0;
+	}
+};
+
+#endif //__ABOUTDLG_H__
---
+++ gcode/open-context/stdafx.h
@@ -0,0 +1,41 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__B5466F69_68C9_43BA_BAA4_0DC717E51478__INCLUDED_)
+#define AFX_STDAFX_H__B5466F69_68C9_43BA_BAA4_0DC717E51478__INCLUDED_
+
+// Change these values to use different versions
+#define WINVER		0x0400
+#define _WIN32_IE	0x0500
+#define _RICHEDIT_VER	0x0100
+
+
+#include <atlbase.h>
+#include <atlapp.h>
+
+extern CAppModule _Module;
+
+#include <atlcom.h>
+#include <atlhost.h>
+#include <atlwin.h>
+#include <atlctl.h>
+#include <atlsplit.h>
+#include <atlframe.h>
+#include <atlctrls.h>
+#include <atldlgs.h>
+#include "aboutdlg.h"
+#include <shlobj.h>
+#include "dragdropimpl.h"
+
+inline LPITEMIDLIST Pidl_GetNextItem(LPCITEMIDLIST);
+UINT Pidl_GetSize(LPCITEMIDLIST);
+LPITEMIDLIST Pidl_Create(UINT);
+LPITEMIDLIST Pidl_Concatenate(LPCITEMIDLIST, LPCITEMIDLIST);
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__B5466F69_68C9_43BA_BAA4_0DC717E51478__INCLUDED_)
