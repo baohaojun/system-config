75401e1d2c618857c5793f469e87b9682cad0cbf
Bao Haojun <baohaojun@gmail.com>
2009-05-24 07:32:21 +0000

    no commit message provided

--- gcode/simple-mfc6/SimpleMFC.cpp
+++ gcode/simple-mfc6/SimpleMFC.cpp
@@ -1,25 +1,43 @@
 #include <afxwin.h>
-class CSimpleFrame : public CFrameWnd
+class CMainFrame : public CFrameWnd
 {
 public:
-	CSimpleFrame()
-		{
-// Create the window's frame
-			Create(NULL, "Windows Application");
-		}
+	CMainFrame();
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	DECLARE_MESSAGE_MAP()
 };
-struct CSimpleApp : public CWinApp
+
+CMainFrame::CMainFrame()
 {
-	BOOL InitInstance()
-		{
-// Use a pointer to the window's frame for the application
-// to use the window
-			CSimpleFrame *Tester = new CSimpleFrame ();
-			m_pMainWnd = Tester;
-// Show the window
-			m_pMainWnd->ShowWindow(SW_SHOW);
-			m_pMainWnd->UpdateWindow();
-			return TRUE;
-		}
+	Create(NULL, "Windows Application", WS_OVERLAPPEDWINDOW,
+	       CRect(120, 100, 700, 480), NULL);
+}
+
+BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+ON_WM_CREATE()
+END_MESSAGE_MAP()
+
+int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if( CFrameWnd::OnCreate(lpCreateStruct) == 0)
+	{
+		MessageBox("The window has been created!!!");
+		return 0;
+	}
+	return -1;
+}
+
+class CExerciseApp: public CWinApp
+{
+public:
+	BOOL InitInstance();
 };
-CSimpleApp theApp;
+BOOL CExerciseApp::InitInstance()
+{
+	m_pMainWnd = new CMainFrame ;
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+	return TRUE;
+}
+CExerciseApp theApp;
