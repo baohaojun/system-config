7da2941941039e7d416629112adfbef7797644a8
Bao Haojun <baohaojun@gmail.com>
2009-05-25 08:46:11 +0000

    no commit message provided

---
+++ gcode/findexec-regexp/findexec.h
@@ -0,0 +1,19 @@
+#ifndef __FINDEXEC_H__
+#define __FINDEXEC_H__
+
+extern LPWSTR match_class;
+extern LPWSTR match_program;
+extern LPWSTR match_title;
+extern LPWSTR match_save;
+extern wstring nofound_exec_program;
+extern wstring found_exec_program;
+extern LPWSTR prog_name;
+extern bool act_first;
+extern bool minimize_others;
+extern bool print_windows;
+extern bool iconic_first_test;
+
+
+
+
+#endif
