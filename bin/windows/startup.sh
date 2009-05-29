fc5a14e6361230cbdace7b557f0c92706ffd11cb
Bao Haojun <baohaojun@gmail.com>
2009-05-29 02:32:45 +0000

    no commit message provided

--- bin/windows/startup.sh
+++ bin/windows/startup.sh
@@ -5,16 +5,14 @@ cd "$HOMEDRIVE/$HOMEPATH"
 #cygrunsrv.exe -I startup -p /c/WINDOWS/system32/subst.exe -a 'q: "c:\documents and settings\bhj"'
 
 HOME2="`pwd`"
-ln -sf "$HOME2" /qq
 #psexec -s subst q: 'c:\Documents and Settings\bhj'
 subst q: "$HOMEDRIVE"\\"$HOMEPATH"
 export HOME=/q
-
-
-
 cd ~/doc
 regedit /s ImeNoToggle.reg
 . ~/.bashrc-windows
+
+ln -sf "$HOME2" /qq
 cd ~/Local\ Settings/Temp && rm tramp* -rf
 
 cygpath -alwm `which bash` > /c/.bash-loc
