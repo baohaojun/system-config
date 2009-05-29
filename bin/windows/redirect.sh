b30e8dd67eca27aae98c5f32440ddcd09449c1df
Bao Haojun <baohaojun@gmail.com>
2009-05-29 07:37:13 +0000

    no commit message provided

--- bin/windows/redirect.sh
+++ bin/windows/redirect.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd ~/bin/windows/redirect
+cd ~/bin/windows/redirect_vc6
 for x in *.sh; do
     ln -f ../shellHelper_vc6.exe `echo $x|sed -e 's/.sh$/.exe/'`; 
 done
 
+cd ~/bin/windows/redirect_cyg
+for x in *.sh; do 
+    ln -f ../shellHelper_cyg.exe `echo $x|sed -e 's/.sh$/.exe/'`;
+done
