bdad89031668208c04e27765ecb1176723bdb702
Bao Haojun <baohaojun@gmail.com>
2009-05-31 10:11:04 +0000

    no commit message provided

--- bin/windows/download-external.sh
+++ bin/windows/download-external.sh
@@ -4,10 +4,12 @@ cd /c/download
 wget -c http://download.sysinternals.com/Files/ProcessMonitor.zip
 wget -c http://download.sysinternals.com/Files/ProcessExplorer.zip
 wget -c http://download.sysinternals.com/Files/PsTools.zip
+wget -c http://puttycyg.googlecode.com/files/puttycyg-20090219.zip
 cd ~/bin/windows/lnks
 for x in /c/download/*.zip; do 
     if [[ -f "$x" ]]; then
         /bin/unzip -o "$x";
     fi
 done
+mv ./*/* . 
 chmod a+x ./*
