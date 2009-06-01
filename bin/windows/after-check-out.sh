f2967ee4df700bd294849500fa6d7b134d3f38b8
Bao Haojun <baohaojun@gmail.com>
2009-06-01 08:04:50 +0000

    no commit message provided

--- bin/windows/after-check-out.sh
+++ bin/windows/after-check-out.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-mount c: /c
-mount d: /d
-mount e: /e
-mount q: /q
-
+set -e
 cd ~/bin/windows/
 mkdir ~/bin/windows/lnks
 ./download-external.sh
@@ -15,3 +11,4 @@ cd ~/bin/windows
 ./update-password.sh
 echo 'after check out update success!'
 ln -sf ~/'Application Data/Microsoft/Internet Explorer/Quick Launch' ~/SendTo/
+echo "After check out success!"
