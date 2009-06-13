e79a96f177ac554aeb8afb744b49914ac0d3507e
Bao Haojun <baohaojun@gmail.com>
2009-06-13 14:29:53 +0000

    no commit message provided

--- bin/windows/pageant.sh
+++ bin/windows/pageant.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cygstart ~/bin/windows/pageant.exe "\"`cygpath -alwm ~/.ssh/isa_putty.ppk`\""
+if test -f ~/.ssh/isa_putty.ppk; then
+    cygstart ~/bin/windows/pageant.exe "\"`cygpath -alwm ~/.ssh/isa_putty.ppk`\""
+fi
