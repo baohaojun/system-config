5845f0458f8be4f5cd2dbd09042d5f8f36360fcb
Bao Haojun <baohaojun@gmail.com>
2009-07-20 14:15:19 +0000

    no commit message provided

--- bin/windows/startup/BatchStart.sh
+++ bin/windows/startup/BatchStart.sh
@@ -4,17 +4,21 @@
 cd ~/doc
 regedit /s ime-noctrlshift-noaltshift.reg
 helpEmacs.py&
-ssh bhj2 'while true; do 
+
+(
+    while ! ssh bhj2 echo hello; do true; done; 
+    ssh bhj2 'while true; do 
 offlineimap
 echo wait for 300 seconds
 sleep 300; done'&
-
-ssh bhj2 'while true; do 
+    
+    ssh bhj2 'while true; do 
 pop2imap --host1 localhost --user1 baohaojun@yahoo.com --port1 2000 --passfile1 .remote-auth --host2 localhost --user2 bhj --passfile2 .local-auth --folder hotmail.com --delete
 echo wait for 300 seconds
 sleep 300
 done'&
 
+)
 MYXWINSH=~/bin/windows/lnks/myxwin.sh
 grep -v xterm /usr/bin/startxwin.sh|sed -ne '/^#.*/d; /^exit$/d; /./p' > $MYXWINSH
 chmod +x $MYXWINSH
