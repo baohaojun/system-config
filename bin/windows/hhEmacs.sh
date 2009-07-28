26cefa9ed51740b8067eb12ee9b4fa5b95d35809
Bao Haojun <baohaojun@gmail.com>
2009-07-28 08:40:54 +0000

    no commit message provided

--- bin/windows/hhEmacs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-(
-    echo ImeSelect;
-    while true; do
-        nc -l -p 3837 </dev/null
-    done
-    ) | ~/bin/dexplore.exe&
--- bin/windows/startup.sh
+++ bin/windows/startup.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-
+#remember where old HOME is
 cd "$HOMEDRIVE/$HOMEPATH"
-#cygrunsrv.exe -I startup -p /c/WINDOWS/system32/subst.exe -a 'q: "c:\documents and settings\bhj"'
-
 HOME2="`pwd`"
-#psexec -s subst q: 'c:\Documents and Settings\bhj'
+
+#so that I can write /q/ anywhere I want to write "$HOME" in .sh; it'll get worse in .emacs!
 subst q: "$HOMEDRIVE"\\"$HOMEPATH"
 export HOME=/cygdrive/q
+
+
+#so that C-SPC will not toggle IME, because it's used by emacs set-mark-command
 cd ~/doc
 regedit /s ImeNoToggle.reg
+
+#setup environment?
 . ~/.bashrc-windows
 
+#rm temp files from tramp
 cd ~/Local\ Settings/Temp && rm tramp* -rf
 
+#so that shellHelper_vc6.exe can find where bash is
 cygpath -alwm `which bash` > /cygdrive/c/.bash-loc
-export CYGDIR=`cygpath -alwm /`
-cd ~/bin/windows/startup
 
+#this is useless now?
+export CYGDIR=`cygpath -alwm /`
+
+#start everything in ~/bin/windows/startup
+cd ~/bin/windows/startup
 rm *.stackdump
 for x in *; do 
     ./"$x"&
 done
 
+#make sure the next time login will run this script again
 ln -sf "$HOME2"/bin/windows/startup.sh ~/start\ menu/programs/startup
