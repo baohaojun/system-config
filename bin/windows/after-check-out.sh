485f90d80490b3779cf9a4eb9ce48f9066fb9eb4
Bao Haojun <baohaojun@gmail.com>
2009-08-13 07:13:54 +0000

    no commit message provided

--- bin/windows/after-check-out.sh
+++ bin/windows/after-check-out.sh
@@ -37,10 +37,8 @@ cd ~/bin/windows
 echo 'after check out update success!'
 ln -sf ~/'Application Data/Microsoft/Internet Explorer/Quick Launch' ~/SendTo/
 mkdir -p ~/.fonts
-cp ~/doc/MONACO.TTF /cygdrive/c/windows/fonts/simsun.ttc /cygdrive/c/windows/fonts/cour.ttf ~/.fonts || true
+cp ~/doc/monaco-linux.ttf /cygdrive/c/windows/fonts/simsun.ttc /cygdrive/c/windows/fonts/cour.ttf ~/.fonts || true
 fc-cache
 for x in {c..z}; do test -e /$x || (rm -f /$x; ln -s /cygdrive/$x /); done
-
-    
 
 echo "After check out success!"
