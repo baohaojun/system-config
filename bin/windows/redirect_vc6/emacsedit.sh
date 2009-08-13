d741af0272e7c2f080610d525452894bf3480631
Bao Haojun <baohaojun@gmail.com>
2009-08-13 07:21:37 +0000

    no commit message provided

--- bin/windows/redirect_vc6/emacsedit.sh
+++ bin/windows/redirect_vc6/emacsedit.sh
@@ -6,7 +6,7 @@ argv=( "$@" )
 for ((x=0; x<${#argv[@]}; x++)); do 
     char0=${argv[$x]:0:1}
     if test $char0 != - -a $char0 != +; then
-        argv[$x]="`cygpath -au \"${argv[$x]}\"`"
+        argv[$x]=$(cygpath -au "${argv[$x]}")
     fi
 done
 
