60bc868138eedb42b84966185c2f0b44811340f0
Bao Haojun <baohaojun@gmail.com>
2009-09-27 00:08:01 +0000

    no commit message provided

--- bin/windows/redirect_vc6/winpath.sh
+++ bin/windows/redirect_vc6/winpath.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 File=$(cygpath -au "${1:-$(getclip)}")
-cygpath -au "$(readlink -f "$File")"|tr -d '\r\n'|xargs -0 cygpath -alw|tr -d '\r\n'|putclip
+cygpath -au "$(readlink -m "$File")"|tr -d '\r\n'|xargs -0 cygpath -alw|tr -d '\r\n'|putclip
 getclip
