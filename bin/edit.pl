a4ea51b785fed10dee3bbce7ed384927d2375750
Bao Haojun <baohaojun@gmail.com>
2009-10-21 06:26:56 +0000

    no commit message provided

--- bin/edit.pl
+++ bin/edit.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl
-use Net::OpenSSH; 
+use String::ShellQuote;
 ($myname, $myip, $remote_ip, $myfile) = (@ENV{WHOAMI, LOCALIP, REMOTEIP}, $ARGV[0]);
 
 $ssh_cmd = "/scp:$myname\@$myip:$myfile";
-$ssh_cmd = Net::OpenSSH->shell_quote($ssh_cmd);
+$ssh_cmd = shell_quote($ssh_cmd);
 
 $ssh_cmd = <<EOC;
 psexec.exe -sid "\$(cygpath -alw "\$(which findexec)")" -c emacs-
@@ -11,7 +11,7 @@ emacsclient -n $ssh_cmd
 EOC
 
 $ssh_cmd =~ s/\n/;/g;
-$ssh_cmd = Net::OpenSSH->shell_quote($ssh_cmd);
+$ssh_cmd = shell_quote($ssh_cmd);
 
 print "ssh $remote_ip $ssh_cmd\n";
 system("ssh $remote_ip $ssh_cmd");
