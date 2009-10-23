3ccd0c1bc0315a4b1ea72e153d8977a3b2d6c9e9
Moritz Bunkus <moritz@bunkus.org>
2009-10-23 12:24:04 +0200

    Added a copyright notice and installation instructions.

--- mo-git-blame.el
+++ mo-git-blame.el
@@ -1,3 +1,34 @@
+;;; mo-git-blame -- An interactive, interative 'git blame' mode for Emacs
+
+;; Copyright (C) 2009  Moritz Bunkus <moritz@bunkus.org>
+;;
+;; mo-git-blame is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+;;
+;; mo-git-blame is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Installation:
+;;;
+;;; Put this file somewhere in your load-path or add the directory it
+;;; is in to it, e.g.:
+;;;
+;;; (add-to-list 'load-path "~/.emacs.d/mo-git-blame")
+;;;
+;;; Then add two autoload definitions:
+;;;
+;;; (autoload 'mo-git-blame-file "mo-git-blame" nil t)
+;;; (autoload 'mo-git-blame-current "mo-git-blame" nil t)
+
 (require 'easymenu)
 
 (defvar mo-git-blame-vars nil
