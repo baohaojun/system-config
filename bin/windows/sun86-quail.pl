794ca82fcce75b7019bbb692941a8c396c452032
Bao Haojun <baohaojun@gmail.com>
2009-11-05 05:12:40 +0000

    no commit message provided

--- bin/windows/sun86-quail.pl
+++ bin/windows/sun86-quail.pl
@@ -2,7 +2,12 @@
 use Encode;
 use utf8;
 
-while (<>) {
+open($fwubi, "<", "wubi.txt") or die $!;
+open($fpy, "<", "py.txt") or die $!;
+
+$line = 0;
+while (<$fwubi>) {
+    $line += 1;
     chomp;
     $_ = decode_utf8($_);
 
@@ -13,20 +18,38 @@ while (<>) {
     for $key (@keys) {
         next if $done{$key.$chinese};
         $done{$key.$chinese} = 1;
-        
+
         push @{$key_hash{$key}}, $chinese;
+
+        if (length (decode_utf8 $chinese) == 1) { 
+            $seq{$chinese} = $line unless $seq{$chinese};
+            push @{$chinese_hash{$chinese}}, $key;
+        }
     }
+}
+
+while (<$fpy>) {
+    chomp();
+    ($key, $chinese) = split;
+    local $"=" ";
+    push @{$py_hash{"z".$key}}, {chinese=>$chinese, wubi=>"(@{$chinese_hash{$chinese}})"};
 }
 
 @keys = sort keys %key_hash;
 
-# #print like the following:
 # #("aaaa" ["工" "恭恭敬敬"])
 $" = qq(" ");
 foreach $key (@keys) {
-     print qq(("$key" ["@{$key_hash{$key}}"])), "\n";
+    print qq(("$key" ["@{$key_hash{$key}}"])), "\n";
 }
 
+@keys = sort keys %py_hash;
+
+foreach $key (@keys) {
+    @py_data = map {$_ = ${$_}{"chinese"} . ${$_}{"wubi"} } sort {$seq{$a{"chinese"}} <=> $seq{$b{"chinese"}}} @{$py_hash{$key}};
+    $" = qq(" ");
+    print qq/("$key" ["@py_data"])/, "\n";
+}
 # foreach $key (keys %single_key_hash) {
 # #    print qq(("$key" ["$single_key_hash{$key}"])), "\n";
 # }
