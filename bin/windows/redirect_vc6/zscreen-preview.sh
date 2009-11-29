5f07263d28d4763bc92f45cb995a3ec26192459f
Bao Haojun <baohaojun@gmail.com>
2009-11-29 15:06:43 +0000

    no commit message provided

--- bin/windows/redirect_vc6/zscreen-preview.sh
+++ bin/windows/redirect_vc6/zscreen-preview.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/images/
 mv "$1" ~/images/
 sleep 5 && cygpath -au ~/images/"$(basename "$1")"|tr -d '\n'|putclip&
-cygstart "$(getclip)"
+cygstart ~/images/"$(basename "$1")"
 echo -n ../images/"$(basename "$1")"|putclip
 findexec -p gimp-2.6
