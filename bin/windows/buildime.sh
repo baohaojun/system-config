dba561c780c98f7da5bd1b95146d1d94a7ec3804
Bao Haojun <baohaojun@gmail.com>
2010-01-04 08:44:27 +0000

    no commit message provided

--- bin/windows/buildime.sh
+++ bin/windows/buildime.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e 
+cd /q/gcode/scim-cs/scim
 seq=$1
 python q:/bin/windows/terminateModule.py ywbhj"$seq" || true
 setime.pl $seq > 1.reg
 regedit /s 1.reg
+rm -f 1.reg
 
 shift
 test "$#" = 0 && vccompile '' '' || "$@" 
