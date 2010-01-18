25229b7bff7aa2cf798e9a76f430c7fc762efb94
Bao Haojun <baohaojun@gmail.com>
2010-01-18 01:35:32 +0000

    no commit message provided

--- bin/windows/redirect_vc6/zscreen-preview.sh
+++ bin/windows/redirect_vc6/zscreen-preview.sh
@@ -3,5 +3,11 @@ mkdir -p ~/images/
 mv "$1" ~/images/
 cygpath -au ~/images/"$(basename "$1")"|tr -d '\n' > ~/.zscreen.history
 cygstart ~/images/"$(basename "$1")"
-echo -n ../images/"$(basename "$1")"|putclip
+
+quote() #from bash completion
+{
+    echo \'${1//\'/\'\\\'\'}\' #'# Help vim syntax highlighting
+}
+
+cygstart /bin/bash -c "$(quote "sleep 2 && echo -n ~/images/""$(basename "$1")""|putclip")"
 findexec -p gimp-2.6
