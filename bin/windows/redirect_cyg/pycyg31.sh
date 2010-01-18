7f3e3f061d1f7dad75992237007f7b3b6e3df9d5
Bao Haojun <baohaojun@gmail.com>
2010-01-18 02:14:24 +0000

    no commit message provided

---
+++ bin/windows/redirect_cyg/pycyg31.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+debug=false
+if [[ "$1" == -x ]]
+then
+    debug=true
+    shift
+fi
+pyfile="$1"
+ if ! [[ ${pyfile:0:1} == / ]];
+ then
+     pyfile=`which "$pyfile"`
+ fi
+shift
+pyfile=`cygpath -alw "$pyfile"`
+
+wpy31 "$pyfile" "$@"
+if [[ $debug == true ]]
+then
+    echo -n Press any key to exit ...
+    read
+fi
