44a68b834515b7ba28177377efffeb72754763ba
theonering <patrick.vonreth@gmail.com>
2010-03-06 19:18:34 +0100

    some cleaning up, copyright header

--- src/plugins/dbusbinding/dbusbinding.cpp
+++ src/plugins/dbusbinding/dbusbinding.cpp
@@ -1,3 +1,19 @@
+/****************************************************************************************
+ * Copyright (c) 2010 Patrick von Reth <patrick.vonreth@gmail.com>                      *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
 #include "dbusbinding.h"
 #include <QtCore>
 
@@ -29,12 +45,6 @@ void DBusBinding::registerTypes(){
 
 
 ApplicationsList DBusBinding::getApplicationList(){
-    //workaround for the "" application bug
-    if(snore->getAplicationList()->contains("")){
-        snore->getAplicationList()->take("").clear();
-        qDebug()<<"Removed wrong entry ("") in application list";
-    }
-
     return *snore->getAplicationList();
 }
 
