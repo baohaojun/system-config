e36ba88b65c39b91ee4d0244ea026af6267ec25b
Bao Haojun <baohaojun@gmail.com>
2010-03-17 06:58:35 +0000

    no commit message provided

--- bin/beagle-grep.sh
+++ bin/beagle-grep.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-start_dir=`pwd`
-while test ! -d "$start_dir"/.beagle -a "$start_dir" != /
-do
-    start_dir=`readlink -f "$start_dir"/..`
-done
-
-if test "$start_dir" = /
-then
-    echo 'Error: can not find .beagle anywhere'
-    exit
-fi
-
-beagle_dir=$start_dir/.beagle
-
-beagle-static-query \
-    --add-static-backend "$beagle_dir" \
-    --backend none --max-hits 100000 "$2"\
-|sed -e 's!^file://!!'\
-|xargs grep -H -n -I -e "$2"
+my-beagle "$2"|xargs grep -H -n -I -e "$2"
