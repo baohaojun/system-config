c748fada1e8cabe569067d456d9e5dd6c4c1e412
Bao Haojun <baohaojun@gmail.com>
2010-04-30 09:28:34 +0000

    no commit message provided

---
+++ bin/linux/test-beagle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmpd=/tmp/$$.test-beagle
+mkdir $tmpd
+for x in "$@"; do echo "$x"; done > $tmpd/1.txt
+cd $tmpd
+mkdir .beagle; beagle-build-index --recursive --deny-pattern .beagle --enable-deletion --target .beagle/ .
+beagle-dump-index --indexdir=`pwd`/.beagle --term-frequencies
+rm $tmpd -rf
