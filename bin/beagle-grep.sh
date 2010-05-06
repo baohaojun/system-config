4e733dca69d5072d9b27083e09359105119bbd2a
Bao Haojun <baohaojun@gmail.com>
2010-05-06 13:44:58 +0000

    no commit message provided

--- bin/beagle-grep.sh
+++ bin/beagle-grep.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo -n beagle query argument \`'[01;31m'$(get_longest_token "$@")'[0m'\' 1>&2
 echo 1>&2
-my-beagle "$@"|xargs grep -H -n -I "$@" 
+my-beagle "$@"|tee /tmp/$$.beagel|xargs grep -H -n -I "$@" 
+cat /tmp/$$.beagel|grep -I "$@"|perl -npe 's/$/:1: [01;31m****************![0m/'
+rm /tmp/$$.beagel
