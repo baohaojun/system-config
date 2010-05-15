1b7b775744b1fcd9026109ad3fa4e3a8aca7e468
Bao Haojun <baohaojun@gmail.com>
2010-05-15 23:37:18 +0800

    do the job after checkout

---
+++ bin/after-co-ln-s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+cd ~/windows-config/
+for x in * .*;
+do
+    if test $x = . -o $x = .. -o $x = .git; 
+    then
+        continue;
+    fi
+    if test -e ~/$x -a "$(readlink -f ~/$x)" != ~/windows-config/$x; 
+    then
+        echo "Error: ~/$x already exist and it's not softlink to ~/windows-config/$x"
+        exit -1
+    elif ! test -h ~/$x;
+    then
+        ln -s ~/windows-config/$x ~/
+    fi
+done
