c73e64315b65b59ecbc8ecad4106fd3a8da7e7f5
Bao Haojun <baohaojun@gmail.com>
2010-05-28 11:01:30 +0800

    make after-check-out shorter by modularizing it a bit

--- bin/linux/download-external.sh
+++ bin/linux/download-external.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-set -e
-cd ~/bin/linux/ext/
-ext_download=(
-    http://www.antlr.org/download/antlr-3.2.tar.gz
-    http://www.antlr.org/download/antlr-3.2.jar
-    http://www.antlr.org/download/antlrworks-1.3.1.jar
-    http://www.stringtemplate.org/download/stringtemplate-3.2.1.tar.gz
-)
-for x in "${ext_download[@]}"; do 
-    while ! wget --timeout 30 --connect-timeout 30 --tries 1 $x; do rm `basename $x`; done
-done
-mv antlr-3.2.jar antlr3.jar
-tar zxfv stringtemplate*tar.gz
-mv stringte*/lib/string*.jar stringtemplate.jar
-mv antlrworks-1.3.1.jar antlrworks.jar
-cd ~/.emacs_d/lisp/ext/
 
-mkdir -p ~/Downloads/intel
-cd ~/Downloads/intel
+function download_antlr() {
+    mkdir -p ~/bin/linux/ext/
+    cd ~/bin/linux/ext/
+    ext_download=(
+        http://www.antlr.org/download/antlr-3.2.tar.gz
+        http://www.antlr.org/download/antlr-3.2.jar
+        http://www.antlr.org/download/antlrworks-1.3.1.jar
+        http://www.stringtemplate.org/download/stringtemplate-3.2.1.tar.gz
+    )
+    for x in "${ext_download[@]}"; do 
+        while ! wget -c --timeout 30 --connect-timeout 30 --tries 1 $x; do echo `basename $x` 'download failed, retry!'; done
+    done
+    ln -sf antlr-3.2.jar antlr3.jar
+    tar zxfv stringtemplate*tar.gz
+    ln -sf stringte*/lib/string*.jar stringtemplate.jar
+    ln -sf antlrworks-1.3.1.jar antlrworks.jar
+}
 
+function download_lisp() {
+    mkdir -p ~/.emacs_d/lisp/ext/
+    cd ~/.emacs_d/lisp/ext/
+}
 
-cat <<EOF|lftp
+function download_books() {
+    mkdir -p ~/Downloads/intel
+    cd ~/Downloads/intel
+
+#only download these things for myself, or else it will take too much time
+    cat <<EOF|lftp || true
 get -c http://download.intel.com/design/PentiumII/manuals/24319002.pdf -o "Intel Architecture Software Developer's Manual, Volume 1: Basic Architecture.pdf"
 get -c http://download.intel.com/design/PentiumII/manuals/24319102.pdf -o "Intel Architecture Software Developer's Manual, Volume 2: Instruction Set Reference Manual.pdf"
 get -c http://download.intel.com/design/PentiumII/manuals/24319202.pdf -o "Intel Architecture Software Developer's Manual, Volume 3: System Programming.pdf"
@@ -34,5 +42,11 @@ get -c http://www.intel.com/Assets/PDF/manual/253669.pdf -o "Intel® 64 and IA-3
 get -c http://www.intel.com/Assets/PDF/manual/248966.pdf -o "Intel® 64 and IA-32 Architectures Optimization Reference Manual.pdf"
 get -c http://www.acpi.info/DOWNLOADS/ACPIspec40.pdf
 EOF
+}
 
-echo OK
+function download_external() {
+    download_antlr
+    download_lisp
+    download_books    
+}
+
