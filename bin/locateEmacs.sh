ff5399fc45338aeaa19283f8a347062ca4e37ecf
Bao Haojun <baohaojun@gmail.com>
2010-05-28 19:38:36 +0800

    minor fix

--- bin/locateEmacs.sh
+++ bin/locateEmacs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-(for x in ~/tmp/locate/*; do if test -f "$x"; then locate -d "$x" "$@"; fi; done
-locate "$@" )|sort -u
+(for x in ~/tmp/locate/*; do if test -f "$x"; then locate -d "$x" "$@"; fi; done)|sort -u
 #locate "$@" |perl -npe 's!^(.*)$!cygpath.exe -alw "$1"!'|sh|perl -npe 's!\\!/!g; s!^!c:/!'
