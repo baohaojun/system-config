daf610f07b2ee3c7cca7f2a28b348a16da6df221
Bao Haojun <baohaojun@gmail.com>
2010-05-31 18:01:48 +0800

    do not error exit when files (such as .bashrc) already exist, instead, back it up

--- .config-ubuntu.sh
+++ .config-ubuntu.sh
@@ -25,17 +25,18 @@ deb http://security.ubuntu.com/ubuntu lucid-security multiverse
 EOF"
 
 sudo apt-get update
-sudo apt-get install git-core ssh subversion vim
+sudo apt-get install -y git-core ssh subversion vim
 
-mkdir ~/windows-config
+
+cd ~
+git clone git://192.168.0.46/windows-config.git || (cd ~/windows-config && git pull)
 cd ~/windows-config
-git clone git://github.com/baohaojun/windows-config.git
 git checkout -- . 
 ./bin/after-co-ln-s.sh
 wget http://192.168.0.46/jdk-1_5_0_22-linux-amd64.bin
 sudo sh ./jdk-1_5_0_22-linux-amd64.bin
 sudo mv jdk1.5.0_22 /usr/local/jdk1.5
-apt-get install lftp
+sudo apt-get install -y lftp
 mkdir -p ~/bin/linux/ext/
 cd ~/bin/linux/ext
 lftp -c "mirror -c http://192.168.0.46/google"
@@ -43,7 +44,8 @@ tar zxfv ./google/android-sdk_r06-linux_86.tgz
 mkdir -p ~/bin/linux/ext/android-sdk-linux_86/temp
 cd ~/bin/linux/ext/android-sdk-linux_86/temp
 ln ../../google/* . 
-android
-~/bin/linux/after-checkout.sh
+. ~/.bashrc
+~/bin/linux/ext/android-sdk-linux_86/tools/android&
+~/bin/linux/after-check-out.sh
 
 echo 'OK, your configuration has completed!'
--- bin/after-co-ln-s.sh
+++ bin/after-co-ln-s.sh
@@ -10,7 +10,8 @@ do
     if test -e ~/$x -a "$(readlink -f ~/$x)" != "$(readlink -f ~/windows-config/$x)"; 
     then
         echo "Error: ~/$x already exist and it's not softlink to ~/windows-config/$x"
-        exit -1
+        mv ~/$x ~/$x.bak
+        ln -s ~/windows-config/$x ~/
     elif ! test -h ~/$x;
     then
         ln -s ~/windows-config/$x ~/
