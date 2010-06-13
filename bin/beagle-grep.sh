245d9f37d2e539f4770e7a088ec9c7ef9baa84a7
Bao Haojun <baohaojun@gmail.com>
2010-06-13 10:30:25 +0800

    make android install more intelligent

--- bin/beagle-grep.sh
+++ bin/beagle-grep.sh
@@ -6,13 +6,28 @@ use String::ShellQuote;
 
 my $pat = '';
 my $pathpat = ();
+chomp(my $pwd =qx/pwd/);
+my $pwd_pat='no such componet';
+
+my $ignore_case;
 
 GetOptions(
            "e=s" => \$pat,
            "p=s" => \$pathpat,
+           "d=s" => \$pwd_pat,
+           "i!" => \$ignore_case,
           );
 
+if ($ignore_case) {
+  $ignore_case = '-i';
+} else {
+  $ignore_case = '';
+}
+
 die 'no regexp specified' unless $pat;
+$pwd =~ s!($pwd_pat.*?)/.*!$1!; 
+chdir $pwd or die "$pwd: $!";
+
 $pat = shell_quote($pat);
 print "pat is `$pat'\n";
 
@@ -27,7 +42,7 @@ my $beagle_files = qx/my-beagle $pat/;
 my @beagle_files = split(/\n/, $beagle_files);
 
 
-open(my $grep, "|xargs grep -H -n -I -e $pat");
+open(my $grep, "|xargs grep -H -n -I $ignore_case -e $pat");
 
 for (@beagle_files) {
   next unless m/$pathpat/i;
