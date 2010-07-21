3c703e1fdc41c7f2c51a931710ba961661fa95eb
Bao Haojun <baohaojun@gmail.com>
2010-07-21 19:29:38 +0800

    mf

--- bin/linux/test-beagle.sh
+++ bin/linux/test-beagle.sh
@@ -2,12 +2,13 @@
 
 tmpd=/tmp/$$.test-beagle
 mkdir $tmpd
-txt_file=$tmpd/1.cs
+txt_file=$tmpd/${TBNAME:-1.cs}
 
 for x in "$@"; do echo "$x"; done > $txt_file
 
 mkdir $tmpd/.beagle; beagle-build-index --recursive --deny-pattern .beagle --enable-deletion --target $tmpd/.beagle/ $tmpd
 beagle-dump-index --indexdir=$tmpd/.beagle --term-frequencies
+beagle-static-query --add-static-backend $tmpd/.beagle --backend none --max-hits 100000 "$@"
 #beagle-extract-content $txt_file
 #rm $tmpd -rf
 #my-beagle "$@"
