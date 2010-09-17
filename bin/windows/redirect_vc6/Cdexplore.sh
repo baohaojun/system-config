b25b9d150dc3c28f8e6a4c7a154382864d88de14
Bao Haojun <baohaojun@gmail.com>
2010-09-17 17:44:38 +0800

    mf

--- bin/windows/redirect_vc6/Cdexplore.sh
+++ bin/windows/redirect_vc6/Cdexplore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp /c/windows/hh 'q:\bin\windows\redirect_vc6\dexplore.exe'
-/c/windows/apppatch/apploc 'q:\bin\windows\redirect_vc6\dexplore.exe' "`cygpath -alw \"$1\"`" /L0409
+cp /c/windows/hh 'q:\windows-config\bin\windows\redirect_vc6\dexplore.exe'
+/c/windows/apppatch/apploc 'q:\windows-config\bin\windows\redirect_vc6\dexplore.exe' "`cygpath -alw \"$1\"`" /L0409
 
