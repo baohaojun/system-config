a742c79ca539f61ee3c477bd814655369d5f5d5d
Bao Haojun <baohaojun@gmail.com>
2010-10-29 22:05:38 +0800

    mf

--- gcode/wikipediaDumpReader-0.2.10/get_article.py
+++ gcode/wikipediaDumpReader-0.2.10/get_article.py
@@ -4,6 +4,7 @@ import os, sys
 import re
 import codecs
 from xml.dom.minidom import parseString
+import struct
 from xml.parsers.expat import ExpatError
 try:
 	import mod.bz2 as bz2
@@ -17,6 +18,18 @@ def die(arg):
 
 f = bz2.BZ2File("/home/bhj/wikipedia/enwiki-latest-pages-articles.xml.bz2")
 prev_block_num, prev_block_byte, prev_block_bit, next_block_num, next_block_byte, next_block_bit, block, start, len_ = map(lambda(x): int(x, 0), sys.argv[1:])
+
+if (start < 0):
+    # this is work-around for a bug where wikidump-reader incorrectly used 64bit instead of 32bit for the hi-4byte calc.
+    I_bits = struct.calcsize('I') * 8
+    l_bits = struct.calcsize('l') * 8
+    maxI = 1 << I_bits
+    maxl = 1 << l_bits
+
+    bh = -start/maxl + 1
+    start = start + bh*maxl - bh*maxI
+    sys.stderr.write("fixed start to be %d, bh is %x\n" % (start, bh))
+
 
 if block == prev_block_num:
     block_byte = prev_block_byte
