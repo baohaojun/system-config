cee9f5583368b50c13b9a24e46de617bb8ad6fab
Bao Haojun <baohaojun@gmail.com>
2010-11-02 00:06:33 +0800

    mf

--- gcode/wikipediaDumpReader-0.2.10/mparser.py
+++ gcode/wikipediaDumpReader-0.2.10/mparser.py
@@ -8,27 +8,41 @@ import sys
 import codecs
 import struct
 from xml.dom.minidom import parseString
+import time
+
+g_old_time = time.time()
+g_cur_time = g_old_time
+g_cur_numarticles = 0
+g_old_numarticles = 0
+
 
 def buildIndex(inputbz2Archive):
     f = bz2.BZ2File(inputbz2Archive)
     if f.readline()[0:10] != "<mediawiki":
         sys.stderr.write("Maybe not a correct mediawiki bz2 dump\n");
     blocknum = -1
-    numarticles = 0
+    global g_cur_numarticles, g_old_numarticles, g_cur_time, g_old_time
+    g_cur_numarticles = 0
     maxI = (1 << (struct.calcsize('I')*8))
     def writeBlock(bzblocks):
-        sys.stderr.write("Processing block 0x%x\n" % bzblocks[0]);
-        sys.stdout.write("block: 0x%x\t0x%x\t0x%x\n" % (bzblocks[0], bzblocks[2], bzblocks[3]))
+        global g_cur_numarticles, g_old_numarticles, g_cur_time, g_old_time
+        g_cur_time = time.time()
+        if (g_cur_time - g_old_time > 10):
+            sys.stderr.write("finished %d articles in %d seconds, rate is %.2f per sec\n" % ( g_cur_numarticles - g_old_numarticles, 
+                                                                                              g_cur_time - g_old_time,
+                                                                                              (g_cur_numarticles - g_old_numarticles)/(g_cur_time - g_old_time)))
+            g_old_time = g_cur_time
+            g_old_numarticles = g_cur_numarticles
+                            
         return bzblocks[0]
 
     try:
         while 1:
             while 1: # find page start
                 l = f.readline()
-                bzblocks1 = f.tellbzblock()
-                if bzblocks1[0] != blocknum:
-                    blocknum = writeBlock(bzblocks1)
-                    sys.stderr.write("0x%x articles found\n" % numarticles)
+                start_block = f.tellbzblock()
+                if start_block[0] != blocknum:
+                    blocknum = writeBlock(start_block)
                 if l == "  <page>\n":
                     start = f.tell()
                     break
@@ -37,10 +51,9 @@ def buildIndex(inputbz2Archive):
 
             while 1: # find title and page end
                  l = f.readline()
-                 bzblocks2 = f.tellbzblock()
-                 if bzblocks2[0] != blocknum:
-                         blocknum = writeBlock(bzblocks2)
-                         sys.stderr.write("0x%x articles found\n" % numarticles)
+                 end_block = f.tellbzblock()
+                 if end_block[0] != blocknum:
+                         blocknum = writeBlock(end_block)
                  if l[0:11] == '    <title>':
                          titleline = l
                  if l == "  </page>\n":
@@ -49,10 +62,15 @@ def buildIndex(inputbz2Archive):
                  if l == "":
                          raise StopIteration
 
-            title = titleline[11:-9]
-            numarticles += 1
-
-            sys.stdout.write(("title: %s\t0x%x\t0x%x\t0x%x\n" % (title, bzblocks1[0], start - (bzblocks1[1][0] + bzblocks1[1][1] * maxI), ending - start) ))
+            D = parseString(titleline)
+            n = D.getElementsByTagName('title')
+            title = n[0].firstChild.nodeValue
+            g_cur_numarticles += 1
+            sys.stdout.write(("0x%08x " * 9 + "%s\n") % (
+                    start_block[0], start_block[2], start_block[3],
+                    end_block[0], end_block[2], end_block[3],
+                    start_block[0], start - (start_block[1][0] + start_block[1][1] * maxI), ending - start,
+                    title.encode('utf-8')))
     except StopIteration:
         sys.stderr.write("End of that block : %s\n" % `f.name`)
 
