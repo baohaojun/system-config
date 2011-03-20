0ccaeab3cd45dbf7e98709b4b1d7bfefd7c0fe11
Bao Haojun <baohaojun@gmail.com>
2011-03-20 22:05:34 +0800

    add shudo program

---
+++ bin/linux/shudo.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+
+$l = 0;
+
+while (<>) {
+    chomp;
+    last if $l == 9;
+    $row[$l] = $_;
+    $l++;
+    die "invalid input: not 9 chars" if length($_) != 9;
+    
+    die "invalid input: non-digit, non-blank" if m/[^0-9 ]/;
+    
+    die "invalid input: repeated digit" if m/([0-9]).*\1/;
+    
+}
+
+die "not enough rows" if $l != 9;
+
+for $col (0..8) {
+    for $row (0..8) {
+        $col[$col] .= substr($row[$row], $col, 1);
+        $m[$row][$col] = substr($row[$row], $col, 1);
+    }
+}
+
+for (0..8) {
+    die "invalid input: repeated digit" if $col[$_] =~ m/([0-9]).*\1/;
+}
+
+sub fill_one_cell ($) {
+    my ($m) = @_;
+    my @dup;
+    my ($found, $br, $bc);
+
+    for $r (0..8) {
+        for $c (0..8) {
+            $dup[$r][$c] = $m->[$r][$c];
+            if (not $found and $m->[$r][$c] eq " ") {
+                $found = 1;
+                $br = $r;
+                $bc = $c;
+                print "found space at [$r][$c]\n";
+            }
+        }
+    }
+
+    if (not $found) {
+        return \@dup;
+    }
+
+    for my $try (1..9) {
+        my $go = 1;
+        for $x (0..8) {
+            if ($m->[$x][$bc] == $try or $m->[$br][$x] == $try) {
+                $go = 0;
+                last;
+            } else {
+                
+            }
+        }
+
+        if ($go == 0 and $try == 9) {
+            return 0;
+        }
+        
+        if ($go == 0) {
+            next;
+        }
+        $dup[$br][$bc] = $try;
+        print "try $try at [$br][$bc]\n";
+        if ((my $res = fill_one_cell(\@dup)) != 0) {
+            for $r (0..8) {
+                for $c (0..8) {
+                    print $res->[$r][$c];
+                }
+                print "\n";
+            }
+            exit(0);
+        }
+    }
+                   }
+                
+            
+           
+
+fill_one_cell(\@m);
