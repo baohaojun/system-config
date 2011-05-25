f9800662a7468963fa8295946974ad6b7f75dc92
Bao Haojun <baohaojun@gmail.com>
2011-05-25 09:00:01 +0800

    mf

--- bin/linux/repack-bootimg.pl
+++ bin/linux/repack-bootimg.pl
@@ -18,6 +18,4 @@ chdir $dir or die "$ARGV[1] $!";;
 
 system ("mkbootimg --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.gz -o $ARGV[2]");
 
-unlink("ramdisk-repack.cpio.gz") or die $!;
-
 print "\nrepacked boot image written at $ARGV[1]-repack.img\n";
