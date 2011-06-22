67f127c0ba8b5354fa0d4cb2a1e21bf839e50ab6
Bao Haojun <baohaojun@gmail.com>
2011-06-22 08:32:26 +0800

    Merge branch 'master' of github.com:/baohaojun/windows-config

--- bin/windows/download-external.sh
+++ bin/windows/download-external.sh
@@ -91,7 +91,9 @@ index 4dcc32d..2e6548f 100644
  #endif
 EOF
         ./configure;
-        make -j8 install)
+        make -j8
+        make install
+    )
 
     cd emacs-goodies-el*/elisp/emacs-goodies-el/ && mkdir themes
 }
@@ -256,30 +258,59 @@ function get-putty()
 
 test "$DOWN" == yes && (gcc-switch.sh 3; get-putty; gcc-switch.sh 4)
 
-function get-cscope()
-{
-    if ! test -d /c/download/cscope/cscope; then
-        mkdir -p /c/download/cscope
-        cd /c/download/cscope
-        cvs -d:pserver:anonymous@cscope.cvs.sourceforge.net:/cvsroot/cscope login
-        cvs -z3 -d:pserver:anonymous@cscope.cvs.sourceforge.net:/cvsroot/cscope co -P cscope
-    fi
-}
-
 function get-stlport-and-boost()
 {
-    mkdir /d/bhj -p
+    mkdir -p /d/bhj
     cd /d/bhj
-    mkdir -p STLport-5.2.1/lib boost_1_34_1/stage/lib STLport-5.2.1/stlport
+    mkdir -p -p STLport-5.2.1/lib boost_1_34_1/stage/lib STLport-5.2.1/stlport
     wget -N http://sourceforge.net/projects/stlport/files/STLport/STLport-5.2.1/STLport-5.2.1.tar.bz2
-    tar jxfv STLport-5.2.1.tar.bz2
+
     wget -N http://sourceforge.net/projects/boost/files/boost/1.34.1/boost_1_34_1.tar.bz2
-    tar jxfv boost_1_34_1.tar.bz2
-    for x in ~/vc6/path/*; do export PATH=`readlink -m "$x"`:$PATH; done
+    unset PATHVC
+    for x in ~/vc6/path/*; do export PATHVC=$PATHVC:`readlink -m "$x"`; done
+    export PATH=~/bin/:$PATHVC:$PATH
     export INCLUDE=`for x in ~/vc6/inc/*; do readlink -f "$x"; done|u2dpath`
     export LIB=`for x in ~/vc6/lib/*; do readlink -f "$x"; done|u2dpath`
     
     (
+        mkdir -p vcrun2010
+        cd vcrun2010
+        wget -N http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe
+        chmod +x vcredist_x86.exe
+        cygstart vcredist_x86.exe
+    )
+
+    (
+        mkdir -p psdk
+        cd psdk
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.1.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.2.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.3.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.4.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.5.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.6.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.7.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.8.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.9.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.10.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.11.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.12.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.13.cab
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.bat
+        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/Extract.exe
+        wget -N http://www.avantbrowser.cn/release/absetup.exe
+    )
+
+    (
+        mkdir -p vc6
+        cd vc6
+        wget -N http://download.microsoft.com/download/1/9/f/19fe4660-5792-4683-99e0-8d48c22eed74/Vs6sp6.exe
+    )
+
+    read -p "Make sure you have setup vc6/sp6/psdk! Press any key to continue..."
+
+    (
+        tar jxfv ./STLport-5.2.1.tar.bz2
         cd STLport-5.2.1
         patch -p1 <<EOF
 diff --git a/stlport/stl/config/user_config.h b/stlport/stl/config/user_config.h
@@ -303,35 +334,22 @@ EOF
     )
 
     (
-        mkdir vcrun2010
-        cd vcrun2010
-        wget -N http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.1.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.2.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.3.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.4.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.5.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.6.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.7.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.8.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.9.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.10.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.11.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.12.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.13.cab
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.bat
-        wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/Extract.exe
-        wget -N http://download.microsoft.com/download/1/9/f/19fe4660-5792-4683-99e0-8d48c22eed74/Vs6sp6.exe
-        wget -N http://www.avantbrowser.cn/release/absetup.exe
-
-        chmod +x vcredist_x86.exe
-        cygstart vcredist_x86.exe
-    )
-
-    (
+        tar jxfv boost_1_34_1.tar.bz2
         cd boost_1_34_1
-    )
-    
-    
 
+    )
+    (
+        mkdir -p python2.5
+        cd python2.5
+        wget -N http://python.org/ftp/python/2.7.2/python-2.7.2.msi
+        chmod +x *.msi
+        cygstart *.msi
+    )
+    (
+        mkdir -p python3.1
+        cd python3.1
+        wget -N http://python.org/ftp/python/3.1.3/python-3.1.3.msi
+        chmod +x *.msi
+        cygstart *.msi
+    )        
 }
