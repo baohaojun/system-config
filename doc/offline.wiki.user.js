a3ac3b4674d84fa08e7d041698166df2b94c52a6
Bao Haojun <baohaojun@gmail.com>
2011-07-07 13:38:10 +0800

    mf

--- doc/offline.wiki.user.js
+++ doc/offline.wiki.user.js
@@ -5,16 +5,21 @@
 // @include       http://localhost:8000/*
 // ==/UserScript==
 
+
+var docx = document.location + "";
+
+var lang = docx.replace(new RegExp("^http://localhost:8000/(.*?)/.*"), "$1");
+
 var re = new RegExp("^http://localhost:8000/wiki/");
-var file_re = new RegExp("^http://localhost:8000/article/File%3[Aa]");
+var file_re = new RegExp("^http://localhost:8000/(.*?)/article/File%3[Aa]");
 var slash_re = new RegExp("/*$");
 for (var x in document.links) { 
     var y = document.links[x].href; 
     if (! y) 
         continue; 
 
-    y = y.replace(re, "http://en.wikipedia.org/wiki/");
-    y = y.replace(file_re, "http://en.wikipedia.org/wiki/File%3A");
+    y = y.replace(re, "http://" + lang + ".wikipedia.org/wiki/");
+    y = y.replace(file_re, "http://" + lang + ".wikipedia.org/wiki/File%3A");
 
     document.links[x].href = y.replace(slash_re, "");
 }
