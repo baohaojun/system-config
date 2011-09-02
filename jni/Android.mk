10419f95b930a96814d8fb69987d0baed36e2909
Justin Ho <justinho@google.com>
2011-09-02 08:56:48 -0700

    Adding LOCAL_MODULE_TAGS := optional to PinyinIME
    Change-Id: Ice5dfba97d2f1ccba759b9cf3ba1d3a4451964af

--- jni/Android.mk
+++ jni/Android.mk
@@ -27,5 +27,6 @@ LOCAL_C_INCLUDES += $(JNI_H_INCLUDE)
 LOCAL_LDLIBS += -lpthread
 LOCAL_MODULE := libjni_pinyinime
 LOCAL_SHARED_LIBRARIES := libcutils libutils
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
