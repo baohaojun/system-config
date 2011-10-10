a8af467e5660a35342029c2796de99cd551454b2
Austin <austiny.cn@gmail.com>
2011-10-09 21:41:16 -0400

    added help msg

--- weibo-timeline.el
+++ weibo-timeline.el
@@ -254,6 +254,84 @@
     (ewoc-filter weibo-timeline-data (lambda (data) nil))
     (weibo-timeline-pull-new)))
 
+(defconst weibo-timeline-help-content
+  "* 简介
+
+\"使用Emacs的微博\"（weibo.emacs)是一个在Emacs上使用的微博客户端。它使用Elisp语言编写，提供了察看时间线，察看微博及评论，察看图片，发表和转发微博，发表评论和回复的功能。它基于Emacs，可以在Windows, Linux和Mac上使用。
+
+* 功能
+
+- 察看时间线
+提供察看以下时间线的功能
+    我的关注
+    我的微博
+    提到我的
+    谁在说
+    我的评论
+    收到评论
+    
+- 察看微博和评论
+
+- 提供察看某一条微博和评论的功能
+发表微博，发表评论和回复评论
+
+- 提供发表新微博，对某一条微薄发表评论及回复评论的功能
+
+* 使用方法
+
+- 授权
+
+第一次使用时，将会自动跳转到微博应用授权的页面，登录后确认授权，然后将提示的pin码输回Emacs的mini-buffer即可。
+
+- 开始
+
+通过命令weibo-timeline开始察看时间线: M-x weibo-timeline
+
+- 时间线界面
+
+屏幕的最顶端，会出现如下三排提示：
+
+微博：我的关注(a) 我的微博(i) 提到我的(@) 谁在说(w) 我的评论(o) 收到评论(c)
+
+命令：发表微博(P) 察看(L) 转发(T) 评论(C) 回复(R)
+
+操作：新消息(g) 刷新(r) 下一条（空格) 帮助(h) 退出(q)
+
+微博行包括了不同的时间线及它们的切换方法。括号内的按键将在当前窗口显示对应的时间线：如提到我的(@)，当按下@键时，将切换提到我的时间线。
+
+命令行包括了可以在时间线中使用的命令。除了发表微博外，其他命令都应将光标移到对应的微博或者评论上使用。
+
+操作行包括了可以在时间线中使用的其他命令，如获取新消息，移到下一条消息，以及退出等等。
+
+- 切换时间线
+
+使用微博行所提示的按键进行切换。
+
+- 察看时间线
+
+使用Emacs默认的组合键移动光标。此外p，n，b，f键可用来上下左右移动光标。当光标移动到某一条微博或评论时，可按L键察看其对应的微博和最新评论。当微博中有图片时，会在时间线中显示缩略图。将光标移到图片上，按Enter键，会在新窗口中打开原图（按q键退出图片察看）。其他命令如操作中所示。
+
+- 发表微博
+
+在时间线界面中按P，将出现名为“发表微博”的窗口。在此窗口中编辑你想要发表的微博。完成后，按\\C-c\\C-c(Ctrl-c, Ctrl-c)提交微博。如果在编辑中想取消发表，按\\C-c\\C-d将关闭发表窗口（注意：此时你所编辑的内容将消失）。
+
+- 转发微博
+
+在时间线中，将光标移至某一条微博，按T将出现名为“转发微博”的窗口。此时操作与发表微博类似，完成编辑后，按\\C-c\\C-c提交微博，按\\C-c\\C-d取消。
+
+- 发表评论和回复
+
+在时间线中，按C对当前光标所在的微博发表评论。此时将出现“发表评论”窗口，操作与发表微博相似：完成编辑后，按\\C-c\\C-c提交评论，按\\C-c\\C-d取消。在微博详情和评论时间线中，按R键可以对评论进行回复。
+
+- 备注
+
+注意并非所有命令都对所有时间线适用。比如你不能对微博进行回复和对评论进行转发。命令行将之包含当前时间线可用的命令。")
+
+(defun weibo-timeline-help ()
+  (interactive)
+  (with-output-to-temp-buffer "微博帮助"
+    (princ weibo-timeline-help-content)))
+
 (defvar weibo-timeline-mode-map
   nil
   "Keymap for weibo-timeline-mode")
@@ -266,6 +344,7 @@
 	(define-key map " " 'weibo-timeline-move-next)
 	(define-key map "r" 'weibo-timeline-refresh)
 	(define-key map "s" 'weibo-timeline-inspect)
+	(define-key map "h" 'weibo-timeline-help)
 	
 	(define-key map "P" 'weibo-timeline-post)
 	(define-key map "L" 'weibo-timeline-look)
