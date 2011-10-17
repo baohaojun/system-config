1e73e6ecadc1479a9424c7d520ab9ab54ad4a8bc
Bao Haojun <baohaojun@gmail.com>
2011-10-17 14:34:17 +0800

    mf

--- bin/googleemacs.sh
+++ bin/googleemacs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-firefox 'http://www.google.com/search?q='"`echo \"$@\"|perl -npe 'chomp; s#([^_0-9a-zA-Z ])#sprintf(\"%%%02x\", ord($1))#seg; s# #+#g'`" &
+url='http://www.google.com/search?q='"`echo \"$@\"|perl -npe 'chomp; s#([^_0-9a-zA-Z ])#sprintf(\"%%%02x\", ord($1))#seg; s# #+#g'`" 
+echo "$url"|tee /dev/tty|putclip
+firefox "$url"&
