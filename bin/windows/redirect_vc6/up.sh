86c817f3570f4e616287244af4144451ce46c37e
Bao Haojun <baohaojun@gmail.com>
2011-11-21 16:54:38 +0800

    mf

--- bin/windows/redirect_vc6/up.sh
+++ bin/windows/redirect_vc6/up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 File=$(cygpath -au "${1:-.}")
 if test ${File:0:2} = //; then
-    cygpath -au "$File"|tr -d '\r\n'|putclip
+    printf %q "$(cygpath -au "$File"|tr -d '\r\n')"|putclip
 else 
-cygpath -au "$(readlink -f "$File")"|tr -d '\r\n'|xargs -0 cygpath -au|tr -d '\r\n'|putclip
+    printf %q "$(cygpath -au "$(readlink -f "$File")"|tr -d '\r\n'|xargs -d '\n' cygpath -au|tr -d '\r\n')"|putclip
 fi
 getclip
