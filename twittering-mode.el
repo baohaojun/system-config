c39cb9d603f1df3febbd820c4ebd45fb76892123
William Xu <william.xwl@gmail.com>
2011-12-15 20:43:41 +0800

    (twittering-make-fontified-tweet-text): Fix dead loop bug.

--- twittering-mode.el
+++ twittering-mode.el
@@ -543,7 +543,7 @@ If we invoke `twittering-get-and-render-timeline' from a twittering buffer, then
 do not display unread notifier on mode line.")
 
 (defvar twittering-use-master-password nil
-  "*Wheter to store private information encrypted with a master password.")
+  "*Whether to store private information encrypted with a master password.")
 (defvar twittering-private-info-file
   (expand-file-name "~/.emacs.d/twittering/.twittering.gpg")
   "*File for storing encrypted private information when
@@ -4175,10 +4175,20 @@ following symbols;
      (unless twittering-is-getting-emotions-p
        (setq twittering-is-getting-emotions-p t)
        (twittering-get-simple nil nil nil 'emotions))
-     (while (string-match "\\(\\[[^][]+\\]\\)" str)
-       (let ((repl (twittering-make-emotions-string
-                    nil nil (match-string 1 str))))
-         (setq str (replace-match repl nil nil str)))))
+     (let ((last-match nil)
+           index
+           skip-fake-emotion)
+       (while (if skip-fake-emotion
+                  (progn
+                    (setq skip-fake-emotion nil)
+                    (setq index (string-match "\\(\\[[^][]+\\]\\)" str (1+ index))))
+                (setq index (string-match "\\(\\[[^][]+\\]\\)" str)))
+         (when (equal last-match (match-string 1 str))
+           (setq skip-fake-emotion t))
+         (setq last-match (match-string 1 str))
+
+         (let ((repl (twittering-make-emotions-string nil nil last-match)))
+           (setq str (replace-match repl nil nil str))))))
 
     ((douban)
      (setq str (twittering-make-string-with-uri-property str))))
