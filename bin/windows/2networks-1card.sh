51793bbc4bf9b8a5d1f579485a605126daa4aebf
Bao Haojun <baohaojun@gmail.com>
2012-01-10 17:12:21 +0800

    mf

--- bin/windows/2networks-1card.sh
+++ bin/windows/2networks-1card.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 netsh interface ip set address name=eth0 source=dhcp
-internal_ip=$(ifconfig  eth0|grep v4|perl -npe 's/.*: //')
+internal_ip=$(ifconfig  eth0|grep 'ip.*addr' -i|perl -npe 's/.*: //')
 external_ip=192.168.33.$(ifconfig /all |md5sum.exe |pn 1|perl -npe 's/^/((x=0x/; s/$/%255)); if test \$x -lt 0; then ((x=-x)); fi; echo \$x/'|bash)
 netsh interface ip add address "eth0" $internal_ip 255.255.255.0
-netsh interface ip add address "eth0" $external_ip 255.255.255.0 192.168.33.1
+netsh interface ip add address "eth0" $external_ip 255.255.255.0 
+route add 0.0.0.0 mask 0.0.0.0 192.168.33.1
 netsh interface ip set dns name=eth0 source=static addr=192.168.18.1 register=primary
