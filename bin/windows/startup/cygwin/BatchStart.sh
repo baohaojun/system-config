fa3b8c775ef20694d5a5c3fbe648381859892c8c
Bao Haojun <baohaojun@gmail.com>
2012-01-16 21:31:18 +0800

    mf

--- bin/windows/startup/cygwin/BatchStart.sh
+++ bin/windows/startup/cygwin/BatchStart.sh
@@ -39,7 +39,7 @@ if test -e ~/.offlineimaprc; then
     loop-start ~/bin/windows/Imap4Monitor /c/Python25/python.exe Imap4Monitor.py
 fi
 loop-start ~/bin/windows/notification-daemon/ /c/Python25/python.exe notification-daemon.py
-pageant $(wlp ~/.ssh/putty_rsa.ppk)&
+run pageant $(wlp ~/.ssh/putty_rsa.ppk)&
 close-window '\\osk.exe'
 ~/bin/windows/redirect.sh&
 
