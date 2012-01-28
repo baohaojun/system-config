1fe1d577ca2520b2a482997769fb5401c332b365
Bao Haojun <baohaojun@gmail.com>
2012-01-28 10:04:59 +0800

    Merge branch 'from-mac/00-21-cc-6c-b2-73-bj-lt7-Linux' of github.com:baohaojun/windows-config

--- bin/windows/2networks-1card.sh
+++ bin/windows/2networks-1card.sh
@@ -3,16 +3,7 @@ set -x
 
 ip_b4=$(from-mac|cut -b 21-37 |md5sum |pn 1|perl -npe 's/^/((x=0x/; s/$/%100)); if test \$x -lt 0; then ((x=-x)); fi; echo \$x/'|bash)
 
-if test $(hostname) = BJ-LT7; then
-    netsh interface ip set address name=eth0 source=dhcp
-    while true; do
-	internal_ip=$(ifconfig  eth0|grep 'ip.*addr' -i|perl -npe 's/.*: //')
-	test -z "$internal_ip" || break
-	sleep 1;
-    done
-else
-    internal_ip=10.21.128.$ip_b4
-fi
+internal_ip=10.21.128.$ip_b4
 external_ip=192.168.33.$ip_b4
 netsh interface ip add address "eth0" $internal_ip 255.255.255.0
 netsh interface ip add address "eth0" $external_ip 255.255.255.0 
@@ -21,6 +12,7 @@ while true; do
     ifconfig |grep $external_ip && break;
     sleep 1
 done
-route add 0.0.0.0 mask 0.0.0.0 192.168.33.1
+route delete 0.0.0.0
+route add -p 0.0.0.0 mask 0.0.0.0 192.168.33.1
 
 netsh interface ip set dns name=eth0 source=static addr=192.168.18.1 register=primary
