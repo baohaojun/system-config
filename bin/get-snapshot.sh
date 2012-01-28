bf6dd2facaa8ab8cb82354dc061e05665927f3f5
Bao Haojun <baohaojun@gmail.com>
2012-01-28 18:11:24 +0800

    mf

--- bin/get-snapshot.sh
+++ bin/get-snapshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd ${1:-~/user/Documents/My\ Pictures}
-find . -type f -mtime -.1 -print0|xargs -0 bash -c 'last=$1; shift; for x in "$@"; do if test "$last" -ot "$x"; then last=$x; fi; done; np 2>/dev/null "$last"' xx
+find . -type f -mtime -.1 -print0|xargs -0 bash -c 'last=$1; shift; for x in "$@"; do if test "$last" -ot "$x"; then last=$x; fi; done; mp 2>/dev/null "$last"' xx
