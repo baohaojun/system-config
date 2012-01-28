98ee2fb459844880e726bad97121c79d4e93d071
Bao Haojun <baohaojun@gmail.com>
2012-01-28 17:48:46 +0800

    mf

--- bin/windows/get-snapshot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-cd ~/user/Documents/My\ Pictures
-find . -type f -mtime -.1 -print0|xargs -0 bash -c 'last=$1; shift; for x in "$@"; do if test "$last" -ot "$x"; then last=$x; fi; done; wp 2>/dev/null "$last"' xx
