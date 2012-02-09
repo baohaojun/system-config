287f0840320e2930d424d98b1d705ae3585fe0be
Bao Haojun <baohaojun@gmail.com>
2012-02-09 15:44:24 +0800

    will run: 'emacsclient' '/tmp/get-stdin.17342' Waiting for Emacs... Improve notification.

--- bin/local-wiki.sh
+++ bin/local-wiki.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+test -e ~/external/bin/linux/ext/wiki-"$1".txt || set -- en "$@"
 lang=$1
 shift
 firefox 'http://localhost:8000/'$lang'/keyword/'"`echo \"$@\"|perl -npe 'chomp; s#([^_0-9a-zA-Z ])#sprintf(\"%%%02x\", ord($1))#seg; '`" &
