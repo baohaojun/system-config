6e527673d4f02c33acc7b8353cd67db43664bdce
Bao Haojun <baohaojun@gmail.com>
2012-02-09 14:52:00 +0800

    will run: 'emacsclient' '/tmp/get-stdin.13279' Waiting for Emacs... Fix notification to use my notify manager.

--- bin/windows/15-00.sh
+++ bin/windows/15-00.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bhj-notify "daily" "Check if you have any unfinished things to do before go off work"
+bhj-notify "Prepare go off work" "Check if you have any unfinished things to do before go off work"
 
 if test $(dayofweek) = Fri; then
     do-weekly < /dev/null
