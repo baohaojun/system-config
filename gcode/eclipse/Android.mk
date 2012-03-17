bfc85e05d35b869ef388a6450473cc754055ea56
Bao Haojun <baohaojun@gmail.com>
2012-03-17 21:39:21 +0800

    Do not use lcd in android mm, it is dependent on ~/.bashrc. Do not deal with symlink in lookup_file, need find out why I did it in the first place. Take more careful git commit comment next time, write down the details of the use case.

---
+++ gcode/eclipse/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := CrossDictionary
+
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_PACKAGE)
