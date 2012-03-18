808be23106340b675239e6d8c9382aa588b1ead6
Bao Haojun <baohaojun@gmail.com>
2012-03-18 10:31:16 +0800

    Use webview for the eclipse cross dictionary project.

--- gcode/eclipse/src/com/baohaojun/crossdict/CrossDictionaryActivity.java
+++ gcode/eclipse/src/com/baohaojun/crossdict/CrossDictionaryActivity.java
@@ -2,12 +2,18 @@ package com.baohaojun.crossdict;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class CrossDictionaryActivity extends Activity {
     /** Called when the activity is first created. */
+    WebView mWebView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	mWebView = (WebView) findViewById(R.id.webview);
+	mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadData("<a href='x'>Hello World! - 1</a>", "text/html", null);
     }
 }
