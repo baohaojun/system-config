1d5e471cf4b8daa196b6082c9a67b8eca1388244
Bao Haojun <baohaojun@gmail.com>
2012-04-25 20:27:49 +0800

    Make galaxy nexus rooting more straight forward.

---
+++ doc/superboot.sh
@@ -0,0 +1,8 @@
+mount -o rw,remount -t ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/system /system
+rm /system/bin/su
+rm /system/xbin/su
+mkdir /system/xbin
+cat /superboot/su>/system/xbin/su
+chmod 6755 /system/xbin/su
+cat /superboot/Superuser.apk>/system/app/Superuser.apk
+mount -o ro,remount -t ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/system /system
---
+++ gcode/android-start-debian/HelloAndroid/src/com/bhj/boot/HelloAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.bhj.boot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
