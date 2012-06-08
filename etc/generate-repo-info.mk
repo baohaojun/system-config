7fc12298d0a5da60312cce419b88a4fdbf83493e
Bao Haojun <baohaojun@gmail.com>
2012-06-08 14:26:57 +0800

    Update my-rfa, which is a parallel 'repo forall'.

--- etc/generate-repo-info.mk
+++ etc/generate-repo-info.mk
@@ -1,5 +1,5 @@
 repo_info.pl : manifest.xml $(shell which print-repo-info-as-perl) $(MAKEFILE_LIST)
 	echo '#!/usr/bin/perl' > $@
-	rfa print-repo-info-as-perl >> $@
-	echo 'foreach (@ARGV) { print $$repo_info{$$ENV{"PWD"}}{$$_} . "\n"; }' >> $@
+	repo forall -c print-repo-info-as-perl >> $@
+	echo 'foreach (@ARGV) { print $$repo_info{$$ENV{"PWD"}}{$$_} . "\\n"; }' >> $@
 	chmod +x $@
