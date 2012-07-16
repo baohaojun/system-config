aef08814a70e3d1a32cec5ca3c076d196c6565e5
Bao Haojun <baohaojun@gmail.com>
2012-07-16 11:22:22 +0800

    Android build script.

--- etc/generate-repo-info.mk
+++ etc/generate-repo-info.mk
@@ -1,5 +1,8 @@
 repo_info.pl : manifest.xml $(shell which print-repo-info-as-perl) $(MAKEFILE_LIST)
 	echo '#!/usr/bin/perl' > $@
+	echo 'chomp(my $$realpath = qx(readlink -f $$0));' >> $@
+	echo 'my $$android_path = $$realpath;' >> $@
+	echo '$$android_path =~ s,/.repo/repo_info.pl,,;' >> $@
 	repo forall -c print-repo-info-as-perl >> $@
 	echo 'foreach (@ARGV) { print $$repo_info{$$ENV{"PWD"}}{$$_} . "\\n"; }' >> $@
 	chmod +x $@
