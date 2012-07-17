1d1f2a79f20770dccdb2234e95f4f2bae8e5a50c
Bao Haojun <baohaojun@gmail.com>
2012-07-17 15:44:12 +0800

    Build android in an unshared jail. So that more ccache cache can be shared.

--- etc/generate-repo-info.mk
+++ etc/generate-repo-info.mk
@@ -1,4 +1,4 @@
-repo_info.pl : manifest.xml $(shell which print-repo-info-as-perl) $(MAKEFILE_LIST)
+repo_info.pl : manifest.xml $(shell which print-repo-info-as-perl) $(MAKEFILE_LIST) $(shell if test -e local_manifest.xml; then echo local_manifest.xml; fi)
 	echo '#!/usr/bin/perl' > $@
 	echo '$$android_path = $$0;' >> $@
 	echo '$$android_path =~ s,/.repo/repo_info.pl,,;' >> $@
