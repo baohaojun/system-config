2e824d4c62ffcd4bd2126c67176c18701bd4e64d
Bao Haojun <baohaojun@gmail.com>
2012-08-10 17:52:43 +0800

    Add logcat

--- etc/generate-repo-info.mk
+++ etc/generate-repo-info.mk
@@ -1,6 +1,3 @@
 repo_info.pl : manifest.xml $(shell which print-repo-info-as-perl-all) $(MAKEFILE_LIST) $(shell if test -e local_manifest.xml; then echo local_manifest.xml; fi)
-	echo '#!/usr/bin/perl' > $@
-	echo '$$android_path = $$0;' >> $@
-	echo '$$android_path =~ s,/.repo/repo_info.pl,,;' >> $@
 	print-repo-info-as-perl-all >> $@
 	chmod +x $@
