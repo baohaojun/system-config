dc8ef5f186c1960c330ec3ab394dd5fa6be960bb
Bao Haojun <baohaojun@gmail.com>
2012-08-13 11:36:33 +0800

    Fix a typo.

--- etc/generate-repo-info.mk
+++ etc/generate-repo-info.mk
@@ -1,3 +1,3 @@
 repo_info.pl : manifest.xml $(shell which print-repo-info-as-perl-all) $(MAKEFILE_LIST) $(shell if test -e local_manifest.xml; then echo local_manifest.xml; fi)
-	print-repo-info-as-perl-all >> $@
+	print-repo-info-as-perl-all > $@
 	chmod +x $@
