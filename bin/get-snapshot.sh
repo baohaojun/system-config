43fc443646b93dac50b6b6d0e3fca6352354e910
Bao Haojun <baohaojun@gmail.com>
2012-08-29 09:13:22 +0800

    Fix get newest file using "ls -t".

--- bin/get-snapshot.sh
+++ bin/get-snapshot.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
-if test -e ~/user/Documents/My\ Pictures; then
-    dir=~/user/Documents/My\ Pictures
-else
-    dir=~/shots
-fi
-cd ${1:-$dir}
-find . -type f -mtime -.1 -print0|xargs -0 bash -c '
-last=$1;
-shift;
-for x in "$@"; do
-    if test "$last" -ot "$x"; then
-        last=$x;
-    fi;
-done;
+cd ${1:-.}
 
-readlink -m "$last"
-mp >/dev/null 2>&1 "$last"' xx
+ignore_pat='\.~.*#$'
+newest=$(ls -t -A | grep -v -e "$ignore_pat" | head -n 1)
+readlink -m "$newest"
+mp >/dev/null 2>&1 "$newest" </dev/null
