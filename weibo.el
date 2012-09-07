b7606ffe13f2f39b9044f2ff3a641d8b95bfc554
Austin <austiny.cn@gmail.com>
2012-09-07 00:06:14 -0400

    fix defstruct definition

--- weibo.el
+++ weibo.el
@@ -13,6 +13,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(eval-when-compile (require 'cl))
 (require 'url)
 (require 'json)
 (require 'weibo-authorize)
