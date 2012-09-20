213aaa45985e714dd1938bf41f772f98a0085e7b
Bao Haojun <hjbao@marvell.com>
2012-09-20 19:11:05 +0800

    Add filter for text/x-markdown

--- Filters/FilterText.cs
+++ Filters/FilterText.cs
@@ -83,6 +83,7 @@ namespace Beagrep.Filters {
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("text/x-troff"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("text/x-troff-man"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("text/x-xslt"));
+			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("text/x-markdown"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-shellscript"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-yaml"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/xslt+xml"));
