bf8800dec86f49c891a93f954d03b76838a48d00
Bao Haojun <baohaojun@gmail.com>
2012-10-12 10:16:18 +0800

    refine the android full build script.

--- bin/fix-mk.pl
+++ bin/fix-mk.pl
@@ -2,9 +2,12 @@
 use strict;
 my @lines;
 my $line;
+my $cross_compile_export = 'export CROSS_COMPILE:=$(CCACHE_COMMAND) $(KERNEL_TOOLCHAIN_PREFIX)';
 while (<>) {
-
-    s/ CROSS_COMPILE=\$(\(|\{)CROSS_COMPILE[^({})]*(\)|\})//;
+    s/^\s*\Qexport CROSS_COMPILE:=$(KERNEL_TOOLCHAIN_PREFIX)\E/$cross_compile_export/;
+    if (m/make.*cross_compile.*=/i) {
+	s/ CROSS_COMPILE\s*:?=\s*\$(\(|\{)(CROSS_COMPILE|KERNEL_TOOLCHAIN_PREFIX)[^({})]*(\)|\})//;
+    }
     s/ -j\$\(MAKE_JOBS\)//;
     if ($line) {
 	$line = $line . $_;
