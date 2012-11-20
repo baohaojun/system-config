d27b6864156cf2c4e01663a807485a7b6868e407
Bao Haojun <baohaojun@gmail.com>
2012-11-20 17:27:02 +0800

    Add a script for ease gerrit merging according to Change-ID.

--- gcode/offline.wikipedia/mediawiki_sa/resources/mediawiki/mediawiki.specials.search.js
+++
@@ -1,8 +0,0 @@
-/*
- * JavaScript for Specical:Search
- */
-
-// Emulate HTML5 autofocus behavior in non HTML5 compliant browsers
-if ( !( 'autofocus' in document.createElement( 'input' ) ) ) {
-	$( 'input[autofocus]' ).focus();
-}
