05a4d525999ca60e3038833b860c7b8259266723
Bao Haojun <baohaojun@gmail.com>
2012-11-27 19:45:57 +0800

    Fix marvell scripts, the default (shortest) command is always the latest one. Add a java template file, start to learn java programming.

---
+++ doc/Test.java
@@ -0,0 +1,19 @@
+class Test {
+    public static void main(String []args) {
+	System.out.println("hello world");
+	Test x = new Test2();
+	x.hello(x);
+    }
+    void hello(Test x) {
+	System.out.println("hello of Test");
+    }
+}
+
+class Test2 extends Test {
+    void hello(Object x) {
+	System.out.println("hello of Test2");
+    }
+    void hello(Test x) {
+	System.out.println("hello of Test2");
+    }
+}
