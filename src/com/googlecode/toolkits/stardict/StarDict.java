37fa50d45b1c2b87002c9e11b8fbada137d89b5a
Bao Haojun <hjbao@marvell.com>
2012-12-26 10:04:25 +0800

    Add a script for building ant.
    Use expect to support signing android apk using keystore.

--- src/com/googlecode/toolkits/stardict/StarDict.java
+++ src/com/googlecode/toolkits/stardict/StarDict.java
@@ -33,91 +33,86 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-class Location {
-    public int offset;
-    public int size;
-}
+public class StarDict {
 
-public class StarDict {  
+    static class IndexEntry {
+	public String mName;
+	public ArrayList<Pair<Integer, Integer>> start_ends;
+
+	public IndexEntry(String name) {
+	    mName = name;
+	}
+
+	public void add(int start, int end) {
+	    start_ends.add(new Pair(start, end));
+	}
+    }
+
+    static int packI(byte[] buffer, int offset) {
+	return 
+	    ((0XFF & buffer[offset]) << 24) +
+	    ((0XFF & buffer[offset+1]) << 16) +
+	    ((0XFF & buffer[offset+2]) << 8) +
+	    ((0XFF & buffer[offset+3]) << 0);
+    }	    
+	
     /**/
     RandomAccessFile index;
-    RandomAccessFile yaindex;
-    DictZipFile dz;
-    String dictname;
-    public String last_error = "";
+    RandomAccessFile dict;
+
+    HashMap<String, ArrayList<Pair<Integer, Integer>>> entries_map = new HashMap<String, ArrayList<Pair<Integer, Integer>>>();    
   
     /**
      * 
      * @param dictname
      */
+    void die(String s) {
+	System.out.println(s);
+	System.exit(-1);
+    }
+
     public StarDict(String dictname) {
 	try {
-	    this.dictname = dictname;
+	    int length = (int) new File(dictname+".idx").length();
+
 	    this.index = new RandomAccessFile(dictname+".idx", "r");
-	    this.dz = new DictZipFile(dictname+".dict.dz");
-	    this.yaindex = new RandomAccessFile(dictname+".yaidx", "r");
-	    //this.dz.runtest();
+	    this.dict = new RandomAccessFile(dictname+".dz", "r");
+
+	    byte[] buffer = new byte[length];
+	    index.readFully(buffer, 0, length);
+
+	    int start = 0;
+	    for (int l = 0; l < length; l++) {
+		if (buffer[l] == 0) {
+		    String s = new String(buffer, start, l - start, "UTF8");
+		    int n = buffer[l + 1];
+		    ArrayList<Pair<Integer, Integer>> start_ends = new ArrayList<Pair<Integer, Integer>>(n);
+		    for (int e = 0; e < n; e++) {
+			int b1 = l + 2 + e * 8;
+			int b2 = l + 2 + e * 8 + 4;
+
+			int e_start = packI(buffer, b1);
+			int e_end = packI(buffer, b2);
+
+			start_ends.add(new Pair(e_start, e_end));
+		    }
+		    l++;
+		    l += 8 * n;
+		    start = l + 1;
+		    entries_map.put(s, start_ends);
+		}
+	    }		
 	}
 	catch(FileNotFoundException e) {
-	    last_error = e.toString();
 	    e.printStackTrace();
 	}
 	catch(Exception e) {
-	    last_error = e.toString();
 	    e.printStackTrace();
 	}
-    }
-  
-    public String getWord(int p, Location l) {
-	if(l==null) {
-	    l = new Location();
-	}
-	String word = null;
-	byte [] buffer = new byte[1024];
-	int dataoffset = 0;
-	int datasize = 0;
-	int offset = 0; // the offset of the p-th word in this.index
-	try {
-	    this.yaindex.seek(p*4);
-	    int size = this.yaindex.read(buffer, 0, 4);
-	    if (size!=4) {
-		throw new Exception("Read Index Error");
-	    }
-	    for(int i=0;i<4;i++) {
-		offset<<=8;
-		offset|=buffer[i]&0xff;
-	    }
-	    this.index.seek(offset);
-	    size = this.index.read(buffer, 0, 1024);
-	    for(int i=0;i<size;i++) {
-		if (buffer[i]==0) {
-		    word = new String(buffer, 0, i, "UTF8");
-		    dataoffset = 0;
-		    datasize = 0;
-		    for (int j=i+1;j<i+5;j++) {
-			dataoffset<<=8;
-			dataoffset|=buffer[j]&0xff;
-		    }
-		    for (int j=i+5;j<i+9;j++) {
-			datasize<<=8;
-			datasize|=buffer[j]&0xff;
-		    }
-		    break;
-		}
-	    }
-	    //System.out.println(datasize);
-	    //buffer = new byte[datasize];
-	    //this.dz.seek(dataoffset);
-	    //this.dz.read(buffer, datasize);
-	    l.offset = dataoffset;
-	    l.size = datasize;
-	}
-	catch(Exception e) {
-	    last_error = e.toString();
-	    e.printStackTrace();
-	}
-	return word;
     }
   
     /**
@@ -125,106 +120,89 @@ public class StarDict {
      * @param word
      * @return the explanation of the word
      */
-    public String getExplanation(String word) {
-	int i = 0;
-	int max = getWordNum();
-	String w = "";
-	int mid = 0;
-	Location l = new Location();
-	String exp = null;
-	//return this.dz.test()+this.dz.last_error;
-	///*
-	while( i<=max ) {
-	    mid = (i + max)/2;
-	    w = getWord(mid, l);
-	    if (w.compareTo(word)>0) {
-		max = mid-1;
-	    }
-	    else if(w.compareTo(word)<0) {
-		i = mid+1;
-	    } 
-	    else {
-		break;
-	    }
-	}
-	//get explanation
-	byte [] buffer = new byte[l.size];
-	this.dz.seek(l.offset);
-	try {
-	    this.dz.read(buffer, l.size);
-	}
-	catch(Exception e) {
-	    last_error = e.toString();
-	    buffer = null;
-	    exp = e.toString();
-	}
-    
-	try {
-	    if (buffer == null) {
-		exp = "Error when reading data\n"+exp;
-	    }
-	    else {
-		exp = new String(buffer, "UTF8");
-	    }
-	}
-	catch(Exception e) {
-	    last_error = e.toString();
-	    e.printStackTrace();
-	}
-	return w+"\n"+exp;
-	//*/
-    }
-  
-    /**
-     * 
-     * @return
-     */
-    public String getVersion() {
-	try {
-	    BufferedReader br = new BufferedReader(new FileReader(this.dictname+".ifo"));
-	    String line = br.readLine();
-	    while(line != null) {
-		String [] version = line.split("=");
-		if (version.length == 2 && version[0].equals("version")) {
-		    return version[1];
-		}
-		line = br.readLine();
-	    }
-	}
-	catch(IOException e) {
-	    last_error = e.toString();
-	    e.printStackTrace();
-	}
-	return "UNKNOWN VERSION";
-    }
-  
-    public int getWordNum() {
-	try {
-	    BufferedReader br = new BufferedReader(new FileReader(this.dictname+".ifo"));
-	    String line = br.readLine();
-	    while(line != null) {
-		String [] version = line.split("=");
-		if (version.length == 2 && version[0].equals("wordcount")) {
-		    return Integer.parseInt(version[1]);
-		}
-		line = br.readLine();
-	    }
-	}
-	catch(IOException e) {
-	    last_error = e.toString();
-	    e.printStackTrace();
-	}
-	return 0;
-    }
+    public ArrayList<String> getExplanation(String word) {
+	
+	ArrayList<Pair<Integer, Integer>> start_ends = entries_map.get(word);
 
+	if (start_ends != null) {
+	    ArrayList<String> ret = new ArrayList<String>();
+	    for (Pair<Integer, Integer> p : start_ends) {
+		int start = (int) p.first;
+		int end = (int) p.second;
+
+		byte[] buffer = new byte[end - start];
+
+		try {
+		    dict.seek(start);
+		    dict.readFully(buffer, 0, buffer.length);
+		    ret.add(new String(buffer, "UTF8"));
+		    
+		} catch (Exception e) {
+		    System.out.printf("start for %s is %d, stop is %d\n", word, start, end);
+		    e.printStackTrace();
+		}
+	    }
+	    return ret;
+	}
+	return null;
+    }
   
     public static void main(String[] args) {
-	StarDict dict = new StarDict("/home/bhj/today/stardict.zip.unzip.24247/AHD4_2.8");
-	System.out.println(dict.getVersion());
-	Location l = new Location();
-	String w = dict.getWord(1000, l);
-	System.out.println(w);
-	System.out.println(dict.getExplanation(w));
-	System.out.println(dict.getExplanation("this"));
+	StarDict dict = new StarDict("/sdcard/ahd/ahd");
+	dict.getExplanation("hello");
+    }
+}
+
+class Pair<F, S> {
+    public final F first;
+    public final S second;
+
+    /**
+     * Constructor for a Pair. If either are null then equals() and hashCode() will throw
+     * a NullPointerException.
+     * @param first the first object in the Pair
+     * @param second the second object in the pair
+     */
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Checks the two objects for equality by delegating to their respective equals() methods.
+     * @param o the Pair to which this one is to be checked for equality
+     * @return true if the underlying objects of the Pair are both considered equals()
+     */
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Pair)) return false;
+        final Pair<F, S> other;
+        try {
+            other = (Pair<F, S>) o;
+        } catch (ClassCastException e) {
+            return false;
+        }
+        return first.equals(other.first) && second.equals(other.second);
+    }
+
+    /**
+     * Compute a hash code using the hash codes of the underlying objects
+     * @return a hashcode of the Pair
+     */
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + first.hashCode();
+        result = 31 * result + second.hashCode();
+        return result;
+    }
+
+    /**
+     * Convenience method for creating an appropriately typed pair.
+     * @param a the first object in the Pair
+     * @param b the second object in the pair
+     * @return a Pair that is templatized with the types of a and b
+     */
+    public static <A, B> Pair <A, B> create(A a, B b) {
+        return new Pair<A, B>(a, b);
     }
 }
