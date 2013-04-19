2140ef4fe40bee3b185815ba145cb4977fe98b7a
Matus Goljer <dota.keys@gmail.com>
2013-04-19 15:08:02 +0200

    Added package header and licence

--- skeleton-complete.el
+++ skeleton-complete.el
@@ -1,5 +1,40 @@
-(provide 'skeleton-complete)
+;;; skeleton-complete.el --- Dynamically expand expressions by provided skeleton (flex matching)
+
+;; Copyright (C) 2013 Bao Haojun
+
+;; Author: Bao Haojun <baohaojun@gmail.com>
+;; Maintainer: Bao Haojun <baohaojun@gmail.com>
+;; Created: 15th April 2013
+;; Keywords: abbrev
+;; Version: 0.0.20130419
+;; URL: https://github.com/baohaojun/skeleton-complete
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; For more information see the readme at:
+;; https://github.com/baohaojun/skeleton-complete
+
+;;; Code:
+
 (require 'ecomplete)
+
 (global-set-key [(meta g)(return)] 'skeleton-display-abbrev)
 
 (defun skeleton-display-abbrev ()
@@ -295,3 +330,7 @@ words closer to the (point) appear first"
 			  (setq strlist (delete-dups (append (skeleton-get-matches-order skeleton t) strlist))))))
 		    (delete buf-old (buffer-list)))
 	    strlist))))))
+
+(provide 'skeleton-complete)
+
+;;; skeleton-complete.el ends here
