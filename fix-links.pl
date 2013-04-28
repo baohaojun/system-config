20d450ece4a2c0df2aa53271319285f860a4beeb
Bao Haojun <baohaojun@gmail.com>
2013-04-28 14:21:56 +0800

    Move images around to separate dir

--- fix-links.pl
+++ fix-links.pl
@@ -7,7 +7,9 @@ sub debug(@) {
     print STDERR "@_\n";
 }
 
+our $working_file_sq;
 our $working_file;
+our $working_file_dir;
 sub fix_link($)
 {
     my ($link) = @_;
@@ -19,6 +21,8 @@ sub fix_link($)
     } elsif ($link =~ m/^file:/) {
         $link =~ s!^file:!!;
         $file = $link;
+    } elsif ($link =~ m/:/) {
+        return $link;
     } else {
         $file = $link;
     }
@@ -31,21 +35,22 @@ sub fix_link($)
         $org_file =~ s/\.html$/.org/;
     }
 
-    debug "org_file is $org_file";
-    if ($org_file =~ m/\.org$/) {
-        $org_file = shell_quote($org_file);
-        chomp($org_file = qx(find blog -name $org_file));
 
-        if ($org_file =~ m/\n/) {
-            die "$link matched with multiple files";
-        }
+    $org_file = shell_quote($org_file);
+    chomp($org_file =
+          (qx(find blog -name $org_file) or
+           qx(find . -name $org_file)));
+
+    if ($org_file =~ m/\n/) {
+        die "$link matched with multiple files";
+    } elsif (-e $org_file) {
         $file = $org_file;
     }
 
     if (-e $file) {
         debug "file is $file";
         $file = shell_quote($file);
-        chomp ($file = qx(relative-path $file $working_file));
+        chomp ($file = qx(relative-path $file $working_file_sq));
         if ($file !~ m!/!) {
             $file = "./$file";
         }
@@ -60,7 +65,9 @@ for my $filename (@ARGV) {
     open (my $fh, "<", $filename) or die "Can not open $filename";
     open (my $fh_new, ">", "$filename.$$") or die "Can not open $filename.$$";
 
-    $working_file = shell_quote($filename);
+    $working_file = $filename;
+    ($working_file_dir = $filename) =~ s!(.*/).*!$1!;
+    $working_file_sq = shell_quote($filename);
     while (<$fh>) {
         s/\[\[(.*?)\]/"[[" . fix_link($1) . "]"/eg;
         print $fh_new $_;
