719b32fbe1792d07340db75dec202d92c557979c
Bao Haojun <baohaojun@gmail.com>
2013-05-20 17:58:36 +0800

    Redo the blog design.

--- js/post.js
+++ js/post.js
@@ -1,35 +1,4 @@
 $(document).ready(function(){
-
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        }
-        ,BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        }
-        ,iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        }
-        ,Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        }
-        ,Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        }
-        ,any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
-
-    $('pre').addClass('prettyprint linenums') //添加Google code Hight需要的class
-
-    window.disqus_shortname = 'baohaojun';
-    $('#disqus_container .disqus').on('click',function(){
-        $(this).html('loading...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
-
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
         if(href){
