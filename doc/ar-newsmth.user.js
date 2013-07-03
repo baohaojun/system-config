d77ac42a353252a8e281cdcda14ae81d4c581c6b
Bao Haojun <baohaojun@gmail.com>
2013-07-03 21:49:55 +0800

    Fix bbs-robot for same-thread reading
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ doc/ar-newsmth.user.js
@@ -0,0 +1,41 @@
+
+// Hello World! example user script
+// version 0.1 BETA!
+// 2005-04-25
+// Copyright (c) 2005, Mark Pilgrim
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+//
+// --------------------------------------------------------------------
+//
+// This is a Greasemonkey user script.  To install it, you need
+// Greasemonkey 0.3 or later: http://greasemonkey.mozdev.org/
+// Then restart Firefox and revisit this script.
+// Under Tools, there will be a new menu item to "Install User Script".
+// Accept the default configuration and install.
+//
+// To uninstall, go to Tools/Manage User Scripts,
+// select "Hello World", and click Uninstall.
+//
+// --------------------------------------------------------------------
+//
+// ==UserScript==
+// @name          Hello World
+// @namespace     http://diveintomark.org/projects/greasemonkey/
+// @description   example script to alert "Hello world!" on every page
+// @include       http://ar.newsmth.net/*
+// @exclude       http://diveintogreasemonkey.org/*
+// @exclude       http://www.diveintogreasemonkey.org/*
+// ==/UserScript==
+
+
+for (var x in document.images) {
+    var y = document.images[x].src;
+    if (! y)
+        continue;
+    if (! y.match(/\/att\//))
+        continue;
+    if (document.images[x].width > 800) {
+        document.images[x].width = 800;
+    }
+}
--- etc/mach/t420/sawfish.sh
+++ etc/mach/t420/sawfish.sh
@@ -2,5 +2,7 @@
 
 sawfish-client -e '(bind-keys global-keymap "Super-Down" (lambda () (system "~/bin/douban info&")))'
 sawfish-client -e '(bind-keys global-keymap "Super-Up" (lambda () (system "~/bin/douban lyrics&")))'
-sawfish-client -e '(bind-keys global-keymap "Super-Right" (lambda () (system "~/bin/douban next&")))'
-sawfish-client -e '(bind-keys global-keymap "Super-Left" (lambda () (system "~/bin/douban prev&")))'
+sawfish-client -e '(bind-keys global-keymap "Super-S-Right" (lambda () (system "~/bin/douban next&")))'
+sawfish-client -e '(bind-keys global-keymap "Super-S-Left" (lambda () (system "~/bin/douban prev&")))'
+sawfish-client -e '(bind-keys global-keymap "Super-Right" (lambda () (system "~/bin/douban back&")))'
+sawfish-client -e '(bind-keys global-keymap "Super-Left" (lambda () (system "~/bin/douban forth&")))'
