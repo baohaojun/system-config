0f176339722c01fdcf1cc596fa767ac1e367a2aa
Patrick von Reth <vonreth@kde.org>
2013-07-07 10:33:14 +0200

    relicensed

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 #include <QtGui/QApplication>
 
-#if HAVE_KDE
+#ifdef HAVE_KDE
 #include "core/version.h"
 #include <KAboutData>
 #include <KCmdLineArgs>
@@ -15,7 +15,7 @@ int main ( int argc, char *argv[] )
 
 #if HAVE_KDE
     KAboutData about("SnoreNotify",0,ki18n("SnoreNotify"),Snore::Version::version().toLatin1(),
-                     ki18n("A notification deamon."),KAboutData::License_GPL_V2, ki18n("Copyright (c) 2010-2013 Patrick von Reth <vonreth@kde.org>"));
+                     ki18n("A notification deamon."),KAboutData::License_LGPL_V3, ki18n("Copyright (c) 2010-2013 Patrick von Reth <vonreth@kde.org>"));
     KCmdLineArgs::init(argc, argv, &about);
     KUniqueApplication app;
 #else
