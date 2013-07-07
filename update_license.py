68621627e3bc5d300ae21b748f1e3abf72f8ec13
Patrick von Reth <vonreth@kde.org>
2013-07-07 12:38:39 +0200

    fixed copying info

--- update_license.py
+++ update_license.py
@@ -12,18 +12,18 @@ newLicense = """/*
     Copyright (C) 2013  Patrick von Reth <vonreth@kde.org>
 
 
-    VSD is free software: you can redistribute it and/or modify
+    SnoreNotify is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    VSD is distributed in the hope that it will be useful,
+    SnoreNotify is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with SnarlNetworkBridge.  If not, see <http://www.gnu.org/licenses/>.
+    along with SnoreNotify.  If not, see <http://www.gnu.org/licenses/>.
 */"""
  
 def myWalk(root):
