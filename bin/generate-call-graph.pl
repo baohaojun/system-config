bfbbbf8c7372ef1b9c89a5f5d69d789ef20f0e7d
Bao Haojun <baohaojun@gmail.com>
2013-07-25 16:45:26 +0800

    Move ajoke out of system-config
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/generate-call-graph.pl
+++ bin/generate-call-graph.pl
@@ -9,14 +9,14 @@ if (not -e $globalrc) {
 }
 
 my $langmap = shell_quote(qx(lang-map-for-ctags $globalrc));
-open(my $ctags_pipe, "-|", "mkcscope_files | ctags-exuberant --langmap=$langmap -xu --filter --filter-terminator='###terminator###\n' --format=2")
+open(my $ctags_pipe, "-|", "mkcscope_files | ctags-ajoke --langmap=$langmap -xu --filter --filter-terminator='###terminator###\n' --format=2")
     or die "Can not create ctags program";
 
 my ($last_ctags_output, $last_ctags_def, $last_ctags_type, $last_ctags_linum, $last_ctags_file,
     $this_ctags_output, $this_ctags_def, $this_ctags_type, $this_ctags_linum, $this_ctags_file,
     $current_file_handle, $current_file_line, %def_contains_map,
     );
-    
+
 sub debug(@) {
     print STDERR "@_\n";
 }
@@ -25,7 +25,7 @@ my $lang_token_regexp = qr/[_0-9a-z]+/i;
 
 sub def_contains($) {
     my ($word) = @_;
-    $def_contains_map{$last_ctags_def} = {} 
+    $def_contains_map{$last_ctags_def} = {}
     unless exists $def_contains_map{$last_ctags_def};
 
     $def_contains_map{$last_ctags_def}{"$last_ctags_file:$last_ctags_linum:$last_ctags_type"} = {}
@@ -35,9 +35,9 @@ sub def_contains($) {
     unless exists $def_contains_map{$last_ctags_def}{"$last_ctags_file:$last_ctags_linum:$last_ctags_type"}{$current_file_line};
 
     $def_contains_map{$last_ctags_def}{"$last_ctags_file:$last_ctags_linum:$last_ctags_type"}{$current_file_line}{$word} = 1;
-		 }
+                 }
 
-open(my $call_graph, ">", "call_graph.dot") or 
+open(my $call_graph, ">", "call_graph.dot") or
     die "Can not open call_graph.dot";
 sub write_graph(@) {
     print $call_graph @_;
@@ -48,45 +48,45 @@ write_graph("digraph {\ngraph [ ratio=.5 ];\n");
 while (<$ctags_pipe>) {
     chomp;
     if (m/^###terminator###$/) {
-	while ($current_file_handle and my $line = <$current_file_handle>) {
-	    $current_file_line++;
-	    while ($line =~ m/$lang_token_regexp/g) {
-		def_contains($&);
-	    }
-	}
-	close($current_file_handle) if $current_file_handle;
-	undef $current_file_handle;
-	undef $last_ctags_def;
-	next;
+        while ($current_file_handle and my $line = <$current_file_handle>) {
+            $current_file_line++;
+            while ($line =~ m/$lang_token_regexp/g) {
+                def_contains($&);
+            }
+        }
+        close($current_file_handle) if $current_file_handle;
+        undef $current_file_handle;
+        undef $last_ctags_def;
+        next;
     }
 
     ($this_ctags_def, $this_ctags_type, $this_ctags_linum, $this_ctags_file,) = split;
     unless ($this_ctags_linum =~ m/^\d+$/ and -e $this_ctags_file and length($this_ctags_def) > 1 and $this_ctags_def =~ m/$lang_token_regexp/) {
-	debug "file $this_ctags_file, line $this_ctags_linum";
-	next;
+        debug "file $this_ctags_file, line $this_ctags_linum";
+        next;
     }
     my $type = $last_ctags_type;
     my $do_record = 0;
     if ($type eq "function" or $type eq "method" or $type eq "class") {
-	$do_record = 1;
+        $do_record = 1;
     }
     unless ($current_file_handle) {
-	open($current_file_handle, "<", $this_ctags_file);
-	$current_file_line = 0;
+        open($current_file_handle, "<", $this_ctags_file);
+        $current_file_line = 0;
     }
     if ($last_ctags_def) {
-	while($current_file_line + 1 < $this_ctags_linum and my $line = <$current_file_handle>) {
-	    $current_file_line++;
-	    next unless $do_record;
-	    chomp($line);
-	    while ($line =~ m/$lang_token_regexp/g) {
-		def_contains($&);
-	    }
-	}
+        while($current_file_line + 1 < $this_ctags_linum and my $line = <$current_file_handle>) {
+            $current_file_line++;
+            next unless $do_record;
+            chomp($line);
+            while ($line =~ m/$lang_token_regexp/g) {
+                def_contains($&);
+            }
+        }
     }
 
-    ($last_ctags_def, $last_ctags_type, $last_ctags_linum, $last_ctags_file,) = 
-	($this_ctags_def, $this_ctags_type, $this_ctags_linum, $this_ctags_file,);
+    ($last_ctags_def, $last_ctags_type, $last_ctags_linum, $last_ctags_file,) =
+        ($this_ctags_def, $this_ctags_type, $this_ctags_linum, $this_ctags_file,);
 
 }
 
@@ -94,68 +94,68 @@ my %graph_map;
 my (%calls_map, %called_by_map, %label_map);
 foreach my $def (sort keys %def_contains_map) {
     foreach my $def_info (sort keys %{$def_contains_map{$def}}) {
-	$def_info =~ m/:\d+:/ or die "def_info format incorrect";
-	my ($file, $line, $type) = ($`, $&, $'); #');
+        $def_info =~ m/:\d+:/ or die "def_info format incorrect";
+        my ($file, $line, $type) = ($`, $&, $'); #');
 
-	my $label = "\"$def\\n$type on $file$line\"";
-	unless (exists $label_map{$label}) {
-	    $label_map{$label} = "l" . ((keys %label_map) + 0);
-	    # write_graph("$label_map{$label} [label=$label]\n");
-	}
-	    
-	#write_graph("\"$def\" -> $label_map{$label}\n");
-				    
-	foreach my $line (sort keys %{$def_contains_map{$def}{$def_info}}) {
-	    foreach my $word (sort keys %{$def_contains_map{$def}{$def_info}{$line}}) {
-		if (exists $def_contains_map{$word} and $word ne $def) {
-		    unless (exists $called_by_map{$word}) {
-			$called_by_map{$word} = {};
-		    }
-		    $called_by_map{$word}{"$file:$line"} = "$def: $def_info";
+        my $label = "\"$def\\n$type on $file$line\"";
+        unless (exists $label_map{$label}) {
+            $label_map{$label} = "l" . ((keys %label_map) + 0);
+            # write_graph("$label_map{$label} [label=$label]\n");
+        }
 
-		    $graph_map{$def} = {} unless exists $graph_map{$def};
-		    unless (exists $graph_map{$def}{$word}) {
-			$graph_map{$def}{$word} = 1;
-			foreach my $info (keys %{$def_contains_map{$word}}) {
-			    if ($info =~ m/method|function/) {
-				write_graph("\"$def\" -> \"$word\"\n");
-				last;
-			    }
-			}
-		    }
-			
+        #write_graph("\"$def\" -> $label_map{$label}\n");
+
+        foreach my $line (sort keys %{$def_contains_map{$def}{$def_info}}) {
+            foreach my $word (sort keys %{$def_contains_map{$def}{$def_info}{$line}}) {
+                if (exists $def_contains_map{$word} and $word ne $def) {
+                    unless (exists $called_by_map{$word}) {
+                        $called_by_map{$word} = {};
+                    }
+                    $called_by_map{$word}{"$file:$line"} = "$def: $def_info";
+
+                    $graph_map{$def} = {} unless exists $graph_map{$def};
+                    unless (exists $graph_map{$def}{$word}) {
+                        $graph_map{$def}{$word} = 1;
+                        foreach my $info (keys %{$def_contains_map{$word}}) {
+                            if ($info =~ m/method|function/) {
+                                write_graph("\"$def\" -> \"$word\"\n");
+                                last;
+                            }
+                        }
+                    }
 
 
-		    $calls_map{def} = {} unless exists $calls_map{$def};
-		    $calls_map{$def}{$def_info} = {} unless exists $calls_map{$def}{$def_info};
-		    $calls_map{$def}{$def_info}{$line} = {} unless exists $calls_map{$def}{$def_info}{$line};
-		    $calls_map{$def}{$def_info}{$line}{$word} = 1;									    
-		}
-	    }
-	}
+
+                    $calls_map{def} = {} unless exists $calls_map{$def};
+                    $calls_map{$def}{$def_info} = {} unless exists $calls_map{$def}{$def_info};
+                    $calls_map{$def}{$def_info}{$line} = {} unless exists $calls_map{$def}{$def_info}{$line};
+                    $calls_map{$def}{$def_info}{$line}{$word} = 1;
+                }
+            }
+        }
     }
 }
 
 foreach my $def (sort keys %def_contains_map) {
-    
+
     print "* $def\n\n";
     foreach my $def_info (sort keys %{$def_contains_map{$def}}) {
-	print "** is defined on\n";
-	print "*** $def_info\n";
-	$def_info =~ m/:\d+:/;
-	my $file = $`;
-	foreach my $line (sort keys %{$calls_map{$def}{$def_info}}) {
-	    foreach my $word (sort keys %{$calls_map{$def}{$def_info}{$line}}) {
-		print "- calls $word on $file:$line\n";
-	    }
-	}
+        print "** is defined on\n";
+        print "*** $def_info\n";
+        $def_info =~ m/:\d+:/;
+        my $file = $`;
+        foreach my $line (sort keys %{$calls_map{$def}{$def_info}}) {
+            foreach my $word (sort keys %{$calls_map{$def}{$def_info}{$line}}) {
+                print "- calls $word on $file:$line\n";
+            }
+        }
     }
-    
+
     print "\n** is called by\n";
     foreach my $file_lino (sort keys %{$called_by_map{$def}}) {
-	print "- on $file_lino by $called_by_map{$def}{$file_lino}\n";
+        print "- on $file_lino by $called_by_map{$def}{$file_lino}\n";
     }
     print "\n";
 }
-	
+
 write_graph("}\n");
--- bin/windows/download-external.sh
+++ bin/windows/download-external.sh
@@ -15,7 +15,7 @@ function get-python() {
     cd ~/external/python
     for x in  ~/etc/python-win/vers/*;
     do
-	x=$(basename $x)
+        x=$(basename $x)
         eval "wget -N http://python.org/ftp/python/$x/python-$x.msi&"
     done
     wget -N http://sourceforge.net/projects/pywin32/files/pywin32/Build216/pywin32-216.win32-py2.7.exe&
@@ -23,7 +23,7 @@ function get-python() {
 
     for x in  ~/etc/python-win/vers/*;
     do
-	x=$(basename $x)
+        x=$(basename $x)
         chmod +x python-$x.msi
         cygstart python-$x.msi
     done
@@ -38,19 +38,19 @@ function get-python() {
 function download-all()
 {
     (
-	mkdir -p ~/external/download-all
-	cd ~/external/download-all
+        mkdir -p ~/external/download-all
+        cd ~/external/download-all
 
 
 
-	file_list=(
-	    $(
-		for x in putty.exe puttytel.exe pscp.exe psftp.exe plink.exe pageant.exe puttygen.exe; do
-		    echo http://the.earth.li/~sgtatham/putty/latest/x86/$x;
-		done
-	    )
-	    http://mirrors.zerg.biz/stunnel/stunnel-4.50-installer.exe
-	    http://www.imagemagick.org/download/binaries/ImageMagick-6.7.4-Q16-windows.zip
+        file_list=(
+            $(
+                for x in putty.exe puttytel.exe pscp.exe psftp.exe plink.exe pageant.exe puttygen.exe; do
+                    echo http://the.earth.li/~sgtatham/putty/latest/x86/$x;
+                done
+            )
+            http://mirrors.zerg.biz/stunnel/stunnel-4.50-installer.exe
+            http://www.imagemagick.org/download/binaries/ImageMagick-6.7.4-Q16-windows.zip
             http://download.sysinternals.com/Files/ProcessMonitor.zip
             http://download.sysinternals.com/Files/ProcessExplorer.zip
             http://download.sysinternals.com/Files/PsTools.zip
@@ -60,26 +60,26 @@ function download-all()
             http://www.nirsoft.net/utils/iconsext.zip
             http://www.winterdrache.de/freeware/png2ico/data/png2ico-win-2002-12-08.zip
             http://download.sysinternals.com/Files/Handle.zip
-	)
+        )
 
-	for x in "${file_list[@]}"; do
+        for x in "${file_list[@]}"; do
             if ! [[ -f `basename "$x"` ]]; then
-		eval "wget -N $x&"
+                eval "wget -N $x&"
             fi
-	done
+        done
         wait
-	cygstart ~/bin/windows/kbddvp.exe
+        cygstart ~/bin/windows/kbddvp.exe
 
-	cd ~/bin/windows/ext
-	for x in ~/external/download-all/*.zip; do 
+        cd ~/bin/windows/ext
+        for x in ~/external/download-all/*.zip; do
             if [[ -f "$x" ]]; then
-		/bin/unzip -o "$x";
+                /bin/unzip -o "$x";
             fi
-	done
-	find . -type f -print0|xargs -0 bash -c 'mv "$@" ./' true
-	find . -type f | perl -ne 'chomp; if (lc($_) ne $_) { printf "mv \"%s\" \"%s\"\n", $_, lc($_)}'|bash -x
-	mv ./*/* . || true
-	chmod a+x ./* || true
+        done
+        find . -type f -print0|xargs -0 bash -c 'mv "$@" ./' true
+        find . -type f | perl -ne 'chomp; if (lc($_) ne $_) { printf "mv \"%s\" \"%s\"\n", $_, lc($_)}'|bash -x
+        mv ./*/* . || true
+        chmod a+x ./* || true
     )&
 }
 
@@ -87,16 +87,16 @@ function setup-deb-src()
 {
     mkdir -p ~/external/emacs-site-lisp/ ~/external/deb-src
     cp /usr/share/emacs/site-lisp/subdirs.el ~/external/emacs-site-lisp/
-    cd ~/external/deb-src    
+    cd ~/external/deb-src
 
     source_list=(
         mirrors.163.com/debian/dists/unstable/main/source/Sources.gz
-        mirrors.163.com/debian/dists/unstable/contrib/source/Sources.gz 
+        mirrors.163.com/debian/dists/unstable/contrib/source/Sources.gz
         mirrors.163.com/debian/dists/unstable/non-free/source/Sources.gz
     )
 
     y=0
-    for x in "${source_list[@]}"; do 
+    for x in "${source_list[@]}"; do
         ((y++)) || true
         ( builtin cd ../ && wget -N -r http://$x )
         ln -sf ../$x Sources.gz.$y
@@ -106,39 +106,39 @@ function setup-deb-src()
 function get-bbdb()
 {
     (
-	set -ex
-	setup-deb-src
-	x=$(get-deb-src-dir bbdb)
-	rm *bbdb*/ *BBDB* -rf
-	wget -N "$x"
-	cd ~/external/emacs-site-lisp/
-	if [[ $x =~ .gz$ ]]; then
+        set -ex
+        setup-deb-src
+        x=$(get-deb-src-dir bbdb)
+        rm *bbdb*/ *BBDB* -rf
+        wget -N "$x"
+        cd ~/external/emacs-site-lisp/
+        if [[ $x =~ .gz$ ]]; then
             tar zxfv ../deb-src/"$(basename "$x")"
-	else 
-	    tar jxfv ../deb-src/"$(basename "$x")"
-	fi
-	builtin cd *BBDB*/
-	./configure --with-emacs=$HOME/external/emacs-nt/bin/emacs --with-gnus-dir=$HOME/external/emacs-nt/lisp/gnus 
-	find . -iname 'makefile'|xargs.exe dos2unix
-	perl -npe 's/`pwd`/`cygpath -alm .`/g' -i lisp/Makefile
-	make -C lisp bbdb-autoloads.el || true
-	make
+        else
+            tar jxfv ../deb-src/"$(basename "$x")"
+        fi
+        builtin cd *BBDB*/
+        ./configure --with-emacs=$HOME/external/emacs-nt/bin/emacs --with-gnus-dir=$HOME/external/emacs-nt/lisp/gnus
+        find . -iname 'makefile'|xargs.exe dos2unix
+        perl -npe 's/`pwd`/`cygpath -alm .`/g' -i lisp/Makefile
+        make -C lisp bbdb-autoloads.el || true
+        make
     )&
 }
 
 function get-boost-vc9() {
     (
-	set -e
-	cd ~/external/deb-src
-	x=http://sourceforge.net/projects/boost/files/boost/1.47.0/boost_1_47_0.7z
-	wget -N $x
-	7z -x $x
-	cd boost_1_47_0
-	my-read make sure that you have installed vc9
-	cd ./tools/build/v2/
-	cmd.exe /c bootstrap.bat
-	cd - 
-	./tools/build/v2/b2.exe --toolset=msvc-9.0 stage
+        set -e
+        cd ~/external/deb-src
+        x=http://sourceforge.net/projects/boost/files/boost/1.47.0/boost_1_47_0.7z
+        wget -N $x
+        7z -x $x
+        cd boost_1_47_0
+        my-read make sure that you have installed vc9
+        cd ./tools/build/v2/
+        cmd.exe /c bootstrap.bat
+        cd -
+        ./tools/build/v2/b2.exe --toolset=msvc-9.0 stage
     )
 }
 
@@ -151,70 +151,70 @@ function emacs-site-lisps()
         `get-deb-src-dir cscope`
         `get-deb-src-dir muse-el`
         `get-deb-src-dir w3m-el-snapshot`
-	`get-deb-src-dir bbdb`
+        `get-deb-src-dir bbdb`
     )
 
     emacs_zip=http://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-24.0.92-bin-i386.zip
     non_emacs_list=(
-	`get-deb-src-dir offlineimap`
-	`get-deb-src-dir exuberant-ctags`
-	http://www.python.org/ftp/python/3.1.4/Python-3.1.4.tar.bz2
-	$emacs_zip
+        `get-deb-src-dir offlineimap`
+        `get-deb-src-dir exuberant-ctags`
+        http://www.python.org/ftp/python/3.1.4/Python-3.1.4.tar.bz2
+        $emacs_zip
     )
-    
+
     for x in "${file_list[@]}" "${non_emacs_list[@]}"; do
         eval "wget -N $x&"
     done
     wait
 
     (
-	oldd=$(pwd)
-	mkdir -p ~/external/
-	cd ~/external/
-	
-	unzip $oldd/$(basename $emacs_zip)
-	rm -rf emacs-nt
-	mv emacs-24*/ emacs-nt
-	chmod +x emacs-nt/bin/*
-	cd emacs-nt/lisp/gnus
-	rm nnmaildir.elc 
-	patch -p2 < ~/doc/nnmaildir.patch 
-	(get-bbdb)
+        oldd=$(pwd)
+        mkdir -p ~/external/
+        cd ~/external/
+
+        unzip $oldd/$(basename $emacs_zip)
+        rm -rf emacs-nt
+        mv emacs-24*/ emacs-nt
+        chmod +x emacs-nt/bin/*
+        cd emacs-nt/lisp/gnus
+        rm nnmaildir.elc
+        patch -p2 < ~/doc/nnmaildir.patch
+        (get-bbdb)
     )&
 
     (
-	cd ~/external/emacs-site-lisp/
-	for x in "${file_list[@]}"; do
-	    if [[ $x =~ .gz$ ]]; then
-		tar zxfv ../deb-src/"$(basename "$x")"
-	    else 
-		tar jxfv ../deb-src/"$(basename "$x")"
-	    fi
-	done
-	cd emacs-goodies-el*/elisp/emacs-goodies-el/ && mkdir themes
+        cd ~/external/emacs-site-lisp/
+        for x in "${file_list[@]}"; do
+            if [[ $x =~ .gz$ ]]; then
+                tar zxfv ../deb-src/"$(basename "$x")"
+            else
+                tar jxfv ../deb-src/"$(basename "$x")"
+            fi
+        done
+        cd emacs-goodies-el*/elisp/emacs-goodies-el/ && mkdir themes
 
     )&
 
     (
-	for x in "${non_emacs_list[@]}"; do
-	    if [[ $x =~ .gz$ ]]; then
-		tar zxfv ../deb-src/"$(basename "$x")"
-	    elif [[ $x =~ .bz2$ ]]; then 
-		tar jxfv ../deb-src/"$(basename "$x")"
-	    fi
-	done
+        for x in "${non_emacs_list[@]}"; do
+            if [[ $x =~ .gz$ ]]; then
+                tar zxfv ../deb-src/"$(basename "$x")"
+            elif [[ $x =~ .bz2$ ]]; then
+                tar jxfv ../deb-src/"$(basename "$x")"
+            fi
+        done
     )
 
     rm  ~/bin/windows/ext/offlineimap -f
     ln -sf $(readlink -f *offlineimap*/offlineimap.py) ~/system-config/bin/windows/ext/offlineimap
     (
-	builtin cd *ctags*/ && ./configure && make -j8 install && ln -sf /usr/local/bin/ctags.exe /usr/bin/ctags-exuberant
+        builtin cd *ctags*/ && ./configure && make -j8 install && ln -sf /usr/local/bin/ctags.exe /usr/bin/ctags-exuberant
     )&
     (
-	builtin cd ~/gcode/global && sh reconf.sh && ./configure && make -j8 install && git clean -xfd
+        builtin cd ~/gcode/global && sh reconf.sh && ./configure && make -j8 install && git clean -xfd
     )&
     (
-        set -e; 
+        set -e;
         builtin cd *python*/ || builtin cd *Python*/
         patch -p1 <<EOF
 diff --git a/Modules/main.c b/Modules/main.c
@@ -232,7 +232,7 @@ EOF
         ./configure;
         make -j8
         make install
-	ln -sf /usr/local/bin/python3.1.exe /usr/local/bin/python3
+        ln -sf /usr/local/bin/python3.1.exe /usr/local/bin/python3
     )&
 
 }
@@ -255,7 +255,7 @@ function manual-download()
         http://code.google.com/p/zscreen/downloads/list
         http://www.avira.com/en/download/
     )
-    for x in "${page_list[@]}"; do 
+    for x in "${page_list[@]}"; do
         cygstart "$x";
     done
 }
@@ -267,7 +267,7 @@ function get-ms-addons()
         http://download.microsoft.com/download/5/6/7/567758a3-759e-473e-bf8f-52154438565a/dotnetfx.exe
         http://download.microsoft.com/download/0/6/1/061F001C-8752-4600-A198-53214C69B51F/dotnetfx35setup.exe
     )
-    for x in "${page_list[@]}"; do 
+    for x in "${page_list[@]}"; do
         wget -c "$x";
         `basename "$x"` /q
     done
@@ -276,9 +276,9 @@ function get-ms-addons()
 function setup-vc6-env() {
     unset PATHVC
     for x in ~/vc6/path/* ~/vc6/inc/* ~/vc6/lib/*; do
-	if test ! -e "$x"; then
-	    my-read -p "setup-vc6-env: $x is $(readlink -m "$x"); but it does not exist"
-	fi
+        if test ! -e "$x"; then
+            my-read -p "setup-vc6-env: $x is $(readlink -m "$x"); but it does not exist"
+        fi
     done
     for x in ~/vc6/path/*; do export PATHVC=$PATHVC:`readlink -m "$x"`; done
     export PATH=~/bin/:$PATHVC:$PATH
@@ -290,14 +290,14 @@ function setup-vc6-env() {
 
 function build-boost() {
     (
-	cd ~/external/boost
+        cd ~/external/boost
         rm -rf boost_1_34_1/
         tar jxfv boost_1_34_1.tar.bz2
         mkdir -p boost_1_34_1/stage/lib
 
         (
             cd boost_1_34_1/tools/jam/src
-	    patch -p1 <<EOF
+            patch -p1 <<EOF
 diff --git a/build.bat b/build.bat
 index c331d5c..3cd4f42 100755
 --- a/build.bat
@@ -309,7 +309,7 @@ index c331d5c..3cd4f42 100755
 -.\bootstrap\jam0 -f build.jam --toolset=%BOOST_JAM_TOOLSET% "--toolset-root=%BOOST_JAM_TOOLSET_ROOT% " %args%
 +.\bootstrap\jam0 -f build.jam --toolset=%BOOST_JAM_TOOLSET% "--toolset-root=%BOOST_JAM_TOOLSET_ROOT%" %args%
  :Skip_Jam
- 
+
  :Finish
 
 EOF
@@ -323,7 +323,7 @@ EOF
         find . -name "*.dll" -o -name "*.lib" > files.txt
         cp `cat files.txt` ./stage/lib
         cd ./stage/lib
-        mv libboost_regex-vc6-mt-1_34_1.lib libboost_regex-vc6-mt-p-1_34_1.lib 
+        mv libboost_regex-vc6-mt-1_34_1.lib libboost_regex-vc6-mt-p-1_34_1.lib
     )
 }
 
@@ -331,7 +331,7 @@ function get-stlport-and-boost()
 {
     mkdir -p ~/external/boost
     cd ~/external/boost
-    mkdir -p STLport-5.2.1/lib STLport-5.2.1/stlport boost_1_34_1/stage/lib 
+    mkdir -p STLport-5.2.1/lib STLport-5.2.1/stlport boost_1_34_1/stage/lib
 
     wget -N http://sourceforge.net/projects/stlport/files/STLport/STLport-5.2.1/STLport-5.2.1.tar.bz2&
     wget -N http://sourceforge.net/projects/boost/files/boost/1.34.1/boost_1_34_1.tar.bz2&
@@ -341,7 +341,7 @@ function get-stlport-and-boost()
         mkdir -p vcrun2010
         cd vcrun2010
         wget -N http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe&
-	wait
+        wait
         chmod +x vcredist_x86.exe
         cygstart vcredist_x86.exe
     ) >/dev/null 2>&1 &
@@ -365,9 +365,9 @@ function get-stlport-and-boost()
         wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/PSDK-FULL.bat&
         wget -N http://download.microsoft.com/download/platformsdk/sdk/update/win98mexp/en-us/3790.0/FULL/Extract.exe&
         wget -N http://www.avantbrowser.cn/release/absetup.exe&
-	wait
-	cmd.exe /c PSDK-FULL.bat ./ext
-	cygstart ./ext
+        wait
+        cmd.exe /c PSDK-FULL.bat ./ext
+        cygstart ./ext
     ) >/dev/null 2>&1 &
 
     (
@@ -396,7 +396,7 @@ index 4efd727..06317c7 100644
  #define _STLP_NEW_PLATFORM_SDK 1
 -*/
 +
- 
+
  /*
   * For the same reason as the one above we are not able to detect easily use
 EOF
@@ -407,22 +407,22 @@ EOF
 }
 
 
-if test "$(basename -- "$BASH_SOURCE")" = "$(basename -- "$0")"; 
-then 
-    if test "$(basename -- "$0")" != download-external.sh; 
+if test "$(basename -- "$BASH_SOURCE")" = "$(basename -- "$0")";
+then
+    if test "$(basename -- "$0")" != download-external.sh;
     then
-	set -e
-	set -x
-	`basename $0` "$@"
+        set -e
+        set -x
+        `basename $0` "$@"
     else
-	set -x
-	(get-python)
-	(download-all)
-	(setup-deb-src)
-	(emacs-site-lisps)
-	(manual-download)
-	(get-ms-addons)
-	(setup-vc6-env)
-	(get-stlport-and-boost)
+        set -x
+        (get-python)
+        (download-all)
+        (setup-deb-src)
+        (emacs-site-lisps)
+        (manual-download)
+        (get-ms-addons)
+        (setup-vc6-env)
+        (get-stlport-and-boost)
     fi
 fi
