531c91f45720d4fa8916a8b1ec07fa475b1dec19
Bao Haojun <baohaojun@gmail.com>
2013-08-13 10:31:06 +0800

    Add a script to search android sdk
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/download-google-sdk.sh
+++ bin/download-google-sdk.sh
@@ -23,20 +23,20 @@ done&
     x=$(if test -e ndk.ver; then cat ndk.ver; else echo 8; fi)
     ((x++))
     while true; do
-	wget -N http://dl.google.com/android/ndk/android-ndk-r$x-linux-x86.tar.bz2 || break
-	wget -N http://dl.google.com/android/ndk/android-ndk-r$x-darwin-x86.tar.bz2
-	wget -N http://dl.google.com/android/ndk/android-ndk-r$x-windows.zip
-	((x++))
+        wget -N http://dl.google.com/android/ndk/android-ndk-r$x-linux-x86.tar.bz2 || break
+        wget -N http://dl.google.com/android/ndk/android-ndk-r$x-darwin-x86.tar.bz2
+        wget -N http://dl.google.com/android/ndk/android-ndk-r$x-windows.zip
+        ((x++))
     done
 
     ((x--))
     for r in f e d c b; do
-	wget -N http://dl.google.com/android/ndk/android-ndk-r$x$r-linux-x86.tar.bz2 || continue
+        wget -N http://dl.google.com/android/ndk/android-ndk-r$x$r-linux-x86.tar.bz2 || continue
         if tty >/dev/null 2>&1; then
             break;
         fi
-	wget -N http://dl.google.com/android/ndk/android-ndk-r$x$r-darwin-x86.tar.bz2 || continue
-	if wget -N http://dl.google.com/android/ndk/android-ndk-r$x$r-windows.zip; then break; fi
+        wget -N http://dl.google.com/android/ndk/android-ndk-r$x$r-darwin-x86.tar.bz2 || continue
+        if wget -N http://dl.google.com/android/ndk/android-ndk-r$x$r-windows.zip; then break; fi
     done
 )&
 
@@ -46,14 +46,28 @@ for x in http://android-sdk-addons.motodevupdate.com/addons.xml \
     http://developer.sonyericsson.com/edk/android/repository.xml \
     http://dl.htcdev.com/sdk/addon.xml \
     http://innovator.samsungmobile.com/android/repository/repository.xml \
-    https://dl-ssl.google.com/android/repository/addon.xml \
+    http://dl.google.com/android/repository/addon.xml \
     http://software.intel.com/sites/landingpage/android/addon.xml \
     http://www.echobykyocera.com/download/echo_repository.xml; do
     wget -N -r $x || true
 done
 
-for x in $(seq 5 1000); do
-    wget -N -r https://dl-ssl.google.com/android/repository/repository-$x.xml || break
+re_ok=false
+first_good_sdk=5
+if test -e ~/.logs/first-good-sdk-xml.n; then
+    first_good_sdk=$(cat ~/.logs/first-good-sdk-xml.n)
+fi
+for x in $(seq $first_good_sdk 1000); do
+    if wget -N -r http://dl.google.com/android/repository/repository-$x.xml; then
+        if test $re_ok = false; then
+            echo $x > ~/.logs/first-good-sdk-xml.n
+        fi
+        re_ok=true;
+    else
+        if test $re_ok = true; then
+            break
+        fi
+    fi
 done
 
 ((x--))
@@ -64,15 +78,13 @@ else
     vpattern=shit
 fi
 
-for x in dl-ssl.google.com/android/repository/*.xml; do
+for x in dl.google.com/android/repository/*.xml; do
     xmlstarlet sel -N \
         $(cat $x|grep -P -e 'sdk=".*?"' -o|perl -npe 's/"//g') \
         -B -t -m "//sdk:archive" -v "sdk:url" -o ':' -v "sdk:checksum" \
-        -n $x    
+        -n $x
 done | sort -u | perl -npe 's!(.*):(.*)!test `shasum </dev/null \$(basename $1)|awk "{print \\\\\$1}"`x = $2x && echo $1 already exist || (echo download $1; if [[ $1 =~ :// ]]; then wget -N $1; else wget -N http://dl.google.com/android/repository/\$(basename $1); fi)!g'|grep -i -e "$vpattern" -v|bash -x
 
 mkdir -p ../temp
 cd ../temp
 ln ../google/* . -f
-
-
