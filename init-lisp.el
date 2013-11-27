b7af8329c1d0448ac9f7ace8deb97a114c8e0e89
Bao Haojun <baohaojun@gmail.com>
2013-11-27 16:43:05 +0800

    init-lisp.el: fix keybinding conflict for edebug
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- init-lisp.el
+++ init-lisp.el
@@ -124,7 +124,7 @@
 (add-to-list 'auto-mode-alist '("\\.emacs-project\\'" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("archive-contents\\'" . emacs-lisp-mode))
 
-(define-key emacs-lisp-mode-map (kbd "C-x C-a") 'pp-macroexpand-last-sexp)
+(define-key emacs-lisp-mode-map (kbd "C-x A") 'pp-macroexpand-last-sexp)
 (define-key emacs-lisp-mode-map (kbd "C-x C-e") 'pp-eval-last-sexp)
 
 (require-package 'cl-lib-highlight)
