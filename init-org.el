cd56449e154bab45cbf1e83138a996cf54caf500
Bao Haojun <baohaojun@gmail.com>
2013-12-02 19:48:26 +0800

    init-org.el: be verbose in my org todo note
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- init-org.el
+++ init-org.el
@@ -31,8 +31,8 @@
 
 
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
-              (sequence "WAITING(w@/!)" "SOMEDAY(S)" "|" "CANCELLED(c@/!)"))))
+      (quote ((sequence "TODO(t!)" "STARTED(s!)" "|" "DONE(d@)")
+              (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "|" "CANCELLED(c@/!)"))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
