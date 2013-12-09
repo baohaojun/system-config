cb8c4f6a30922cbdc787b50ef2c39cebe1adcc83
Bao Haojun <baohaojun@gmail.com>
2013-12-09 13:24:32 +0800

    bin/Linux/config/pkgs/evince: for pdf-for-kindle. evince used to be installed by gnome, which I don't use anymore (replaced with xfce4) bin/Linux/config/pkgs/file-roller: file-roller used to be installed by gnome bin/Linux/config/pkgs/python-levenshtein: for my english dictionary, search for a word by its distance to other words bin/aof: :-) bin/overide/adb: hint about adb device wait bin/pdf-split.sh: fix select size logic error when there is only 1 size; add option for specifying a range; make pdf small by converting to .ps and back to .pdf; bin/sendtext-android: wait for the adb device
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/pdf-split.sh
+++ bin/pdf-split.sh
@@ -7,24 +7,52 @@ function die() {
     exit -1
 }
 
+my_min=1
+my_max=
+TEMP=$(getopt -o m:M: --long min:max: -n $(basename $0) -- "$@")
+eval set -- "$TEMP"
+while true; do
+    case "$1" in
+        -m|--min)
+            my_min=$2
+            shift 2
+            ;;
+        -M|--max)
+            my_max=$2
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            die "internal error"
+            ;;
+    esac
+done
+
 if test $# != 1; then
     die "Error: Usage $(basename $0) pdf"
 fi
-pages=$(pdf-get-pages "$1")
+pages=${my_max:-$(pdf-get-pages "$1")}
 step=1
 
 seq_done=true
-for x in $(seq 1 1 $pages); do
+for x in $(seq $my_min 1 $pages); do
     if test ! -e seq-$x.pdf; then
         seq_done=false
         break
     fi
 done
 if test $seq_done = false || (test $seq_done = true && yes-or-no-p "Redo split?"); then
-    run-in-parallel -j 10 -I %N 'pdfjoin --no-landscape --rotateoversize false  '"$(printf %q "$1")"' %N --outfile seq-%N.pdf' $(seq 1 $step $pages)
+    run-in-parallel -j 10 -I %N '
+                                    pdfjoin --no-landscape --rotateoversize false  '"$(printf %q "$1")"' %N --outfile seq-%N.pdf
+                                    pdf2ps seq-%N.pdf
+                                    ps2pdf seq-%N.ps
+                                ' $(seq $my_min $step $pages)
 fi
 
-page_sizes=$(for x in $(seq 1 $step $pages); do
+page_sizes=$(for x in $(seq $my_min $step $pages); do
     pdfinfo seq-$x.pdf 2>/dev/null
 done | grep 'page size' -i | perl -ne '
     chomp;
@@ -36,23 +64,24 @@ done | grep 'page size' -i | perl -ne '
     }' | sort -n)
 
 size=$(select-output-line -- echo "$page_sizes")
+export selected_width=$(echo $size | pn 3)
+export selected_height=$(echo $size | pn 5)
+export nup_size=$(echo $size|perl -npe 's!.*?(\d+\.\d+) x (\d+\.\d+).*!sprintf "%fin,%fin", $1/72,$2/72!e')
 if test "$size" != "$page_sizes"; then
-    export selected_width=$(echo $size | pn 3)
-    export selected_height=$(echo $size | pn 5)
-    export nup_size=$(echo $size|perl -npe 's!.*?(\d+\.\d+) x (\d+\.\d+).*!sprintf "%fin,%fin", $1/72,$2/72!e')
     run-in-parallel -j 10 -I %N '
         if test $(pdf-get-width seq-%N.pdf) != $selected_width -o $(pdf-get-height seq-%N.pdf) != $selected_height; then
             pdfnup --no-landscape --rotateoversize false  --nup 1x1 --papersize "{$nup_size}" seq-%N.pdf --outfile seq-ss-%N.pdf
         else
             cp seq-%N.pdf seq-ss-%N.pdf
         fi
-    ' $(seq 1 $pages)
+    ' $(seq $my_min $pages)
 else
-    for x in $(seq 1 $pages); do
+    for x in $(seq $my_min $pages); do
         cp seq-$x.pdf seq-ss-$x.pdf
     done
 fi
 
-pdfjoin --no-landscape --rotateoversize false  --papersize "{${selected_width}pt,${selected_height}pt}" --outfile odd.pdf $(for x in $(seq 1 2 $pages); do echo seq-ss-$x.pdf; done)
-pdfjoin --no-landscape --rotateoversize false  --papersize "{${selected_width}pt,${selected_height}pt}" --outfile even.pdf $(for x in $(seq 2 2 $pages); do echo seq-ss-$x.pdf; done)
+my_min_p1=$((my_min + 1))
+pdfjoin --no-landscape --rotateoversize false  --papersize "{${selected_width}pt,${selected_height}pt}" --outfile odd.pdf $(for x in $(seq $my_min 2 $pages); do echo seq-ss-$x.pdf; done)
+pdfjoin --no-landscape --rotateoversize false  --papersize "{${selected_width}pt,${selected_height}pt}" --outfile even.pdf $(for x in $(seq $my_min_p1 2 $pages); do echo seq-ss-$x.pdf; done)
 
