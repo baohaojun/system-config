d2676573175b82a649645ac648ca15c68e9f84ba
Bao Haojun <baohaojun@gmail.com>
2014-01-06 22:35:58 +0800

    bin/after-co-ln-s.sh: do the lid suspend in zz-switch-touchpad bin/git-push-mac: only tag true blog articles bin/start-ap: start-ap should work out of the box bin/tune-brightness: att doc/usr/lib/pm-utils/sleep.d/99-switch-touchpad: do lid suspend
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/after-co-ln-s.sh
+++ bin/after-co-ln-s.sh
@@ -231,11 +231,9 @@ if test ! -d ~/.config/about_me && yes-or-no-p "You want to configure your about
 fi
 
 sudo mkdir -p /etc/acpi/local/
-sudo ln -sf ~/etc/acpi/local/lid.sh.pre /etc/acpi/local/
 
 sudo cp ~/etc/udev/rules.d/90-bhj-personal.rules /etc/udev/rules.d/90-bhj-personal.rules
 sudo cp ~/etc/lib/udev/keymaps/microsoft-ergonomic-keyboard /lib/udev/keymaps/microsoft-ergonomic-keyboard
-sudo cp ~/doc/usr/lib/pm-utils/sleep.d/99-switch-touchpad /usr/lib/pm-utils/sleep.d/99-switch-touchpad
 sudo cp ~/doc/usr/lib/pm-utils/sleep.d/99-switch-touchpad /lib/systemd/system-sleep/zz-switch-touchpad
 sudo cp ~/etc/tsocks.conf /etc/tsocks.conf
 cat ~/etc/news/leafnode/config | sudo tee /etc/news/leafnode/config >/dev/null 2>&1 || true
