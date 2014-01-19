b18ac9fc780a14c07a5a457cc5724a5a915dd417
Bao Haojun <baohaojun@gmail.com>
2014-01-19 23:07:20 +0800

    .emacs_d/lisp/bhj-set-key.el: maximize emacs window bin/Darwin/after-check-out.sh: try to make mac after-check-out.sh work bin/Darwin/apt-get: port bin/Linux/config/do_compile: compile beagrep for mac correctly bin/axml2xml.pl: use the right version of perl bin/git-update-ignore: do not offer to glob a directory bin/my-rfa: fix my-rfa so that it works on mac, and will respect repo group
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ bin/Darwin/after-check-out.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+. ~/.bashrc
+touch ~/.where ~/.where.lock
+
+. ~/bin/Linux/download-external.sh
+cpan String::Approx
+cpan String::ShellQuote
+
+download_external >/dev/null 2>&1 &
+
+set -e
+export PATH=~/bin/Linux/config:$PATH
+
+#update the system
+
+mkdir -p ~/src/github
+emacs-install-packages
+
+if test ! -d /usr/local/share/info; then
+    sudo mkdir -p /usr/local/share/info
+fi
+
+sudo ln -s ~/doc/bash.info.gz /usr/local/share/info/ -f
+
+(
+    cd /usr/local/share/info/
+    sudo ginstall-info bash.info.gz /usr/local/share/info/dir
+)
+
+#编译一些软件
+compile_beagrep
+compile_gtags
+compile_ctags
+
+echo 'OK'
--- bin/axml2xml.pl
+++ bin/axml2xml.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 ###############################################################################
 ##
 ## Copyright (C) 2008 Josh Guilfoyle <jasta@devtcg.org>
@@ -76,7 +76,7 @@ my @stroffs;
 
 for (my $i = 0; $i < $strings; $i++)
 {
-	push @stroffs, unpack('V', read_doc($doc, 4));
+        push @stroffs, unpack('V', read_doc($doc, 4));
 }
 
 debug(Dumper map { sprintf('%02x', $_) } @stroffs);
@@ -88,19 +88,19 @@ my $curroffs = 0;
 # the positions are not sorted :)
 foreach my $offs (sort { $a <=> $b } @stroffs)
 {
-	die unless $offs == $curroffs;
+        die unless $offs == $curroffs;
 
-	my $len = unpack('v', read_doc($doc, 2));
+        my $len = unpack('v', read_doc($doc, 2));
 
-	my $str = read_doc($doc, ($len) * 2);
-	debug("str=$str\n");
+        my $str = read_doc($doc, ($len) * 2);
+        debug("str=$str\n");
 
-	# Read the NUL, we're not interested in storing it.
-	read_doc($doc, 2);
+        # Read the NUL, we're not interested in storing it.
+        read_doc($doc, 2);
 
-	$strings->{$offs} = $str;
+        $strings->{$offs} = $str;
 
-	$curroffs += (($len + 1) * 2) + 2;
+        $curroffs += (($len + 1) * 2) + 2;
 }
 
 my @strings = map { $strings->{$_} } @stroffs;
@@ -109,10 +109,10 @@ debugf("curroffs=%d (0x%x)\n", $curroffs, $curroffs);
 
 for (my $i = 0; $i < @strings; $i++)
 {
-	debugf("0x%02x. %s\n", $i, $strings[$i]);
+        debugf("0x%02x. %s\n", $i, $strings[$i]);
 }
 
-# 
+#
 # OPEN TAG:
 # V=tagS 0x1400 1400 V=1 V=0 {ATTR? V=7 V=attrS V=valS V=attrS|0x3<<24 V=valS 0x0301 1000 V=0x18 V=? } V=~0 V=~0
 # V=1    0x0800 0000 V=0x19 0x0201 1000 V=0x38 V=7 V=~0 V=~0
@@ -123,10 +123,10 @@ for (my $i = 0; $i < @strings; $i++)
 # OPEN TAG (outer tag, no attributes):
 # V=tagS 0x1400 1400 V=0    V=0         0x0401 1000 V=0x1c V=0    V=~0
 # V=1    0x0800 0000 V=0x20 0x0201 1000 V=0x38      V=0x4  V=~0   V=~0
-# 
+#
 # OPEN TAG (normal, child, NO ATTRIBUTES):
 # V=tagS 0x1400 1400 V=0    V=0         0x0301 1000 V=0x18 V=0x0b V=~0 V=~0
-# 
+#
 # CLOSE TAG (normal, child):
 # V=tagS 0x0401 1000 V=0x1c V=0         V=~0
 #
@@ -165,253 +165,253 @@ my $nsend = read_doc($doc, 8);
 
 sub print_tree
 {
-	my ($node, $depth) = @_;
+        my ($node, $depth) = @_;
 
-	print "\t" x $depth;
+        print "\t" x $depth;
 
-	if (($node->{flags} & $TAG_TEXT) != 0)
-	{
-		print $node->{name}, "\n";
-		return;
-	}
+        if (($node->{flags} & $TAG_TEXT) != 0)
+        {
+                print $node->{name}, "\n";
+                return;
+        }
 
-	print '<';
+        print '<';
 
-	if (($node->{flags} & $TAG_OPEN) == 0)
-	{
-		print '/';
-	}
+        if (($node->{flags} & $TAG_OPEN) == 0)
+        {
+                print '/';
+        }
 
-	print $node->{name};
+        print $node->{name};
 
-	foreach my $attr (@{$node->{attrs}})
-	{
-		if (scalar(@{$node->{attrs}}) == 1)
-		{
-			print ' ';
-		}
-		else
-		{
-			print "\n", "\t" x ($depth + 1);
-		}
+        foreach my $attr (@{$node->{attrs}})
+        {
+                if (scalar(@{$node->{attrs}}) == 1)
+                {
+                        print ' ';
+                }
+                else
+                {
+                        print "\n", "\t" x ($depth + 1);
+                }
 
-		$attr->{ns} and
-		  printf '%s:', $attr->{ns};
+                $attr->{ns} and
+                  printf '%s:', $attr->{ns};
 
-		printf '%s="%s"', @$attr{qw/name value/};
-	}
+                printf '%s="%s"', @$attr{qw/name value/};
+        }
 
-	scalar(@{$node->{children}}) == 0 and
-	  print ' /';
+        scalar(@{$node->{children}}) == 0 and
+          print ' /';
 
-	print ">\n";
+        print ">\n";
 
-	if (scalar(@{$node->{children}}) > 0)
-	{
-		foreach my $child (@{$node->{children}})
-		{
-			print_tree($child, $depth + 1);
-		}
+        if (scalar(@{$node->{children}}) > 0)
+        {
+                foreach my $child (@{$node->{children}})
+                {
+                        print_tree($child, $depth + 1);
+                }
 
-		print "\t" x $depth, "</$node->{name}>\n";
-	}
+                print "\t" x $depth, "</$node->{name}>\n";
+        }
 }
 
 ###############################################################################
 
 sub read_meat
 {
-	my $tag = read_tag($doc);
-	die unless $tag;
+        my $tag = read_tag($doc);
+        die unless $tag;
 
-	$tag->{children} = [ read_children($doc, $tag->{name}) ];
+        $tag->{children} = [ read_children($doc, $tag->{name}) ];
 
-	return $tag;
+        return $tag;
 }
 
 sub read_children
 {
-	my ($doc, $stoptag) = @_;
-	my @tags;
+        my ($doc, $stoptag) = @_;
+        my @tags;
 
-	while ((my $tag = read_tag($doc)))
-	{
-		# Whitespace leaks into this, but we don't support parsing it
-		# correctly.
-#		next unless $tag->{name} =~ m/[a-z]/i;
+        while ((my $tag = read_tag($doc)))
+        {
+                # Whitespace leaks into this, but we don't support parsing it
+                # correctly.
+#               next unless $tag->{name} =~ m/[a-z]/i;
 
-		if (($tag->{flags} & $TAG_SUPPORTS_CHILDREN) != 0)
-		{
-			if (($tag->{flags} & $TAG_OPEN) != 0)
-			{
-				$tag->{children} = [ read_children($doc, $tag->{name}) ];
-			}
-			elsif ($tag->{name} eq $stoptag)
-			{
-				last;
-			}
-		}
+                if (($tag->{flags} & $TAG_SUPPORTS_CHILDREN) != 0)
+                {
+                        if (($tag->{flags} & $TAG_OPEN) != 0)
+                        {
+                                $tag->{children} = [ read_children($doc, $tag->{name}) ];
+                        }
+                        elsif ($tag->{name} eq $stoptag)
+                        {
+                                last;
+                        }
+                }
 
-		push @tags, $tag;
-	}
+                push @tags, $tag;
+        }
 
-	return @tags;
+        return @tags;
 }
 
 sub read_tag
 {
-	my ($doc, $stoptag) = @_;
-	my $tag;
-	my @xmlns;
+        my ($doc, $stoptag) = @_;
+        my $tag;
+        my @xmlns;
 
 # Hack to support the strange xmlns attribute encoding without disrupting our
 # processor.
 READ_AGAIN:
-	my $name = unpack('V', read_doc($doc, 4));
-	debugf("tag=%s (%d) @ 0x%x\n", $strings[$name], $name, $doc->{pos});
+        my $name = unpack('V', read_doc($doc, 4));
+        debugf("tag=%s (%d) @ 0x%x\n", $strings[$name], $name, $doc->{pos});
 
-	my $flags = unpack('V', read_doc($doc, 4));
-	debugf("	flags=0x%08x (%d, open=%d, children=%d, text=%d)\n", $flags, $flags, $flags & $TAG_OPEN, $flags & $TAG_SUPPORTS_CHILDREN, $flags & $TAG_TEXT);
+        my $flags = unpack('V', read_doc($doc, 4));
+        debugf("        flags=0x%08x (%d, open=%d, children=%d, text=%d)\n", $flags, $flags, $flags & $TAG_OPEN, $flags & $TAG_SUPPORTS_CHILDREN, $flags & $TAG_TEXT);
 
-	# Strange way to specify xmlns attribute.
-	if ($strings[$name] && $strings[$flags])
-	{
-		my $ns = utf16le($strings[$name])->utf8;
-		my $url = utf16le($strings[$flags])->utf8;
+        # Strange way to specify xmlns attribute.
+        if ($strings[$name] && $strings[$flags])
+        {
+                my $ns = utf16le($strings[$name])->utf8;
+                my $url = utf16le($strings[$flags])->utf8;
 
-		# TODO: How do we expect this?
-		if ($ns =~ m/[a-z]/i && $url =~ m/^http:\/\//)
-		{
-			debug("new map: $flags => $name\n");
-			$nsmap->{$flags} = $name;
-			push @xmlns, { name => "xmlns:$ns", value => $url };
-			read_doc_past_sentinel($doc);
-			goto READ_AGAIN;
-		}
-	}
+                # TODO: How do we expect this?
+                if ($ns =~ m/[a-z]/i && $url =~ m/^http:\/\//)
+                {
+                        debug("new map: $flags => $name\n");
+                        $nsmap->{$flags} = $name;
+                        push @xmlns, { name => "xmlns:$ns", value => $url };
+                        read_doc_past_sentinel($doc);
+                        goto READ_AGAIN;
+                }
+        }
 
-	if (($flags & $TAG_SUPPORTS_CHILDREN) != 0 && ($flags & $TAG_OPEN) != 0)
-	{
-		$tag->{attrs} = [ @xmlns ];
+        if (($flags & $TAG_SUPPORTS_CHILDREN) != 0 && ($flags & $TAG_OPEN) != 0)
+        {
+                $tag->{attrs} = [ @xmlns ];
 
-		my $attrs = unpack('V', read_doc($doc, 4));
-		debugf("	attrs=%d\n", $attrs);
+                my $attrs = unpack('V', read_doc($doc, 4));
+                debugf("        attrs=%d\n", $attrs);
 
-		my $unknown = unpack('V', read_doc($doc, 4));
+                my $unknown = unpack('V', read_doc($doc, 4));
 
-		while ($attrs-- > 0)
-		{
-			my $ns = unpack('V', read_doc($doc, 4));
+                while ($attrs-- > 0)
+                {
+                        my $ns = unpack('V', read_doc($doc, 4));
 
-			$ns != 0xffffffff and
-			  debugf("		namespace=%s\n", $strings[$ns]);
+                        $ns != 0xffffffff and
+                          debugf("              namespace=%s\n", $strings[$ns]);
 
-			my $attr = unpack('V', read_doc($doc, 4));
-			debugf("		attr=%s\n", $strings[$attr]);
+                        my $attr = unpack('V', read_doc($doc, 4));
+                        debugf("                attr=%s\n", $strings[$attr]);
 
-			# TODO: Escaping?
-			my $value = unpack('V', read_doc($doc, 4));
-			debugf("		value=%s\n", $strings[$value]);
+                        # TODO: Escaping?
+                        my $value = unpack('V', read_doc($doc, 4));
+                        debugf("                value=%s\n", $strings[$value]);
 
-			my $attrflags = unpack('V', read_doc($doc, 4));
+                        my $attrflags = unpack('V', read_doc($doc, 4));
 
-			my $attr = {
-				name => utf16le($strings[$attr])->utf8,
-				value => utf16le($strings[$value])->utf8,
-				flags => $attrflags,
-			};
+                        my $attr = {
+                                name => utf16le($strings[$attr])->utf8,
+                                value => utf16le($strings[$value])->utf8,
+                                flags => $attrflags,
+                        };
 
-			$ns != 0xffffffff and
-			  $attr->{ns} = utf16le($strings[$nsmap->{$ns}])->utf8;
+                        $ns != 0xffffffff and
+                          $attr->{ns} = utf16le($strings[$nsmap->{$ns}])->utf8;
 
-			push @{$tag->{attrs}}, $attr;
+                        push @{$tag->{attrs}}, $attr;
 
-			my $padding = unpack('V', read_doc($doc, 4));
-#			read_doc_past_sentinel($doc, 1);
-		}
+                        my $padding = unpack('V', read_doc($doc, 4));
+#                       read_doc_past_sentinel($doc, 1);
+                }
 
-		read_doc_past_sentinel($doc);
-	}
-	else
-	{
-		# There is strong evidence here that what I originally thought
-		# to be a sentinel is not ;)
-		my $whatever = unpack('V', read_doc($doc, 4));
-		my $huh = unpack('V', read_doc($doc, 4));
+                read_doc_past_sentinel($doc);
+        }
+        else
+        {
+                # There is strong evidence here that what I originally thought
+                # to be a sentinel is not ;)
+                my $whatever = unpack('V', read_doc($doc, 4));
+                my $huh = unpack('V', read_doc($doc, 4));
 
-		read_doc_past_sentinel($doc);
-	}
+                read_doc_past_sentinel($doc);
+        }
 
-	$tag->{name} = utf16le($strings[$name])->utf8;
-	$tag->{flags} = $flags;
+        $tag->{name} = utf16le($strings[$name])->utf8;
+        $tag->{flags} = $flags;
 
-	return $tag;
+        return $tag;
 }
 
 ###############################################################################
 
 sub read_doc
 {
-	my ($doc, $n) = @_;
+        my ($doc, $n) = @_;
 
-	(length($doc->{data}) - $doc->{pos}) < $n and
-	  die "Not enough data to read $n bytes at $doc->{pos}.\n";
+        (length($doc->{data}) - $doc->{pos}) < $n and
+          die "Not enough data to read $n bytes at $doc->{pos}.\n";
 
-	my $data = substr($doc->{data}, $doc->{pos}, $n);
-	$doc->{pos} += $n;
+        my $data = substr($doc->{data}, $doc->{pos}, $n);
+        $doc->{pos} += $n;
 
-	return $data;
+        return $data;
 }
 
 sub peek_doc
 {
-	my ($doc, $n) = @_;
+        my ($doc, $n) = @_;
 
-	my $data = read_doc($doc, $n);
-	$doc->{pos} -= $n;
+        my $data = read_doc($doc, $n);
+        $doc->{pos} -= $n;
 
-	return $data;
+        return $data;
 }
 
 sub read_doc_past_sentinel
 {
-	my ($doc, $count) = @_;
+        my ($doc, $count) = @_;
 
-	my $pos = $doc->{pos};
+        my $pos = $doc->{pos};
 
-	# Read to sentinel.
-	while ((my $word = read_doc($doc, 4)))
-	{
-		last if unpack('V', $word) == 0xffffffff;
-	}
+        # Read to sentinel.
+        while ((my $word = read_doc($doc, 4)))
+        {
+                last if unpack('V', $word) == 0xffffffff;
+        }
 
-	my $n = 1;
+        my $n = 1;
 
-	# Read past it.
-	if (!defined($count) || $count < $n)
-	{
-		while ((my $word = peek_doc($doc, 4)))
-		{
-			last unless unpack('V', $word) == 0xffffffff;
+        # Read past it.
+        if (!defined($count) || $count < $n)
+        {
+                while ((my $word = peek_doc($doc, 4)))
+                {
+                        last unless unpack('V', $word) == 0xffffffff;
 
-			read_doc($doc, 4);
-			$n++;
+                        read_doc($doc, 4);
+                        $n++;
 
-			last if (defined($count) && $count >= $n);
-		}
-	}
+                        last if (defined($count) && $count >= $n);
+                }
+        }
 
-	debugf("[skipped %d sentinels, %d bytes]\n", $n, $doc->{pos} - $pos);
+        debugf("[skipped %d sentinels, %d bytes]\n", $n, $doc->{pos} - $pos);
 }
 
 ###############################################################################
 
 sub debug($;@)
 {
-	print STDERR @_ if $DEBUG;
+        print STDERR @_ if $DEBUG;
 }
 
 sub debugf($;@)
 {
-	printf STDERR @_ if $DEBUG;
+        printf STDERR @_ if $DEBUG;
 }
