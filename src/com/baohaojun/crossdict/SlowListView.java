8bb50df8245e50725190b01bfb1eb2b81b891c7b
Bao Haojun <baohaojun@gmail.com>
2014-01-23 19:51:00 +0800

    AndroidManifest.xml: jump version src/com/baohaojun/crossdict/SlowListView.java: fix matcher dict crash src/com/googlecode/toolkits/stardict/MatcherDict.java: fix matcher dict crash
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- src/com/baohaojun/crossdict/SlowListView.java
+++ src/com/baohaojun/crossdict/SlowListView.java
@@ -1,27 +1,28 @@
 package com.baohaojun.crossdict;
 import android.app.Activity;
-import android.widget.BaseAdapter;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.view.ViewGroup;
-import android.widget.ListView;
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.widget.AbsListView;
-import android.util.Log;
-import android.widget.AbsListView.OnScrollListener;
-import android.view.View;
-import com.googlecode.toolkits.stardict.StarDict;
+import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.googlecode.toolkits.stardict.MatcherDict;
+import com.googlecode.toolkits.stardict.StarDict;
 import com.googlecode.toolkits.stardict.StarDictInterface;
 
 public class SlowListView extends ListView implements ListView.OnScrollListener {
 
     private boolean mBusy = false;
-    
+
     /**
      * Will not bind views while the list is scrolling
-     * 
+     *
      */
 
     public SlowListView(Context context) {
@@ -38,7 +39,7 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
 
     private class SlowAdapter extends BaseAdapter {
         private LayoutInflater mInflater;
-        
+
         public SlowAdapter(Context context) {
             mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
@@ -46,7 +47,7 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
         /**
          * The number of items in the list is determined by the number of speeches
          * in our array.
-         * 
+         *
          * @see android.widget.ListAdapter#getCount()
          */
         public int getCount() {
@@ -58,7 +59,7 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
          * sufficent to get at the data. If we were using a more complex data
          * structure, we would return whatever object represents one row in the
          * list.
-         * 
+         *
          * @see android.widget.ListAdapter#getItem(int)
          */
         public Object getItem(int position) {
@@ -67,7 +68,7 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
 
         /**
          * Use the array index as a unique id.
-         * 
+         *
          * @see android.widget.ListAdapter#getItemId(int)
          */
         public long getItemId(int position) {
@@ -76,13 +77,13 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
 
         /**
          * Make a view to hold each row.
-         * 
+         *
          * @see android.widget.ListAdapter#getView(int, android.view.View,
          *      android.view.ViewGroup)
          */
         public View getView(int position, View convertView, ViewGroup parent) {
             TextView text;
-            
+
             if (convertView == null) {
                 text = (TextView)mInflater.inflate(R.layout.simple_list_item_activated_1, parent, false);
             } else {
@@ -102,62 +103,75 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
             return text;
         }
     }
-    
+
     public void scrollToWord(String word) {
 
-	int idx = mActiveDict.getWordIdx(word);
-	int first = getFirstVisiblePosition();
-	int count = getChildCount();
+        int idx = mActiveDict.getWordIdx(word);
+        int first = getFirstVisiblePosition();
+        int count = getChildCount();
 
-	Log.e("bhj", String.format("idx is %d\n", idx));
+        Log.e("bhj", String.format("idx is %d\n", idx));
 
-	if (idx < first || idx >= first + count) {
-	    Log.e("bhj", String.format("scroll to %d\n", idx));
-	    setItemChecked(idx, true);
-	    setSelection(idx);
-	} else {
-	    setItemChecked(idx, true);	 
-	}
+        if (idx < first || idx >= first + count) {
+            Log.e("bhj", String.format("scroll to %d\n", idx));
+            setItemChecked(idx, true);
+            setSelection(idx);
+        } else {
+            setItemChecked(idx, true);
+        }
     }
 
+    SlowAdapter mAdapter = null;
     public void createAndSetAdapter(Context context, StarDictInterface dict) {
-	mActiveDict = mTheDict = dict;
-        setAdapter(new SlowAdapter(context));
+        mAdapter = new SlowAdapter(context);
+
+        mActiveDict = mTheDict = dict;
+        MatcherDict d;
+        if (dict instanceof MatcherDict) {
+            d = (MatcherDict) dict;
+            d.setAdapter(mAdapter);
+        }
+
+        setAdapter(mAdapter);
         setOnScrollListener(this);
-	setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        setChoiceMode(ListView.CHOICE_MODE_SINGLE);
     }
-    
-    
+
+
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
             int totalItemCount) {
     }
-    
+
     private StarDictInterface mTheDict;
     private StarDictInterface mActiveDict;
-    
+
     /**
      * Change the active dictionary the list is using.
-     * 
+     *
      * @return true if the dictionary is changed, false if not
      * changed.
      */
     public boolean setActiveDict(StarDictInterface dict) {
-	boolean ret = false;
+        boolean ret = false;
 
-	if (mActiveDict != dict) {
-	    ret = true;
-	    mActiveDict = dict;
-	    requestLayout();
-	    setSelection(0);
-	}
-	return ret;
+        if (mActiveDict != dict) {
+            ret = true;
+            mActiveDict = dict;
+            if (mActiveDict instanceof MatcherDict) {
+                MatcherDict d = (MatcherDict) mActiveDict;
+                d.setAdapter(mAdapter);
+            }
+            requestLayout();
+            setSelection(0);
+        }
+        return ret;
     }
 
     public void onScrollStateChanged(AbsListView view, int scrollState) {
         switch (scrollState) {
         case OnScrollListener.SCROLL_STATE_IDLE:
             mBusy = false;
-            
+
             int first = view.getFirstVisiblePosition();
             int count = view.getChildCount();
             for (int i=0; i<count; i++) {
@@ -167,7 +181,7 @@ public class SlowListView extends ListView implements ListView.OnScrollListener 
                     t.setTag(null);
                 }
             }
-            
+
             break;
         case OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
             mBusy = true;
--- src/com/googlecode/toolkits/stardict/MatcherDict.java
+++ src/com/googlecode/toolkits/stardict/MatcherDict.java
@@ -1,50 +1,57 @@
 package com.googlecode.toolkits.stardict;
-import java.util.ArrayList;
-import java.io.InputStreamReader;
+import android.util.Log;
+import android.widget.BaseAdapter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Formatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MatcherDict implements StarDictInterface {
     static String wordsFileName;
     static String wordsStr;
     static int totalEntries;
 
+    private BaseAdapter mAdapter;
     private static boolean mDebug;
     private static void debug(String format, Object... args) {
-	if (mDebug) {
-	    new Formatter(System.err).format(format, args);
-	}
+        if (mDebug) {
+            new Formatter(System.err).format(format, args);
+        }
+    }
+
+    public void setAdapter(BaseAdapter adapter) {
+        mAdapter = adapter;
     }
 
     static public void useWordsFile(String fileName) {
-	if (wordsFileName != null && wordsFileName.compareTo(fileName) == 0) {
-	    return;
-	}
-	wordsFileName = fileName;
-	try {
-	    InputStreamReader reader = new InputStreamReader(new FileInputStream(wordsFileName + ".dz"), "UTF8");
-	    totalEntries = (int) (new File(wordsFileName + ".ii").length()/4);
-	    int length = (int) new File(wordsFileName + ".dz").length();
-	    char[] buffer = new char[length];
+        if (wordsFileName != null && wordsFileName.compareTo(fileName) == 0) {
+            return;
+        }
+        wordsFileName = fileName;
+        try {
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(wordsFileName + ".dz"), "UTF8");
+            totalEntries = (int) (new File(wordsFileName + ".ii").length()/4);
+            int length = (int) new File(wordsFileName + ".dz").length();
+            char[] buffer = new char[length];
 
-	    int n = 0;
-	    do {
-		int ret = reader.read(buffer, n, buffer.length - n);
-		if (ret <= 0) {
-		    break;
-		}
-		n += ret;
-	    } while (true);
+            int n = 0;
+            do {
+                int ret = reader.read(buffer, n, buffer.length - n);
+                if (ret <= 0) {
+                    break;
+                }
+                n += ret;
+            } while (true);
 
-	    wordsStr = new String(buffer, 0, n);
-	} catch (Exception e) {
-	    debug("error building words list: %s", e.toString());
-	    wordsStr = null;
-	    wordsFileName = null;
-	}
+            wordsStr = new String(buffer, 0, n);
+        } catch (Exception e) {
+            debug("error building words list: %s", e.toString());
+            wordsStr = null;
+            wordsFileName = null;
+        }
     }
 
     String mMatcherStr;
@@ -57,85 +64,86 @@ public class MatcherDict implements StarDictInterface {
     boolean mExhausted = false;
 
     public MatcherDict(String matcher) {
-	mTotalEntries = totalEntries;
-	mMatcherStr = matcher;
-	mMatchees = new ArrayList<String>(100);
-	mMatcherPattern = Pattern.compile(mMatcherStr, Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
-	mMatcher = mMatcherPattern.matcher(wordsStr);
+        mTotalEntries = totalEntries;
+        mMatcherStr = matcher;
+        mMatchees = new ArrayList<String>(100);
+        mMatcherPattern = Pattern.compile(mMatcherStr, Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);
+        mMatcher = mMatcherPattern.matcher(wordsStr);
 
-	for (int i = 0; i < 100; i++) {
-	    findNextMatch();
-	}	
+        for (int i = 0; i < 100; i++) {
+            findNextMatch();
+        }
     }
 
     private void findNextMatch() {
-	if (mExhausted) {
-	    return;
-	}
+        if (mExhausted) {
+            return;
+        }
 
-	if (mMatcher.find(mStart)) {
-	    int start = mMatcher.start(0);
-	    int end = mMatcher.end(0);
-	    for (; start >= 0; start--) {
-		if (wordsStr.charAt(start) == '\n') {
-		    break;
-		}
-	    }
-	    start++;
+        if (mMatcher.find(mStart)) {
+            int start = mMatcher.start(0);
+            int end = mMatcher.end(0);
+            for (; start >= 0; start--) {
+                if (wordsStr.charAt(start) == '\n') {
+                    break;
+                }
+            }
+            start++;
 
-	    for (; end < wordsStr.length(); end++) {
-		if (wordsStr.charAt(end) == '\n') {
-		    break;
-		}
-	    }
-	    mStart = end + 1;
+            for (; end < wordsStr.length(); end++) {
+                if (wordsStr.charAt(end) == '\n') {
+                    break;
+                }
+            }
+            mStart = end + 1;
 
-	    // this is not needed, see substring
-	    // end--; 
-	    mMatchees.add(wordsStr.substring(start, end));
-	} else {
-	    mExhausted = true;
-	    mTotalEntries = mMatchees.size();
-	}
+            // this is not needed, see substring
+            // end--;
+            mMatchees.add(wordsStr.substring(start, end));
+        } else {
+            mExhausted = true;
+            mTotalEntries = mMatchees.size();
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
     public int getWordIdx(String word) {
-	for (int i = 0; i < mMatchees.size(); i++) {
-	    if (mMatchees.get(i).compareTo(word) == 0) {
-		return i;
-	    }
-	}
-	return 0;
+        for (int i = 0; i < mMatchees.size(); i++) {
+            if (mMatchees.get(i).compareTo(word) == 0) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     @Override
     public String getWord(int idx) {
-	if (idx < mMatchees.size()) {
-	    return mMatchees.get(idx);
-	}
-	
-	while (! mExhausted && mMatchees.size() <= idx) {
-	    findNextMatch();
-	}
-	if (idx >= mMatchees.size()) {
-	    return "";
-	}
-	return mMatchees.get(idx);
+        if (idx < mMatchees.size()) {
+            return mMatchees.get(idx);
+        }
+
+        while (! mExhausted && mMatchees.size() <= idx) {
+            findNextMatch();
+        }
+        if (idx >= mMatchees.size()) {
+            return "";
+        }
+        return mMatchees.get(idx);
     }
-    
+
     @Override
     public int getTotalNumOfEntries() {
-	return mTotalEntries;
+        return mTotalEntries;
     }
 
     public static void main(String[] args) {
-	mDebug = true;
-	useWordsFile("words");
-	MatcherDict dict = new MatcherDict("hello");
-	for (String word : dict.mMatchees) {
-	    System.out.println("hello word: " + word);
-	}
+        mDebug = true;
+        useWordsFile("words");
+        MatcherDict dict = new MatcherDict("hello");
+        for (String word : dict.mMatchees) {
+            System.out.println("hello word: " + word);
+        }
     }
-    
+
 }
