a03d18ed9a310eea25c21e6b41e6eb0a4c6de21b
Bao Haojun <baohaojun@gmail.com>
2014-02-05 12:01:58 +0800

    .globalrc: .g4 for antlr 4 bin/Darwin/firefox-search-for-tab: https://bugzilla.mozilla.org/show_bug.cgi?id=393645 bin/Darwin/firefox: found a solution for firefox -new-tab on mac @https://bugzilla.mozilla.org/show_bug.cgi?id=393645 bin/Darwin/imenu-ctags: can't readlink /dev/stdin bin/Darwin/mplayer: simple mplayer on command line on mac bin/get-stdin: use ask-for-input-with-emacs as ask-for-input-with-sawfish on mac bin/imenu-ctags: fix imenu-ctags, where it can't readlink the /dev/stdin to get true filename bin/my-dict: https://bugzilla.mozilla.org/show_bug.cgi?id=393645 bin/ssh-to-phone: fix it on the note3, bare sh doesn't work doc/galaxy-nexus-root/sdcard/start-debian2.sh: fix it for note3 etc/arp-hosts: for note3
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- doc/galaxy-nexus-root/sdcard/start-debian2.sh
+++ doc/galaxy-nexus-root/sdcard/start-debian2.sh
@@ -3,7 +3,7 @@ set -e
 
 # Make tmp
 if [ ! -d /tmp ]; then
-    mount -o rw,remount / && mkdir /tmp && mount -o ro,remount /
+    mount -o rw,remount / && mkdir /tmp && chmod 1777 /tmp && mount -o ro,remount /
 fi
 if [ ! -f /tmp/.mounted ] ; then
     mount -t tmpfs none /tmp && touch /tmp/.mounted
@@ -28,6 +28,9 @@ if [ -e /data/debian/dev/.dev-not-mounted ] ; then
    mount -o bind /dev/pts /data/debian/dev/pts
    mount -o bind /dev/shm /data/debian/dev/shm
    for x in system data cache factory d acct; do
+       if test ! -e /$x; then
+           continue
+       fi
        mkdir -p /data/debian/android/$x
        mount -o bind /$x /data/debian/android/$x
    done
@@ -49,7 +52,7 @@ if test ! -e /data/debian/second-stage-done; then
     touch /data/debian/second-stage-done
 fi
 touch /data/debian/home/bhj/.config/sudo-ssh
-chroot /data/debian /usr/sbin/service ssh start
+chroot /data/debian /etc/init.d/ssh start
 
 # mount sd in debian
 if [ -d /mnt/sdcard/Android ] && [ -d /data/debian/mnt/sdcard/ ] && [ ! -d /data/debian/mnt/sdcard/Android ] ;
