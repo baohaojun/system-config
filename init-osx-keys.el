73600b42dc0851d1259f00ed2d662e045ac218dd
Bao Haojun <baohaojun@gmail.com>
2014-02-10 21:25:17 +0800

    init-osx-keys.el: emacs mac keybinding
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- init-osx-keys.el
+++ init-osx-keys.el
@@ -1,14 +1,9 @@
 (when *is-a-mac*
-  (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'none)
   (setq default-input-method "MacOSX")
   (define-key key-translation-map "\e[21~" [f10])
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(0.001))
-  (global-set-key (kbd "M-`") 'ns-next-frame)
-  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
-  (after-load 'nxml-mode
-    (define-key nxml-mode-map (kbd "M-h") nil))
+  (global-set-key (kbd "s-`") 'ns-next-frame)
   (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
   )
 
