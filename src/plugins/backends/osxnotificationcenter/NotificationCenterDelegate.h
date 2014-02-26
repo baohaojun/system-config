e12c41781a678060506d9580bdda40f6e8fbd9a5
kevin.urban <kevin.urban@stud.unibas.ch>
2014-02-26 23:05:34 +0100

    removed unnecessary files

--- src/plugins/backends/osxnotificationcenter/NotificationCenterDelegate.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Foundation/NSUserNotification.h>
-
-@interface NotificationCenterDelegate : NSObject <NSUserNotificationCenterDelegate> {
-	// no instance variables
-}
-- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification;
-@end
