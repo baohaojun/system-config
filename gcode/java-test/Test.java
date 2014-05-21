cd3319a29467761332ca82926b47ae7aac27333d
Bao Haojun <baohaojun@gmail.com>
2014-05-21 23:27:09 +0800

    .emacs_d/yasnippet/snippets/sh-mode/ifx: if set -x test gcode/java-test/Test.java: regexp test
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- gcode/java-test/Test.java
+++ gcode/java-test/Test.java
@@ -1,5 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
     public static void main(String[] args) {
         System.out.printf("hello %n");
+        Pattern p = Pattern.compile("hello world (\\d+).*");
+        Matcher m = p.matcher("hello world 2222\n");
+        if (m.matches()) {
+            System.out.printf("hello %n");
+        }
     }
 }
