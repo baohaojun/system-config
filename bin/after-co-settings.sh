58e6c819fb7c666d1cb3438f4ea348353faccf94
Bao Haojun <baohaojun@gmail.com>
2014-05-22 18:29:10 +0800

    .mac-hotkey.rc: use my custom version of Emacs bin/after-co-settings.sh: add starttls as an option bin/mailx: allow the send as option
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/after-co-settings.sh
+++ bin/after-co-settings.sh
@@ -53,7 +53,7 @@ ask-for-settings mail "email address" \
 
 ask-for-settings smtp "email smtp server address"
 ask-for-settings smtp-port "email smtp server port"
-ask-for-settings conn-type "connection type" -s nil -s ssl
+ask-for-settings conn-type "connection type" -s nil -s ssl -s starttls
 ask-for-settings 花名 花名
 ask-for-settings mach "Machine type" $(for x in ~/etc/hardware-mach/*; do echo -s $(basename $x); done)
 
