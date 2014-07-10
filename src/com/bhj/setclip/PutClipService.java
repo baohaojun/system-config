5be4f6431ef7d682e6549d38a54d85d47df4485b
Bao Haojun <baohaojun@gmail.com>
2014-07-10 19:10:02 +0800

    .emacs_d/yasnippet/snippets/sh-mode/ll: log log bin/debug-args: debugging arguments in linux bin/draw-notification-with-sawfish: add judge if the icon exist bin/overide/adb: add logcat -c gcode/SetClip/AndroidManifest.xml: add permission gcode/SetClip/src/com/bhj/setclip/PutClipService.java: make it more reliable
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- src/com/bhj/setclip/PutClipService.java
+++ src/com/bhj/setclip/PutClipService.java
@@ -4,7 +4,9 @@ import android.app.Service;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileReader;
@@ -20,22 +22,23 @@ public class PutClipService extends Service {
     public int onStartCommand(Intent intent,  int flags,  int startId)  {
         try {
             if (intent.getIntExtra("getclip", 0) == 1) {
-                FileWriter f = new FileWriter(new File("/sdcard/putclip.txt.1"));
+                FileWriter f = new FileWriter(new File(Environment.getExternalStorageDirectory(), "putclip.txt.1"));
                 ClipboardManager mClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
                 String str = mClipboard.getPrimaryClip().getItemAt(0).getText().toString();
                 f.write(str);
                 f.close();
-                File txt = new File("/sdcard/putclip.txt.1");
-                txt.renameTo(new File("/sdcard/putclip.txt"));
+                File txt = new File(Environment.getExternalStorageDirectory(), "putclip.txt.1");
+                txt.renameTo(new File(Environment.getExternalStorageDirectory(), "putclip.txt"));
             } else {
-                FileReader f = new FileReader(new File("/sdcard/putclip.txt"));
+                FileReader f = new FileReader(new File(Environment.getExternalStorageDirectory(), "putclip.txt"));
                 char[] buffer = new char[1024 * 1024];
                 int n = f.read(buffer);
                 String str = new String(buffer, 0, n);
+                Log.e("bhj", String.format("%s:%d: str is %s", "PutClipService.java", 36, str));
                 ClipboardManager mClipboard;
                 mClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
                 mClipboard.setPrimaryClip(ClipData.newPlainText("Styled Text", str));
-                File putclip = new File("/sdcard/putclip.txt");
+                File putclip = new File(Environment.getExternalStorageDirectory(), "putclip.txt");
                 putclip.delete();
             }
         } catch (Exception e) {
