33b2f5a0c63ddcb1277348807185ce3eb41e673a
Bao Haojun <baohaojun@gmail.com>
2014-07-14 13:27:33 +0800

    build-win.sh: make it compile for 32 bit using mingw
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- build-win.sh
+++ build-win.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 
+function copy-dlls()
+{
+    for x in icudt52.dll icuin52.dll icuuc52.dll QT5CORE.DLL QT5GUI.DLL QT5WIDGETS.DLL; do
+        rsync $1/bin/$x ./release -av
+        chmod 555 ./release/$x
+    done
+
+    for x in libEGL.dll libGLESv2.dll libstdc++-6.dll libwinpthread-1.dll; do
+        rsync $1/bin/$x ./release -av || continue
+        chmod 555 ./release/$x
+    done
+
+    mkdir -p ./release/platforms
+    rsync $1/plugins/platforms/qwindows.dll ./release/platforms -av
+    chmod 555 ./release/platforms/*
+    rsync ~/adb_usb_driver_smartisan ./release -av
+}
+
+function make-release-tgz()
+{
+    rm -rf ./release/*.obj
+    rm -rf ./release/*.o
+    rm -rf ./release/*.cpp
+    rm -rf t1wrench-release
+    cp -av release t1wrench-release
+    tar czfv t1wrench-release.tgz t1wrench-release
+}
+
 if test $# = 0; then
     set -- pub
 fi
 if test $(uname) = Linux; then
-    psync $1 .; ssh $1 "cd $PWD; ./build-win.sh"
+    psync $1 .; ssh $1 "cd $PWD; ./$(basename $0)"
 else
-    perl -npe "$(grep 's/.*ord' ~/bin/vc-utf8-escape)" -i t1wrenchmainwindow.cpp
-    /c/Qt/5.3/msvc2013_64/bin/qmake.exe
     set -e
     set -o pipefail
     /cygdrive/c/Python2/python.exe "C:\cygwin64\home\bhj\system-config\bin\windows\terminateModule.py" T1Wrench || true
-    vc2013env nmake | perl -npe 's/\\/\//g'
-    for x in icudt52.dll icuin52.dll icuuc52.dll libEGL.dll libGLESv2.dll QT5CORE.DLL QT5GUI.DLL QT5WIDGETS.DLL; do
-        rsync /c/Qt/5.3/msvc2013_64/bin/$x ./release -av
-        chmod 555 ./release/$x
-    done
-    mkdir -p ./release/platforms
-    rsync /c/Qt/5.3/msvc2013_64/plugins/platforms/qwindows.dll ./release/platforms -av
-    chmod 555 ./release/platforms/*
-    rsync ~/vcredist_x64.exe ./release/vcredist-vs2013-x86_64.exe
-    rsync ~/adb_usb_driver_smartisan ./release -av
-    rm ./release/*.obj
-    rm ./release/*.cpp
-    rm t1wrench-release -rf
-    mv release t1wrench-release
-    tar czfv t1wrench-release.tgz t1wrench-release
+
+    if test $(basename $0) = build-win.sh; then
+        perl -npe "$(grep 's/.*ord' ~/bin/vc-utf8-escape)" -i t1wrenchmainwindow.cpp
+        /c/Qt/5.3/msvc2013_64/bin/qmake.exe
+
+        vc2013env nmake | perl -npe 's/\\/\//g'
+        copy-dlls /c/Qt/5.3/msvc2013_64/
+        rsync ~/vcredist_x64.exe ./release/vcredist-vs2013-x86_64.exe
+        make-release-tgz
+    else
+        /c/Qt-mingw/./Qt5.3.1/5.3/mingw482_32/bin/qmake.exe
+        export PATH=/cygdrive/c/Qt-mingw/Qt5.3.1/5.3/mingw482_32/bin:/c/Qt-mingw/Qt5.3.1/Tools/mingw482_32/bin:$PATH
+        mingw32-make.exe | perl -npe 's/\\/\//g'
+        copy-dlls /c/Qt-mingw/./Qt5.3.1/5.3/mingw482_32
+        make-release-tgz
+    fi
 fi
