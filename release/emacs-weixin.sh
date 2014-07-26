c1bc4489398066e8281d9f7d7061a079b83a8396
Bao Haojun <baohaojun@gmail.com>
2014-07-26 11:50:55 +0800

    release/emacs-weixin.sh: fix adb server locking weixin
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- release/emacs-weixin.sh
+++ release/emacs-weixin.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if ls -l /proc/$(ps.pl adb.fork-server|pn 1)/fd | grep '\.logs.*\.lock'; then
-    ps-killall adb.fork-server
-    adb devices
-fi
-
 export USE_BUFFER_NAME=send-to-$(basename $0).org
 (
     if ! flock -n 9; then
@@ -22,54 +17,57 @@ export USE_BUFFER_NAME=send-to-$(basename $0).org
         fi
         (
             flock 10
-            putclip-android "$input"&
+            (
+                exec 9>/dev/null 10>&9
+                putclip-android "$input"&
 
-            case $(basename $0) in
-                google+) # send google plus
-                    adb-tap 560 500 # get rid of the pictures
-                    adb-tap 560 1840
-                    adb-long-press 99 383 # long press
-                    adb-tap 497 281 # paste
-                    adb-tap 985 935 # send
-                    ;;
-                t1-putclip)
-                    # do nothing, I will post it myself
-                    ;;
-                weibo) # send weibo
-                    adb-key SPACE
-                    adb-long-press 440 281
-                    adb-tap 545 191
-                    adb-tap 991 166
-                    ;;
-                t1-sms) # quick reply sms
-                    adb-tap 560 1840 # 点空格
-                    adb-long-press 522 912 # 长按输入框
-                    adb-tap 480 802
-                    adb-tap 864 921
-                    ;;
-                cell-mail) # reply mail
-                    adb-swipe 586 878 586 268 500
-                    adb-tap 560 1840 #
-                    adb-tap-2 299 299
-                    adb-tap 505 192
-                    adb-tap 998 174
-                    ;;
-                weixin-new) # weixin friends
-                    adb-key SPACE
-                    adb-tap 303 335
-                    adb-tap 303 335
-                    adb-tap 496 196
-                    adb-tap 989 183
-                    ;;
-                *) # most cases
-                    adb-tap 560 1840 # 点一下底部输入框，弹出软键盘
-                    sleep .1
-                    adb-tap 560 1840 # 再点一下，可能在出键盘，需要输入一个空格
-                    adb-tap-2 560 976 # 双击输入框
-                    adb-tap 525 855 # 点一下粘贴钮
-                    adb-tap 976 976 # 点一下发送钮
-                    ;;
-            esac
+                case $(basename $0) in
+                    google+) # send google plus
+                        adb-tap 560 500 # get rid of the pictures
+                        adb-tap 560 1840
+                        adb-long-press 99 383 # long press
+                        adb-tap 497 281 # paste
+                        adb-tap 985 935 # send
+                        ;;
+                    t1-putclip)
+                        # do nothing, I will post it myself
+                        ;;
+                    weibo) # send weibo
+                        adb-key SPACE
+                        adb-long-press 440 281
+                        adb-tap 545 191
+                        adb-tap 991 166
+                        ;;
+                    t1-sms) # quick reply sms
+                        adb-tap 560 1840 # 点空格
+                        adb-long-press 522 912 # 长按输入框
+                        adb-tap 480 802
+                        adb-tap 864 921
+                        ;;
+                    cell-mail) # reply mail
+                        adb-swipe 586 878 586 268 500
+                        adb-tap 560 1840 #
+                        adb-tap-2 299 299
+                        adb-tap 505 192
+                        adb-tap 998 174
+                        ;;
+                    weixin-new) # weixin friends
+                        adb-key SPACE
+                        adb-tap 303 335
+                        adb-tap 303 335
+                        adb-tap 496 196
+                        adb-tap 989 183
+                        ;;
+                    *) # most cases
+                        adb-tap 560 1840 # 点一下底部输入框，弹出软键盘
+                        sleep .1
+                        adb-tap 560 1840 # 再点一下，可能在出键盘，需要输入一个空格
+                        adb-tap-2 560 976 # 双击输入框
+                        adb-tap 525 855 # 点一下粘贴钮
+                        adb-tap 976 976 # 点一下发送钮
+                        ;;
+                esac
+            )
         ) 10> ~/.logs/$(basename $0).lock-send &
     done
 ) 9> ~/.logs/$(basename $0).lock
