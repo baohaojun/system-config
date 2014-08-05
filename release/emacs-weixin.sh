b56b46002dbd67ed11b5c77d32b9f5a56e1ed1a0
Bao Haojun <baohaojun@gmail.com>
2014-08-05 23:32:48 +0800

    send to both weixin and weibo
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- release/emacs-weixin.sh
+++ release/emacs-weixin.sh
@@ -21,52 +21,78 @@ export USE_BUFFER_NAME=send-to-$(basename $0).org
                 exec 9>/dev/null 10>&9
                 putclip-android "$input"&
 
-                case $(basename $0) in
-                    google+) # send google plus
-                        adb-tap 560 500 # get rid of the pictures
-                        adb-tap 560 1840
-                        adb-long-press 99 383 # long press
-                        adb-tap 497 281 # paste
-                        adb-tap 985 935 # send
-                        ;;
-                    t1-putclip)
-                        # do nothing, I will post it myself
-                        ;;
-                    weibo) # send weibo
-                        adb-key SPACE
-                        adb-long-press 440 281
-                        adb-tap 545 191
-                        adb-tap 991 166
-                        ;;
-                    t1-sms) # quick reply sms
-                        adb-tap 560 1840 # 点空格
-                        adb-long-press 522 912 # 长按输入框
-                        adb-tap 480 802
-                        adb-tap 864 921
-                        ;;
-                    cell-mail) # reply mail
-                        adb-swipe 586 878 586 268 500
-                        adb-tap 560 1840 #
-                        adb-tap-2 299 299
-                        adb-tap 505 192
-                        adb-tap 998 174
-                        ;;
-                    weixin-new) # weixin friends
-                        adb-key SPACE
-                        adb-tap 303 335
-                        adb-tap 303 335
-                        adb-tap 496 196
-                        adb-tap 989 183
-                        ;;
-                    *) # most cases
-                        adb-tap 560 1840 # 点一下底部输入框，弹出软键盘
-                        sleep .1
-                        adb-tap 560 1840 # 再点一下，可能在出键盘，需要输入一个空格
-                        adb-tap-2 560 976 # 双击输入框
-                        adb-tap 525 855 # 点一下粘贴钮
-                        adb-tap 976 976 # 点一下发送钮
-                        ;;
-                esac
+                function emacs-cell-phone() {
+                    case "$1" in
+                        google+) # send google plus
+                            adb-tap 560 500 # get rid of the pictures
+                            adb-tap 560 1840
+                            adb-long-press 99 383 # long press
+                            adb-tap 497 281 # paste
+                            adb-tap 985 935 # send
+                            ;;
+                        t1-putclip)
+                            # do nothing, I will post it myself
+                            ;;
+                        weibo) # send weibo
+                            adb-key SPACE
+                            adb-long-press 440 281
+                            adb-tap 545 191
+                            adb-tap 991 166
+                            ;;
+                        t1-sms) # quick reply sms
+                            adb-tap 560 1840 # 点空格
+                            adb-long-press 522 912 # 长按输入框
+                            adb-tap 480 802
+                            adb-tap 864 921
+                            ;;
+                        cell-mail) # reply mail
+                            adb-swipe 586 878 586 268 500
+                            adb-tap 560 1840 #
+                            adb-tap-2 299 299
+                            adb-tap 505 192
+                            adb-tap 998 174
+                            ;;
+                        both-weixin-and-weibo)
+                            emacs-cell-phone weibo-brand-new
+                            emacs-cell-phone weixin-brand-new
+                            ;;
+                        weibo-brand-new)
+                            adb am start -n com.sina.weibo/.MainTabActivity
+                            sleep .5
+                            putclip-android "$input"&
+                            adb-tap-mid-bot
+                            adb-tap 193 924
+                            emacs-cell-phone weixin-new
+                            ;;
+                        weixin-brand-new)
+                            adb am start -n com.tencent.mm/.ui.LauncherUI
+                            sleep .5
+                            putclip-android "$input"&
+                            adb-tap-2 141 178
+                            adb-tap-2 141 178
+                            adb-tap 510 290
+                            adb-tap 347 487
+                            adb-long-press 947 186
+                            emacs-cell-phone weixin-new
+                            ;;
+                        weixin-new) # weixin friends
+                            adb-key SPACE
+                            adb-tap 303 335
+                            adb-tap 303 335
+                            adb-tap 496 196
+                            adb-tap 989 183
+                            ;;
+                        *) # most cases
+                            adb-tap 560 1840 # 点一下底部输入框，弹出软键盘
+                            sleep .1
+                            adb-tap 560 1840 # 再点一下，可能在出键盘，需要输入一个空格
+                            adb-tap-2 560 976 # 双击输入框
+                            adb-tap 525 855 # 点一下粘贴钮
+                            adb-tap 976 976 # 点一下发送钮
+                            ;;
+                    esac
+                }
+                emacs-cell-phone $(basename $0)
             )
         ) 10> ~/.logs/$(basename $0).lock-send &
     done
