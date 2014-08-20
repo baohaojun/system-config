4b68280115c7a273d8431ccb0945ca8e20958134
Patrick von Reth <vonreth@kde.org>
2014-08-20 13:48:16 +0200

    depend on ecm, drop kde stuff

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,12 @@
 
 #include <QApplication>
 
-
-
-#ifdef HAVE_KDE5
-#include <KAboutData>
-#include <KI18n/KLocalizedString>
-#endif
-
-
-
 int main ( int argc, char *argv[] )
 {
     QApplication app ( argc, argv );
     app.setApplicationName("SnoreNotify");
     app.setOrganizationName("SnoreNotify");
     app.setApplicationVersion(Snore::Version::version());
-
-#ifdef HAVE_KDE5
-    KAboutData about("SnoreNotify","SnoreNotify",Snore::Version::version(),
-                     i18n("A notification deamon."),KAboutLicense::LGPL_V3, i18n("Copyright (c) 2010-2014 Patrick von Reth <vonreth@kde.org>"));
-
-    KAboutData::setApplicationData(about);
-#endif
 
     SnoreNotify sn;
     return app.exec();
