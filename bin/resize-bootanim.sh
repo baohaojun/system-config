da1ca3dea968af79085e4ec21630ac046b3d9874
Bao Haojun <baohaojun@gmail.com>
2014-09-28 17:57:22 +0800

    bin/git-interactive-add: should not use HEAD as the default bin/git-interactive-add: fix for chinese file name bin/git-interactive-add: fix chinese caused double quote
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/resize-bootanim.sh
+++ bin/resize-bootanim.sh
@@ -7,5 +7,5 @@ EOF
 for x in $(find . -name '*.jpg'); do convert -resize $1x$2 $x $x; done
 
 7z a -mx=0 bootanimation.zip desc.txt $(find part*)
-adb_push  bootanimation.zip /system/media/
+adb-push  bootanimation.zip /system/media/
 adb shell bootanimation
