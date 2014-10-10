1e30c05062cb670b28c19a466184e1f8551a5a05
Bao Haojun <baohaojun@gmail.com>
2014-10-10 17:32:08 +0800

    bin/android-generate-ota-from-cm-release: do not stop the ota with ssh shell bin/ask-to-sync-org: do not start it multiple times bin/emacs-get-android-clipboard: make 000 copied into clipboard bin/emoji-to-unicode.pl: add the emojis for weixin and qq bin/get-stdin: make 000 putclip-able bin/putclip-android: use file, do not use cmdline argument, because 000 can't be encoded on cli bin/weixin-all-emojis: click all emojis one by one
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/emoji-to-unicode.pl
+++ bin/emoji-to-unicode.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
 
+# [Smile][Grimace][Drool][Scowl][CoolGuy][Sob][Shy][Silent][Sleep][Cry][Awkward][Angry][Tongue][Grin][Surprise][Frown][Ruthless][Blush][Scream][Puke][Chuckle][Joyful][Slight][Smug][Hungry][Drowsy][Panic][Sweat][Laugh][Commando][Determined][Scold][Shocked][Shhh][Dizzy][Tormented][Toasted][Skull][Hammer][Wave][Speechless][NosePick][Clap][Shame][Trick][Bah ! L][Bah ! R][Yawn][Pooh-pooh][Shrunken][TearingUp][Sly][Kiss][Wrath][Whimper][Cleaver][Watermelon][Beer][Basketball][PingPong][Coffee][Rice][Pig][Rose][Wilt][Lips][Heart][BrokenHeart][Cake][Lightning][Bomb][Dagger][Soccer][Ladybug][Poop][Moon][Sun][Gift][Hug][Strong][Weak][Shake][Peace][Fight][Beckon][Fist][Pinky][RockOn][NO][OK][InLove][Blowkiss][Waddle][Tremble][Aaagh][Twirl][Kotow][Dramatic][JumpRope][Surrender][Exciting][HipHot][ShowLove][Tai Chi L][Tai Chi R]
+# /微笑/撇嘴/色/发呆/得意/流泪/害羞/闭嘴/睡/大哭/尴尬/发怒/调皮/呲牙/惊讶/难过/酷/冷汗/抓狂/吐/偷笑/可爱/白眼/傲慢/饥饿/困/惊恐/流汗/憨笑/大兵/奋斗/咒骂/疑问/嘘/晕/折磨/衰/骷髅/敲打/再见/擦汗/抠鼻/鼓掌/糗大了/坏笑/左哼哼/右哼哼/哈欠/鄙视/委屈/快哭了/阴险/亲亲/吓/可怜/菜刀/西瓜/啤酒/篮球/乒乓/咖啡/饭/猪头/玫瑰/凋谢/示爱/爱心/心碎/蛋糕/闪电/炸弹/刀/足球/瓢虫/便便/月亮/太阳/礼物/拥抱/强/弱/握手/胜利/抱拳/勾引/拳头/差劲/爱你/NO/OK/爱情/飞吻/跳跳/发抖/怄火/转圈/磕头/回头/跳绳/挥手/激动/街舞/献吻/左太极/右太极
+# /::)/::~/::B/::|/:8-)/::</::$/::X/::Z/::'(/::-|/::@/::P/::D/::O/::(/::+/:--b/::Q/::T/:,@P/:,@-D/::d/:,@o/::g/:|-)/::!/::L/::>/::,@/:,@f/::-S/:?/:,@x/:,@@/::8/:,@!/:!!!/:xx/:bye/:wipe/:dig/:handclap/:&-(/:B-)/:<@/:@>/::-O/:>-|/:P-(/::'|/:X-)/::*/:@x/:8*/:pd/:<W>/:beer/:basketb/:oo/:coffee/:eat/:pig/:rose/:fade/:showlove/:heart/:break/:cake/:li/:bome/:kn/:footb/:ladybug/:shit/:moon/:sun/:gift/:hug/:strong/:weak/:share/:v/:@)/:jj/:@@/:bad/:lvu/:no/:ok/:love/:<L>/:jump/:shake/:<O>/:circle/:kotow/:turn/:skip/:oY/:#-0/:hiphot/:kiss/:<&/:&>
+
 use Getopt::Long;
 my $debug;
 GetOptions(
