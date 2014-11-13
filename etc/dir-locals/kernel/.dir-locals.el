c3d2b1a634d925c7c1b97fc476371fa9258cccf9
Bao Haojun <baohaojun@gmail.com>
2014-11-13 20:55:52 +0800

    bin/screenshot: can do it after switching window etc/custom.el: can do kernel programming now
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ etc/dir-locals/kernel/.dir-locals.el
@@ -0,0 +1,13 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  (flycheck-mode . nil)
+  (ac-clang-flags
+   "-include"
+   "/home/bhj/src/linux/include/generated/autoconf.h"
+   "-I/home/bhj/src/linux/arch/arm/mach-msm/include/"
+   "-I/home/bhj/src/linux/include/"
+   "-I/home/bhj/src/linux/arch/arm/include/"
+   "-I/home/bhj/src/linux/arch/arm/include/generated/"
+   "-I.")))
