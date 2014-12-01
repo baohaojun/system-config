9d7c49bfb68f708eb8b37f851d44092a55e043ff
Bao Haojun <baohaojun@gmail.com>
2014-12-01 15:52:57 +0800

    add a script to discard a build
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- commit-all.sh
+++ commit-all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-for x in ~/src/github/T1Wrench-linux ~/src/github/T1Wrench-macos/T1Wrench.app/Contents/MacOS/ ~/src/github/T1Wrench-windows; do
+set -e
+for x in ~/src/github/T1Wrench-linux ~/src/github/T1Wrench-macos/ ~/src/github/T1Wrench-windows; do
     (
         cd $x
+        if test $(basename $0) = reset-all.sh; then
+            git reset --hard origin/$(basename $PWD)
+            exit
+        fi
         git diff HEAD^^ --name-status
         git push >/dev/null 2>&1 && git st&
     )
