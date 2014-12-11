6c34afce2cde05ea4f8c591c5a82700ec7fe9f38
Bao Haojun <baohaojun@gmail.com>
2014-12-11 21:52:31 +0800

    bin/Linux/config/pkgs/libltdl-dev: make sure gtags can build on ubuntu bin/toggle-dvp-us.sh: make sure my keys are ok
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/toggle-dvp-us.sh
+++ bin/toggle-dvp-us.sh
@@ -4,11 +4,9 @@ set -x
     flock 9
     if setxkbmap -query | grep 'variant:\s+dvp' -Pq; then
         setxkbmap -layout us
-        xmodmap -e 'pointer = 1 2 3 4 5 6 7 8 9 10'
-        sawfish-ask-for-input -p 'Switched to qwerty, press any key to continue: '
+        xmodmap ~/system-config/etc/hardware-mach/.Xmodmap-undo
     else
         setxkbmap -layout us -variant dvp
-        re-xmodmap 2>&1 | cat
-        sawfish-ask-for-input -p 'Switched to dvp, press any key to continue: '
+        xmodmap ~/system-config/etc/hardware-mach/.Xmodmap
     fi
 ) 9> ~/.logs/$(basename $0).lock
