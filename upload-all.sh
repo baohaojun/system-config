5742036e7449508b40ae251f7b8c3d788589fc8e
Bao Haojun <baohaojun@gmail.com>
2014-12-13 19:40:47 +0800

    upload-all.sh: add a script to upload to my phone, to baidu wangpan
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ upload-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/tmp
+adb-push  T1Wrench-linux.tgz T1Wrench-macos.zip T1Wrench-windows.zip /sdcard/0/
+adb-tap 544 346
+adb-tap 489 1203
+adb-tap 236 613
+adb-tap 985 387
+adb-tap 951 635
+adb-tap 1001 805
+adb-tap 755 1841
