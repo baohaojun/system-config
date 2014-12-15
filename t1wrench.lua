2ecd40ea39410dd18a870ac5462b90cf40be49f3
Bao Haojun <baohaojun@gmail.com>
2014-12-15 20:52:44 +0800

    t1wrench.lua: fix emoji for weixin
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- t1wrench.lua
+++ t1wrench.lua
@@ -9,7 +9,7 @@ local adb_get_input_window_dump, adb_top_window
 local adb_start_weixin_share, adb_is_window
 local adb_focused_window
 local t1_config, check_phone
-local emoji_for_qq, debug, get_a_note
+local emoji_for_qq, debug, get_a_note, emoji_for_weixin, emoji_for_qq_or_weixin
 local adb_get_last_pic
 -- variables
 local using_scroll_lock = true
@@ -27,23 +27,26 @@ local using_xiaomi_os = false
 local using_oppo_os = false
 local brand = "smartisan"
 local model = "T1"
-local qq_emojis
+local qq_emojis, weixin_emojis
 local sdk_version = 19
 local emojis, emojis_map
 local the_true_adb = "./the-true-adb"
 
 local qq_emoji_table = {
-   "微笑", "撇嘴", "色", "发呆", "得意", "流泪", "害羞", "闭嘴", "睡", "大哭",
-   "尴尬", "发怒", "调皮", "呲牙", "惊讶", "难过", "酷", "冷汗", "抓狂", "吐",
-   "偷笑", "可爱", "白眼", "傲慢", "饥饿", "困", "惊恐", "流汗", "憨笑", "大兵",
-   "奋斗", "咒骂", "疑问", "嘘", "晕", "折磨", "衰", "骷髅", "敲打", "再见",
-   "擦汗", "抠鼻", "鼓掌", "糗大了", "坏笑", "左哼哼", "右哼哼", "哈欠", "鄙视",
-   "委屈", "快哭了", "阴险", "亲亲", "吓", "可怜", "菜刀", "西瓜", "啤酒",
-   "篮球", "乒乓", "咖啡", "饭", "猪头", "玫瑰", "凋谢", "示爱", "爱心", "心碎",
-   "蛋糕", "闪电", "炸弹", "刀", "足球", "瓢虫", "便便", "月亮", "太阳", "礼物",
-   "拥抱", "强", "弱", "握手", "胜利", "抱拳", "勾引", "拳头", "差劲", "爱你",
-   "NO", "OK", "爱情", "飞吻", "跳跳", "发抖", "怄火", "转圈", "磕头", "回头",
-   "跳绳", "挥手", "激动", "街舞", "献吻", "左太极", "右太极",
+   "微笑", "撇嘴", "色", "发呆", "得意", "流泪", "害羞", "闭嘴",
+   "睡", "大哭", "尴尬", "发怒", "调皮", "呲牙", "惊讶", "难过",
+   "酷", "冷汗", "抓狂", "吐", "偷笑", "可爱", "白眼", "傲慢",
+   "饥饿", "困", "惊恐", "流汗", "憨笑", "大兵", "奋斗", "咒骂",
+   "疑问", "嘘", "晕", "折磨", "衰", "骷髅", "敲打", "再见",
+   "擦汗", "抠鼻", "鼓掌", "糗大了", "坏笑", "左哼哼", "右哼哼", "哈欠",
+   "鄙视", "委屈", "快哭了", "阴险", "亲亲", "吓", "可怜", "菜刀",
+   "西瓜", "啤酒", "篮球", "乒乓", "咖啡", "饭", "猪头", "玫瑰",
+   "凋谢", "示爱", "爱心", "心碎", "蛋糕", "闪电", "炸弹", "刀",
+   "足球", "瓢虫", "便便", "月亮", "太阳", "礼物", "拥抱", "强",
+   "弱", "握手", "胜利", "抱拳", "勾引", "拳头", "差劲", "爱你",
+   "NO", "OK", "爱情", "飞吻", "跳跳", "发抖", "怄火", "转圈",
+   "磕头", "回头", "跳绳", "挥手", "激动", "街舞", "献吻", "左太极",
+   "右太极",
 }
 
 for i in ipairs(qq_emoji_table) do
@@ -71,6 +74,10 @@ end
 
 
 emoji_for_qq = function(text)
+   return emoji_for_qq_or_weixin(text, qq_emojis)
+end
+
+emoji_for_qq_or_weixin = function(text, which_emojis)
    local s = 1
    local replace = ""
    repeat
@@ -79,8 +86,12 @@ emoji_for_qq = function(text)
          local emoji = text:sub(fs + 1, fe - 1)
          if qq_emoji_table[emoji] then
             replace = replace .. text:sub(s, fs - 1)
-            local idx = qq_emoji_table[emoji]
-            replace = replace .. qq_emojis[idx]
+            if which_emojis == qq_emojis then
+               local idx = qq_emoji_table[emoji]
+               replace = replace .. which_emojis[idx]
+            else
+               replace = replace .. "/" .. emoji
+            end
             s = fe + 1
          else
             replace = replace .. text:sub(s, fs)
@@ -92,6 +103,10 @@ emoji_for_qq = function(text)
       end
    until s > #text
    return replace
+end
+
+emoji_for_weixin = function(text)
+   return emoji_for_qq_or_weixin(text, weixin_emojis)
 end
 
 local function system(cmds)
@@ -838,6 +853,8 @@ t1_post = function(text) -- use weixin
    if text then
       if window:match("com.tencent.mobileqq") then
          putclip(emoji_for_qq(text))
+      elseif window:match("com.tencent.mm/") then
+         putclip(emoji_for_weixin(text))
       else
          putclip(text)
       end
@@ -1319,6 +1336,21 @@ local function do_it()
       return M
    end
 end
+
+weixin_emojis = {
+   "/::)", "/::~", "/::B", "/::|", "/:8-)", "/::<", "/::$", "/::X", "/::Z", "/::'(",
+   "/::-|", "/::@", "/::P", "/::D", "/::O", "/::(", "/::+", "/:--b", "/::Q", "/::T",
+   "/:,@P", "/:,@-D", "/::d", "/:,@o", "/::g", "/:|-)", "/::!", "/::L", "/::>", "/::,@",
+   "/:,@f", "/::-S", "/:?", "/:,@x", "/:,@@", "/::8", "/:,@!", "/:!!!", "/:xx", "/:bye",
+   "/:wipe", "/:dig", "/:handclap", "/:&-(", "/:B-)", "/:<@", "/:@>", "/::-O", "/:>-|", "/:P-(",
+   "/::'|", "/:X-)", "/::*", "/:@x", "/:8*", "/:pd", "/:<W>", "/:beer", "/:basketb", "/:oo",
+   "/:coffee", "/:eat", "/:pig", "/:rose", "/:fade", "/:showlove", "/:heart", "/:break", "/:cake", "/:li",
+   "/:bome", "/:kn", "/:footb", "/:ladybug", "/:shit", "/:moon", "/:sun", "/:gift", "/:hug", "/:strong",
+   "/:weak", "/:share", "/:v", "/:@)", "/:jj", "/:@@", "/:bad", "/:lvu", "/:no", "/:ok",
+   "/:love", "/:<L>", "/:jump", "/:shake", "/:<O>", "/:circle", "/:kotow", "/:turn", "/:skip", "/:oY",
+   "/:#-0", "/:hiphot", "/:kiss", "/:<&", "/:&>",
+}
+
 qq_emojis = {
 [[]], [[(]], [[]], [[+]], [[]], [[	]], [[]], [[j]],
 [[#]], [[ú]], [[]], [[]], [[]], [[ ]], [[!]], [[ ]],
