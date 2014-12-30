6f5665ec7ff338beecbdc5cdcc3f23f747322737
Bao Haojun <baohaojun@gmail.com>
2014-12-30 20:18:40 +0800

    test mimekit.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- gcode/c#-playground/Hello.cs
+++ gcode/c#-playground/Hello.cs
@@ -1,8 +1,18 @@
+using System.IO;
+using MimeKit;
 class Hello
 {
     static void Main( )
     {
         // Use the system console object
-        System.Console.WriteLine("Hello World");
+            FileStream stream = new FileStream("/home/bhj/Maildir/SentMails/cur/1419597900.M517484P16503Q0.bhj-home:2,S", FileMode.Open);
+            var parser = new MimeParser (stream, MimeFormat.Entity);
+            var message = parser.ParseMessage ();
+
+            FileStream output = new FileStream("/home/bhj/tmp/out.txt", FileMode.Create);
+            foreach (var part in message.BodyParts) {
+                    part.ContentObject.DecodeTo (output);
+                    // do something
+            }
     }
 }
