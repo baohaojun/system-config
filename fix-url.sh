847843c4409a40d7ea0ae1fa360ab8bb0af083dd
Bao Haojun <baohaojun@gmail.com>
2015-01-26 15:01:18 +0800

    add a script to fix the url.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ fix-url.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+perl -npe 's!但是，如果您觉得从github下载太慢的话，可以用百度网盘下载：三个版本的链接都在里面，自取所需吧： .*!但是，如果您觉得从github下载太慢的话，可以用百度网盘下载：三个版本的链接都在里面，自取所需吧： '$1'!' -i /home/bhj/src/github/baohaojun.github.com/blog/2014/12/01/0-T1Wrench-2.0-Usage-Guide.org
