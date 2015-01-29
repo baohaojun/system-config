7769a8952451f86981007a6813765c6130c6ad74
Bao Haojun <baohaojun@gmail.com>
2015-01-29 13:16:38 +0800

    Add some comments and autoload.
    As purcell advised for melpa packaging.
    
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bbyac.el
+++ bbyac.el
@@ -29,16 +29,27 @@
 
 ;;; Commentary:
 
+;; The Bit Bang You Are Completed package is Yet Another Completion
+;; method.
+
+;; Say you have a Lisp variable named
+;; this-is-a-very-long-variable-for-example, in the buffer in which
+;; this name is already typed, you can type tvvf and then press M-g
+;; <return> to complete it.  You can type tvx or even tx for the same
+;; effect, if you like.
+
+;; There are also other completing methods, for e.g., to complete
+;; arbitrary strings, whole lines, sexp and paragraphs. The last two
+;; is not as useful as the others.
+
 ;; For more information see the readme at:
 ;; https://github.com/baohaojun/bbyac
 
 ;;; Code:
 
 (require 'ecomplete)
-
 (require 'cl-lib)
 (require 'thingatpt)
-(require 'ecomplete)
 (require 'browse-kill-ring)
 
 (defgroup bbyac nil
@@ -540,12 +551,14 @@ This func is copied and modified from `ecomplete-display-matches'."
 (define-key bbyac-mode-map (kbd "M-s s") 'bbyac-expand-sexp)
 (define-key bbyac-mode-map (kbd "M-s p") 'bbyac-expand-paragraph)
 
+;;;###autoload
 (define-minor-mode bbyac-mode
   "Toggle the `bbyac-mode' minor mode."
   :lighter " BBC"
   :keymap bbyac-mode-map
   :group 'bbyac)
 
+;;;###autoload
 (define-globalized-minor-mode bbyac-global-mode
   bbyac-mode
   turn-on-bbyac-mode)
