0e8556c383a6ecd02195c1dd78f4ef5e3254ec1a
Bao Haojun <baohaojun@gmail.com>
2015-02-28 16:49:20 +0800

    Util/ExtendedAttribute.cs: fix beagrep for text but mishap mimetype
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- Util/ExtendedAttribute.cs
+++ Util/ExtendedAttribute.cs
@@ -34,142 +34,142 @@ using Mono.Unix.Native;
 
 namespace Beagrep.Util {
 
-	public class ExtendedAttribute {
+        public class ExtendedAttribute {
+                static byte[] xa_buffer = new byte [4096];
 
-		// FIXME: Deprecate post 0.3.3
-		private static string AddPrefix (string name)
-		{
-			return "user.Beagrep." + name;
-		}
+                // FIXME: Deprecate post 0.3.3
+                private static string AddPrefix (string name)
+                {
+                        return "user.Beagrep." + name;
+                }
 
-		private const string AttrName = "user.Beagrep";
+                private const string AttrName = "user.Beagrep";
 
-		static Encoding encoding = new UTF8Encoding ();
+                static Encoding encoding = new UTF8Encoding ();
 
-		// Set a beagrep attribute
-		public static void Set (string path, string value) {
-			Set (path, AttrName, value);
-		}
+                // Set a beagrep attribute
+                public static void Set (string path, string value) {
+                        Set (path, AttrName, value);
+                }
 
-		// Set a non-beagrep attribute
-		private static void Set (string path, string name, string value)
-		{
-			if (! FileSystem.Exists (path))
-				throw new IOException (path);
+                // Set a non-beagrep attribute
+                private static void Set (string path, string name, string value)
+                {
+                        if (! FileSystem.Exists (path))
+                                throw new IOException (path);
 
-			byte[] buffer = encoding.GetBytes (value);
-			int retval = Syscall.lsetxattr (path, name, buffer);
-			if (retval == -1)
-				throw new IOException ("Could not set extended attribute on " + path + ": " + Mono.Unix.Native.Stdlib.strerror (Mono.Unix.Native.Stdlib.GetLastError ()));
-		}
+                        byte[] buffer = encoding.GetBytes (value);
+                        int retval = Syscall.lsetxattr (path, name, buffer);
+                        if (retval == -1)
+                                throw new IOException ("Could not set extended attribute on " + path + ": " + Mono.Unix.Native.Stdlib.strerror (Mono.Unix.Native.Stdlib.GetLastError ()));
+                }
 
-		// Check if a beagrep attribute exists
-		public static bool Exists (string path)
-		{
-			if (! FileSystem.Exists (path))
-				throw new IOException (path);
+                // Check if a beagrep attribute exists
+                public static bool Exists (string path)
+                {
+                        if (! FileSystem.Exists (path))
+                                throw new IOException (path);
 
-			long size = Syscall.lgetxattr (path, AttrName, null, 0);
-			return size >= 0;
-		}
+                        long size = Syscall.lgetxattr (path, AttrName, null, 0);
+                        return size >= 0;
+                }
 
-		// FIXME: Deprecate post 0.3.3
-		public static bool OldExists (string path, string name)
-		{
-			if (! FileSystem.Exists (path))
-				throw new IOException (path);
+                // FIXME: Deprecate post 0.3.3
+                public static bool OldExists (string path, string name)
+                {
+                        if (! FileSystem.Exists (path))
+                                throw new IOException (path);
 
-			name = AddPrefix (name);
+                        name = AddPrefix (name);
 
-			long size = Syscall.lgetxattr (path, name, null, 0);
-			return size >= 0;
-		}
+                        long size = Syscall.lgetxattr (path, name, null, 0);
+                        return size >= 0;
+                }
 
-		// Get a beagrep attribute
-		public static string Get (string path)
-		{
-			return Get (path, AttrName);
-		}
+                // Get a beagrep attribute
+                public static string Get (string path)
+                {
+                        return Get (path, AttrName);
+                }
 
-		// Get a non-beagrep attribute
-		public static string Get (string path, string name)
-		{
-			if (! FileSystem.Exists (path))
-				throw new IOException (path);
+                // Get a non-beagrep attribute
+                public static string Get (string path, string name)
+                {
+                        if (! FileSystem.Exists (path))
+                                throw new IOException (path);
 
-			byte[] buffer;
-			long size = Syscall.lgetxattr (path, name, out buffer);
-			if (size <= 0)
-				return null;
-			return encoding.GetString (buffer);
-		}
+                        long size = Syscall.lgetxattr (path, name, xa_buffer);
+                        if (size <= 0)
+                                return null;
+                        return encoding.GetString (xa_buffer);
+                }
 
-		// Remove a beagrep attribute
-		public static void Remove (string path)
-		{
-			if (! FileSystem.Exists (path))
-				throw new IOException (path);
-			
-			int retval = Syscall.lremovexattr (path, AttrName);
-			if (retval != 0)
-				throw new IOException ("Could not remove extended attribute on " + path + ": " + Mono.Unix.Native.Stdlib.strerror (Mono.Unix.Native.Stdlib.GetLastError ()));
-		}
+                // Remove a beagrep attribute
+                public static void Remove (string path)
+                {
+                        if (! FileSystem.Exists (path))
+                                throw new IOException (path);
 
-		// FIXME: Deprecate post 0.3.3
-		public static void RemoveOld (string path, string name)
-		{
-			Remove (path, AddPrefix (name));
-		}
+                        int retval = Syscall.lremovexattr (path, AttrName);
+                        if (retval != 0)
+                                throw new IOException ("Could not remove extended attribute on " + path + ": " + Mono.Unix.Native.Stdlib.strerror (Mono.Unix.Native.Stdlib.GetLastError ()));
+                }
 
-		// Remove a non-beagrep attribute
-		private static void Remove (string path, string name)
-		{
-			if (! FileSystem.Exists (path))
-				throw new IOException (path);
-			
-			int retval = Syscall.lremovexattr (path, name);
-			if (retval != 0)
-				throw new IOException ("Could not remove extended attribute on " + path + ": " + Mono.Unix.Native.Stdlib.strerror (Mono.Unix.Native.Stdlib.GetLastError ()));
-		}
+                // FIXME: Deprecate post 0.3.3
+                public static void RemoveOld (string path, string name)
+                {
+                        Remove (path, AddPrefix (name));
+                }
 
-		// Check to see if it is possible to get and set attributes on a given file.
-		public static bool Test (string path)
-		{
-			const string test_key = "user.Beagrep.__test_key__";
-			const string test_value = "__test_value__";
+                // Remove a non-beagrep attribute
+                private static void Remove (string path, string name)
+                {
+                        if (! FileSystem.Exists (path))
+                                throw new IOException (path);
 
-			try {
-				Set (path, test_key, test_value);
-				if (Get (path, test_key) != test_value)
-					return false;
-				Remove (path, test_key);
-				return Get (path, test_key) == null;
+                        int retval = Syscall.lremovexattr (path, name);
+                        if (retval != 0)
+                                throw new IOException ("Could not remove extended attribute on " + path + ": " + Mono.Unix.Native.Stdlib.strerror (Mono.Unix.Native.Stdlib.GetLastError ()));
+                }
 
-			} catch {
-				return false;
-			}
-		}
+                // Check to see if it is possible to get and set attributes on a given file.
+                public static bool Test (string path)
+                {
+                        const string test_key = "user.Beagrep.__test_key__";
+                        const string test_value = "__test_value__";
 
-		private static bool ea_support_tested = false;
-		private static bool ea_supported = false;
+                        try {
+                                Set (path, test_key, test_value);
+                                if (Get (path, test_key) != test_value)
+                                        return false;
+                                Remove (path, test_key);
+                                return Get (path, test_key) == null;
 
-		public static bool Supported {
-			get {
-				if (ea_support_tested)
-					return ea_supported;
+                        } catch {
+                                return false;
+                        }
+                }
 
-				if (Environment.GetEnvironmentVariable ("BEAGREP_DISABLE_XATTR") != null) {
-					Logger.Log.Debug ("BEAGREP_DISABLE_XATTR is set");
-					ea_supported = false;
-					ea_support_tested = true;
-					return false;
-				}
+                private static bool ea_support_tested = false;
+                private static bool ea_supported = false;
 
-				ea_supported = Test (PathFinder.HomeDir);
-				ea_support_tested = true;
+                public static bool Supported {
+                        get {
+                                if (ea_support_tested)
+                                        return ea_supported;
 
-				return ea_supported;
-			}
-		}
-	}
+                                if (Environment.GetEnvironmentVariable ("BEAGREP_DISABLE_XATTR") != null) {
+                                        Logger.Log.Debug ("BEAGREP_DISABLE_XATTR is set");
+                                        ea_supported = false;
+                                        ea_support_tested = true;
+                                        return false;
+                                }
+
+                                ea_supported = Test (PathFinder.HomeDir);
+                                ea_support_tested = true;
+
+                                return ea_supported;
+                        }
+                }
+        }
 }
