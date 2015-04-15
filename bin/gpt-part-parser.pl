810d2b8252b9205f305005d7aa01b9e2fd760681
Bao Haojun <baohaojun@gmail.com>
2015-04-15 20:17:55 +0800

    bin/adb-dump-gpt-partition: dump the guid partition bin/gpt-part-parser.pl: can change gpt partition table. bin/gpt-part-parser.pl: print the guid-type too
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/gpt-part-parser.pl
+++ bin/gpt-part-parser.pl
@@ -1,22 +1,189 @@
 #!/usr/bin/perl
 use strict;
 my $gpt;
-while (read(STDIN, $gpt, 128) == 128) {
-    my ($guid_part, $guid_type, $lba1, $lba2, $attr, @name) = unpack("H32H32H16H16H16S36", $gpt);
-    @name = map {chr} grep {$_ != 0} @name;
-    my $name = join('', @name);
-    # { 0xEBD0A0A2, 0xB9E5, 0x4433, {0x87, 0XC0, 0x68, 0XB6, 0XB7, 0x26, 0x99, 0XC7, } };
 
-    my @guid_part;
-    while ($guid_part =~ m/../g) {
-        push @guid_part, $&;
+use Getopt::Long;
+my $partition_name_to_modify;
+my $new_partition_uuid;
+my $output_file;
+GetOptions(
+    "p=s" => \$partition_name_to_modify,
+    "u=s" => \$new_partition_uuid,
+    "o=s" => \$output_file,
+    );
+
+
+read(STDIN, $gpt, 1024 * 1024);
+
+
+my $first_block = substr($gpt, 0, 512);
+
+$gpt = substr($gpt, 512);
+
+my $guid_template = "H32";
+
+#     PACKED struct hotplug_guid
+# {
+#   uint32 data1;
+#   uint16 data2;
+#   uint16 data3;
+#   uint8  data4[8];
+# } PACKED_POST;
+
+my ($sig, $rev, $head_size, $header_crc32, $reserved1, $my_lba, $alternate_lba, $first_usable_lba,
+    $last_usable_lba, $disk_guid, $partition_entry_lba, $number_of_partition_entries, $size_of_partition_entry,
+    $partition_entries_crc32) =
+    unpack("QL4Q4" . $guid_template . "QLLL", $gpt);
+
+printf "sig: %s\n", $sig;
+printf "revision: %08x\n", $rev;
+printf "head_size: %08x\n", $head_size;
+printf "header_crc32: %08x\n", $header_crc32;
+printf "reserved1: %08x\n", $reserved1;
+
+printf "my_lba: %016x\n", $my_lba;
+printf "alt_lba: %016x\n", $alternate_lba;
+printf "first_usable_lba: %016x\n", $first_usable_lba;
+printf "last_usable_lba: %016x\n", $last_usable_lba;
+
+printf "partition_entry_lba: %016x\n", $partition_entry_lba;
+printf "number_of_partition_entries: %08x\n", $number_of_partition_entries;
+printf "size_of_partition_entry: %08x\n", $size_of_partition_entry;
+printf "$partition_entries_crc32: %08x\n", $partition_entries_crc32;
+
+# PACKED struct hotplug_gpt_header
+# {
+#   uint8 signature[GPT_SIGNATURE_SIZE]; /* Should be "EFI PART" */
+#   uint32 revision;    /* Header revision number, this header is 1.0 */
+#   uint32 header_size; /* Size of this header in bytes. */
+#   uint32 header_crc32; /* CRC 32 of this header, set to zero for calculations*/
+#   uint32 reserved1;
+#   uint64 my_lba;  /* Logical Block Address of this header. */
+#   uint64 alternate_lba; /* Logical Block Address of alternate header. */
+#   uint64 first_usable_lba; /* Min LBA for any partition. */
+#   uint64 last_usable_lba; /* Max LBA for any partition. */
+#   struct hotplug_guid disk_guid;
+#   uint64 partition_entry_lba; /* Partiton entry table LBA. */
+#   uint32 number_of_partition_entries; /* Number of entires in partition array*/
+#   uint32 size_of_partition_entry; /* Size of each partition entry. */
+#   uint32 partition_entries_crc32; /* CRC32 of partition entry array. */
+#   /* The rest of the block is reserved. The header size should be 92 bytes. */
+# } PACKED_POST;
+
+
+my @crc32_table = (
+    0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F,
+    0xE963A535, 0x9E6495A3, 0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988,
+    0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91, 0x1DB71064, 0x6AB020F2,
+    0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
+    0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9,
+    0xFA0F3D63, 0x8D080DF5, 0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172,
+    0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B, 0x35B5A8FA, 0x42B2986C,
+    0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59,
+    0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423,
+    0xCFBA9599, 0xB8BDA50F, 0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924,
+    0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D, 0x76DC4190, 0x01DB7106,
+    0x98D220BC, 0xEFD5102A, 0x71B18589, 0x06B6B51F, 0x9FBFE4A5, 0xE8B8D433,
+    0x7807C9A2, 0x0F00F934, 0x9609A88E, 0xE10E9818, 0x7F6A0DBB, 0x086D3D2D,
+    0x91646C97, 0xE6635C01, 0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E,
+    0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457, 0x65B0D9C6, 0x12B7E950,
+    0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65,
+    0x4DB26158, 0x3AB551CE, 0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7,
+    0xA4D1C46D, 0xD3D6F4FB, 0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0,
+    0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9, 0x5005713C, 0x270241AA,
+    0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F,
+    0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81,
+    0xB7BD5C3B, 0xC0BA6CAD, 0xEDB88320, 0x9ABFB3B6, 0x03B6E20C, 0x74B1D29A,
+    0xEAD54739, 0x9DD277AF, 0x04DB2615, 0x73DC1683, 0xE3630B12, 0x94643B84,
+    0x0D6D6A3E, 0x7A6A5AA8, 0xE40ECF0B, 0x9309FF9D, 0x0A00AE27, 0x7D079EB1,
+    0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB,
+    0x196C3671, 0x6E6B06E7, 0xFED41B76, 0x89D32BE0, 0x10DA7A5A, 0x67DD4ACC,
+    0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5, 0xD6D6A3E8, 0xA1D1937E,
+    0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B,
+    0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55,
+    0x316E8EEF, 0x4669BE79, 0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236,
+    0xCC0C7795, 0xBB0B4703, 0x220216B9, 0x5505262F, 0xC5BA3BBE, 0xB2BD0B28,
+    0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D,
+    0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x026D930A, 0x9C0906A9, 0xEB0E363F,
+    0x72076785, 0x05005713, 0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0x0CB61B38,
+    0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0x0BDBDF21, 0x86D3D2D4, 0xF1D4E242,
+    0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777,
+    0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69,
+    0x616BFFD3, 0x166CCF45, 0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2,
+    0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB, 0xAED16A4A, 0xD9D65ADC,
+    0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5, 0x47B2CF7F, 0x30B5FFE9,
+    0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693,
+    0x54DE5729, 0x23D967BF, 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94,
+    0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D
+    );
+
+sub gpt_calc_crc32($$)
+{
+    my($data, $seed) = @_;
+    my @bytes = unpack("C*", $data);
+    my $crc = 0;
+
+    $crc = 0xFFFFFFFF ^ $seed;
+    for my $byte (@bytes) {
+        $crc = ($crc >> 8) ^ $crc32_table[($crc ^ $byte) & 0xff];
+
     }
-    print "$guid_part ";
+    $crc = $crc ^ 0xFFFFFFFF;
+    return $crc;
+}
 
-    $guid_part = sprintf("{ 0x%s, 0x%s, 0x%s, {" . "0x%s, " x 8 . "} };",
-                         join('', reverse @guid_part[0..3]),
-                         join('', reverse @guid_part[4..5]),
-                         join('', reverse @guid_part[6..7]),
-                         @guid_part[8..15]);
-    print "$guid_type : $guid_part : $lba1 : $lba2 : $name\n";
+sub new_gpt_head($) {
+    my ($new_head_crc) = @_;
+    return pack("QL4Q4" . $guid_template . "QLLL",
+                $sig, $rev, $head_size, $new_head_crc, $reserved1, $my_lba, $alternate_lba, $first_usable_lba,
+                $last_usable_lba, $disk_guid, $partition_entry_lba, $number_of_partition_entries, $size_of_partition_entry,
+                $partition_entries_crc32);
+}
+
+my $gpt2 = new_gpt_head(0);
+die "incorrect head crc\n" unless gpt_calc_crc32($gpt2, 0) == $header_crc32;
+
+my $gpt_head_padding = substr($gpt, $head_size, 512 - $head_size);
+
+$gpt = substr($gpt, ($partition_entry_lba - $my_lba) * 512, $number_of_partition_entries * $size_of_partition_entry);
+
+die "incorrect entry crc\n" unless gpt_calc_crc32($gpt, 0) == $partition_entries_crc32;
+
+my $modified_entries = "";
+
+for (1..$number_of_partition_entries) {
+    my $entry = substr($gpt, 0, $size_of_partition_entry);
+    $gpt = substr($gpt, $size_of_partition_entry);
+
+    my ($guid_part, $guid_type, $lba1, $lba2, $attr, @name_arr) = unpack("H32H32H16H16H16S36", $entry);
+    my @name = map {chr} grep {$_ != 0} @name_arr;
+    my $name = join('', @name);
+
+    if ($name eq $partition_name_to_modify) {
+        ($guid_part = $new_partition_uuid) =~ s/-//g;
+    }
+    $modified_entries .= pack("H32H32H16H16H16S36", $guid_part, $guid_type, $lba1, $lba2, $attr, @name_arr);
+
+    $guid_part =~ m/(.{8})(.{4})(.{4})(.{4})(.*)/;
+    print "$name : $1-$2-$3-$4-$5\n";
+
+    $guid_type =~ m/(.{8})(.{4})(.{4})(.{4})(.*)/;
+    print "$name : $1-$2-$3-$4-$5\n";
+
+}
+
+printf "crc2 is %x : %x\n", gpt_calc_crc32($modified_entries, 0), $partition_entries_crc32;
+
+$partition_entries_crc32 = gpt_calc_crc32($modified_entries, 0);
+
+$gpt2 = new_gpt_head(0);
+
+$header_crc32 = gpt_calc_crc32($gpt2, 0);
+
+$gpt2 = new_gpt_head($header_crc32);
+
+if ($output_file) {
+    open(my $file, ">", $output_file) or die "can't open $output_file";
+    print $file "$first_block$gpt2$gpt_head_padding$modified_entries";
+    close($file);
 }
