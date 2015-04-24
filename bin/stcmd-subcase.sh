d356bf373a727f852a2adf60fe5079d420c5ee70
Bao Haojun <baohaojun@gmail.com>
2015-04-24 16:26:33 +0800

    bin/adb-install-debian: try to fix busybox not found issue bin/mailx: fix filename bin/stcmd-subcase.sh: add srem-helper
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/stcmd-subcase.sh
+++ bin/stcmd-subcase.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #adb bash -x stcmd-subcase.sh disable-swap-dump
 my-adb forward tcp:54321 localreserved:scitd
-echo "$@" | nc localhost 54321
+echo srem-helper "$@" | nc localhost 54321
