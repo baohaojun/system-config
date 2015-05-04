948aa79fb2340b883d372f7488400f2b951ef30a
Bao Haojun <baohaojun@gmail.com>
2015-05-04 16:02:42 +0800

    .emacs_d/lisp/bhj-defines.el: add more errors bin/Linux/config/android-build/.list: add bison
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- .emacs_d/lisp/bhj-defines.el
+++ .emacs_d/lisp/bhj-defines.el
@@ -271,7 +271,7 @@ might be bad."
 ;;;###autoload
 (defun bhj-occur-make-errors ()
   (interactive)
-  (let ((bhj-occur-regexp (downcase "\\*\\*\\*.*stop\\|method does not override or implement\\|syntax error\\|invalid argument\\|no such \\|circular.*dropped\\|no rule to\\|failed\\|[0-9]elapsed \\|cannot find symbol\\|error [0-9]\\| : error \\|because of errors\\|[0-9] error\\b\\|error:\\|error while loading shared libraries\\|undefined reference to\\|permission denied")))
+  (let ((bhj-occur-regexp (downcase "\\*\\*\\*.*stop\\|method does not override or implement\\|syntax error\\|invalid argument\\|no such \\|circular.*dropped\\|no rule to\\|failed\\|[0-9]elapsed \\|cannot find symbol\\|error [0-9]\\| : error \\|because of errors\\|[0-9] error\\b\\|error:\\|error while loading shared libraries\\|undefined symbol\\|undefined reference to\\|permission denied")))
     (call-interactively 'bhj-occur)))
 
 ;;;###autoload
