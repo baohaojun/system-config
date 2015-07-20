e304072dda3208d2c7b0d11f1a4ba10624fdb0e2
Patrick von Reth <vonreth@kde.org>
2015-07-20 15:30:52 +0200

    finally fixed escaping

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -2,16 +2,16 @@
 #http://stackoverflow.com/questions/2124753/how-i-can-use-powershell-with-the-visual-studio-command-prompt
 function batCall([string] $path, [string] $arg)
 {
-    Write-Host 'Calling "$path" "$arg"'
-    cmd /c '"$path" "$arg" & set' |
+    Write-Host "Calling `"$path`" `"$arg`""
+    cmd /c  "`"$path`" `"$arg`" & set" |
     foreach {
       if ($_ -match "=") {
+        $v = $_.split("=")
         #Write-Host "ENV:\$($v[0])=$($v[1])"
-        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+        set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
       }
     }
 }
-
 $CMAKE_INSTALL_ROOT="$env:APPVEYOR_BUILD_FOLDER/work/install" -replace "\\", "/"
 
 if ( $env:COMPILER -eq "MINGW" )
@@ -21,6 +21,8 @@ if ( $env:COMPILER -eq "MINGW" )
     $env:PATH=$env:PATH -replace "C:\\Program Files \(x86\)\\Git\\bin", ""
     $CMAKE_GENERATOR="MinGW Makefiles"
     $MAKE = "mingw32-make"
+    
+exit 0
 }
 elseif ( $env:COMPILER -eq "MSVC" )
 {
