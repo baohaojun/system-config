fc593756c0437df4dfcc85f892b6c633c04a6c50
Patrick von Reth <vonreth@kde.org>
2015-07-20 11:41:55 +0200

    fix powershell script

--- ci/msvc.ps1
+++ ci/msvc.ps1
@@ -5,21 +5,21 @@ batCall "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" a
 
 $CMAKE_INSTALL_ROOT=$env:APPVEYOR_BUILD_FOLDER -replace "\\", "/"
 
-$CMAKE_INSTALL_ROOT=$CMAKE_INSTALL_ROOT/work/install
+$CMAKE_INSTALL_ROOT="$CMAKE_INSTALL_ROOT/work/install"
 mkdir $env:APPVEYOR_BUILD_FOLDER\work\install
-mkdir $env:APPVEYOR_BUILD_FOLDER\work\git
 mkdir $env:APPVEYOR_BUILD_FOLDER\work\build
 mkdir $env:APPVEYOR_BUILD_FOLDER\work\build\extra-cmake-modules
 mkdir $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
-If ( -Not Test-Path $env:APPVEYOR_BUILD_FOLDER\work\git){
+If ( !(Test-Path "$env:APPVEYOR_BUILD_FOLDER\work\git")){
+    mkdir $env:APPVEYOR_BUILD_FOLDER\work\git
     cd $env:APPVEYOR_BUILD_FOLDER\work\git
     git clone git://anongit.kde.org/extra-cmake-modules.git
 }
 
 cd $env:APPVEYOR_BUILD_FOLDER\work\build\extra-cmake-modules
-cmake -G"NMake Makefiles" $env:APPVEYOR_BUILD_FOLDER\work\git\extra-cmake-modules -DCMAKE_INSTALL_PREFIX="$(CMAKE_INSTALL_ROOT)"
+cmake -G"NMake Makefiles" $env:APPVEYOR_BUILD_FOLDER\work\git\extra-cmake-modules -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
 nmake install
 
 cd $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
-cmake -G"NMake Makefiles" %APPVEYOR_BUILD_FOLDER% -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$(CMAKE_INSTALL_ROOT)"
+cmake -G"NMake Makefiles" $env:APPVEYOR_BUILD_FOLDER -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
 nmake
