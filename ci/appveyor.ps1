d3921715aab97f2f3aa2830f733b6b96df079b39
Patrick von Reth <vonreth@kde.org>
2015-07-20 15:07:09 +0200

    try to fix the script

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -2,11 +2,11 @@
 #http://stackoverflow.com/questions/2124753/how-i-can-use-powershell-with-the-visual-studio-command-prompt
 function batCall([string] $path, [string] $arg)
 {
-    Write-Host "Calling '$path' '$arg'"
-    cmd /c "'$path' '$arg' & set" |
+    Write-Host 'Calling "$path" "$arg"'
+    cmd /c '"$path" "$arg" & set' |
     foreach {
       if ($_ -match "=") {
-        Write-Host "ENV:\$($v[0])=$($v[1])"
+        #Write-Host "ENV:\$($v[0])=$($v[1])"
         $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
       }
     }
