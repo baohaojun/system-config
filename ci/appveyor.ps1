69dd2d2d97f8b49b6aef52064fb2547bc317f002
Patrick von Reth <vonreth@kde.org>
2015-07-21 14:14:21 +0200

    install ninja using Chocolatey

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -57,9 +57,7 @@ if ( !(Test-Path "$env:APPVEYOR_BUILD_FOLDER\work\install" ) )
     mkdir $env:APPVEYOR_BUILD_FOLDER\work\install
     mkdir $env:APPVEYOR_BUILD_FOLDER\work\git
     
-    Start-FileDownload https://github.com/martine/ninja/releases/download/v1.6.0/ninja-win.zip
-    7z e ninja-win.zip -o$INSTALL_DIR
-    rm  ninja-win.zip
+    cinst ninja
     
     cd $env:APPVEYOR_BUILD_FOLDER\work\git
     git clone -q git://anongit.kde.org/extra-cmake-modules.git
