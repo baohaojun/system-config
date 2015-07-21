d844fae51935e7a95d6f635ea00dcc72d27e5176
Patrick von Reth <vonreth@kde.org>
2015-07-21 16:00:53 +0200

    use full path

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -1,8 +1,8 @@
-Import-Module .\appveyorHelp.psm1 -Force
+Import-Module $env:APPVEYOR_BUILD_FOLDER\ci\appveyorHelp.psm1 -Force -Verbose
 
-INIT @("ninja", "extra-cmake-modules")
+Init @("ninja", "extra-cmake-modules")
 
 mkdir $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
 cd $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
 cmake -G"Ninja" $env:APPVEYOR_BUILD_FOLDER -DCMAKE_BUILD_TYPE=Release -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
-CMAKE-IMAGE-INSTALL "$env:APPVEYOR_BUILD_FOLDER\work\image"
+CmakeImageInstall "$env:APPVEYOR_BUILD_FOLDER\work\image"
