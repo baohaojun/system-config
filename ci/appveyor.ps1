bcc6e3319454b2dd79f10e8b7fa152c4845d2e4f
Patrick von Reth <vonreth@kde.org>
2015-07-22 18:55:40 +0200

    stop on error

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -1,8 +1,10 @@
+$ErrorActionPreference="Stop"
+
 Import-Module $env:APPVEYOR_BUILD_FOLDER\ci\appveyorHelp.psm1 -Force -Verbose
 
 Init @("ninja", "extra-cmake-modules")
 
-mkdir $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
+mkdir -Force $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
 cd $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
 cmake -G"Ninja" $env:APPVEYOR_BUILD_FOLDER -DCMAKE_BUILD_TYPE=Release -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
 CmakeImageInstall "$env:APPVEYOR_BUILD_FOLDER\work\image"
