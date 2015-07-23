51a9a8c5b0d917578bd660f1acfa7c15412f333f
Patrick von Reth <vonreth@kde.org>
2015-07-23 14:32:05 +0200

    log cmake command

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -6,12 +6,14 @@ Init @("ninja", "extra-cmake-modules", "png2ico")
 
 mkdir -Force $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
 cd $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
-cmake -G"Ninja" $env:APPVEYOR_BUILD_FOLDER -DCMAKE_BUILD_TYPE=Release -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
+LogExec cmake -G"Ninja" $env:APPVEYOR_BUILD_FOLDER -DCMAKE_BUILD_TYPE=Release -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
 CmakeImageInstall "$env:APPVEYOR_BUILD_FOLDER\work\image"
 
 SetupSnoreSend  "$env:APPVEYOR_BUILD_FOLDER\work\image\bin" @{
     "global" = @{ "Pushover-SECONDARY_BACKEND/UserKey.v1" = $env:SNORE_PUSHOVER_KEY;
-                  "Toasty-SECONDARY_BACKEND/DeviceID.v1" = $env:SNORE_TOASTY_ID }}
+                  "Toasty-SECONDARY_BACKEND/DeviceID.v1" = $env:SNORE_TOASTY_ID };
+    "snore-send" = @{"Toasty-SECONDARY_BACKEND/Enabled.v1" = "true";
+                     "Pushover-SECONDARY_BACKEND/Enabled.v1" = "true"}}
 
 
 SendSnoreNotification "Build complete!" "Build of SnoreNotify succeeded"
