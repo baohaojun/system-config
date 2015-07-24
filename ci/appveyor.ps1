3d2ed4d4ecfb4ba63eef60c3b76c1ae9247e8cde
Patrick von Reth <vonreth@kde.org>
2015-07-24 11:20:46 +0200

    fix translation generation

--- ci/appveyor.ps1
+++ ci/appveyor.ps1
@@ -6,7 +6,7 @@ Init @("ninja", "extra-cmake-modules", "png2ico")
 
 mkdir -Force $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
 cd $env:APPVEYOR_BUILD_FOLDER\work\build\snorenotify
-LogExec cmake -G"Ninja" $env:APPVEYOR_BUILD_FOLDER -DCMAKE_BUILD_TYPE=Release -DWITH_SNORE_DAEMON=ON -DWITH_FRONTENDS=ON -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
+LogExec cmake -G"Ninja" $env:APPVEYOR_BUILD_FOLDER -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT"
 CmakeImageInstall "$env:APPVEYOR_BUILD_FOLDER\work\image"
 
 SetupSnoreSend  "$env:APPVEYOR_BUILD_FOLDER\work\image\bin" @{
