72e87159b32f04bcc7399b3c5748a56fbf499f91
Bao Haojun <baohaojun@gmail.com>
2015-08-19 15:15:29 +0800

    bin/stcmd-subcase.sh: stcmd-subcase.sh got-ramdump
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/stcmd-subcase.sh
+++ bin/stcmd-subcase.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-#adb bash -x stcmd-subcase.sh disable-swap-dump
-my-adb forward tcp:54321 localreserved:scitd
-echo srem-helper "$@" | nc localhost 54321
+if test "$1" = srem-helper; then
+    shift
+fi
+
+if my-adb shell stcmd-subcase.sh srem-helper "$@" | tee /dev/stderr | grep -q "nc: can't connect to remote host (127.0.0.1): Connection refused"; then
+    my-adb forward tcp:54321 localreserved:scitd
+    echo -e srem-helper "$@\r" | nc localhost 54321
+fi
