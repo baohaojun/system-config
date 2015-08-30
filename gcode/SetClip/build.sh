7df4ec7a50a57c5803843aaef9ffa38037ec884d
Bao Haojun <baohaojun@gmail.com>
2015-08-30 22:23:56 +0800

    gcode/SetClip/build.sh: maybe try to use java 1.7 gcode/SetClip/src/Input.java: can do am startservice now
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- gcode/SetClip/build.sh
+++ gcode/SetClip/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 set -e
+
+# PATH=/usr/lib/jvm/java-7-openjdk-amd64/bin:$PATH
 mm-ant
 
 my-adb 'rm -f /sdcard/setclip-apk.txt; am startservice -n com.bhj.setclip/.PutClipService --ei getapk 1'
