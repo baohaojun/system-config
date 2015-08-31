5a7b60358a67fae62c24ee317e0cd19fd3cd5ec1
Bao Haojun <baohaojun@gmail.com>
2015-08-31 12:39:33 +0800

    gcode/SetClip/build.sh: do install automatically gcode/SetClip/src/Input.java: use thread for it
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ set -x
 set -e
 
 # PATH=/usr/lib/jvm/java-7-openjdk-amd64/bin:$PATH
+(
+    for x in $(seq 1 30); do
+        sleep 1;
+        if test "$(adb-top-activity)" = 警告; then
+            adb-tap 747 1186
+            break
+        fi
+    done
+) >/dev/null 2>&1 &
 mm-ant
 
 my-adb 'rm -f /sdcard/setclip-apk.txt; am startservice -n com.bhj.setclip/.PutClipService --ei getapk 1'
