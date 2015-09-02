b0d5ca2e4c3ea259ec7f9d5bb82b53954008e845
Bao Haojun <baohaojun@gmail.com>
2015-09-02 21:26:11 +0800

    bin/org-export.help: use random css bin/stcmd-subcase.sh: fix usage
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/stcmd-subcase.sh
+++ bin/stcmd-subcase.sh
@@ -3,7 +3,7 @@ if test "$1" = srem-helper; then
     shift
 fi
 
-if my-adb shell stcmd-subcase.sh srem-helper "$@" | tee /dev/stderr | grep -q "nc: can't connect to remote host (127.0.0.1): Connection refused"; then
+if my-adb shell stcmd-subcase.sh "$@" | tee /dev/stderr | grep -q "nc: can't connect to remote host (127.0.0.1): Connection refused"; then
     my-adb forward tcp:54321 localreserved:scitd
     echo -e srem-helper "$@\r" | nc localhost 54321
 fi
