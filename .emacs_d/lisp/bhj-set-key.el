c8d98d243aef2edd4417b1790400be8eb104fe1a
Bao Haojun <baohaojun@gmail.com>
2015-09-15 15:13:04 +0800

    .emacs_d/lisp/bhj-set-key.el: should be easier to re fontlock lua bin/call-home: fix call-home bin/do-natural-scrolling: fix it for touchpad
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- .emacs_d/lisp/bhj-set-key.el
+++ .emacs_d/lisp/bhj-set-key.el
@@ -106,5 +106,6 @@
 (define-key global-map [(super f1)] 'x-maximize-frame)
 (define-key global-map [(control x) (b)] 'ido-switch-buffer)
 (define-key global-map [(control c) (W)] 'bhj-set-working-buffer)
+(define-key global-map [(meta shift o)] '(lambda () (interactive) (font-lock-fontify-block (line-number-at-pos))))
 (define-key global-map [(control z)] 'undo-tree-undo)
 (define-key global-map [(super ?*)] 'enter-emoji)
