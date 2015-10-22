b065aed32f56a01298ad8f1ef3027077daab7ca0
Bao Haojun <baohaojun@gmail.com>
2015-10-22 14:04:08 +0800

    there are 4 files.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- upload-all.sh
+++ upload-all.sh
@@ -7,7 +7,7 @@ if test "$1" = -n; then
     true
 else
     my-adb 'mkdir -p /sdcard/0/; rm /sdcard/0/*'
-    for x in T1Wrench-linux.tgz T1Wrench-macos.zip T1Wrench-windows.zip; do
+    for x in T1Wrench-debian.tgz T1Wrench-ubuntu-14.04.tgz T1Wrench-macos.zip T1Wrench-windows.zip; do
         adb push $x /sdcard/0/${x%.*}-$shortVersion.${x#*.}
     done
 fi
@@ -17,4 +17,5 @@ adb-tap 236 613
 adb-tap 985 387
 adb-tap 951 635
 adb-tap 1001 805
+adb-tap 1001 975
 adb-tap 755 1841
