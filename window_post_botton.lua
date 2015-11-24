f7ea81b56f37f50a949740e96ce9c9e197216dd1
Bao Haojun <baohaojun@gmail.com>
2015-11-24 20:27:09 +0800

    window_post_botton.lua: add some more windows
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- window_post_botton.lua
+++ window_post_botton.lua
@@ -1,7 +1,9 @@
 local map = {}
-map['com.tencent.mm/com.tencent.mm.ui.chatting.ChattingUI'] = 'weixin-chat'
+map['com.tencent.mobileqq/com.tencent.mobileqq.activity.aio.photo.AIOGalleryActivity'] = 'manual-post'
+map['com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity'] = 'weixin-chat'
 map['com.qzone/com.qzonex.module.operation.ui.QZonePublishMoodActivity'] = 'weibo-share'
 map['com.tencent.mm/com.tencent.mm.ui.LauncherUI'] = 'weixin-chat'
+map['com.tencent.mobileqq/com.tencent.mobileqq.activity.TroopMemberListActivity'] = 'manual-post'
 map['com.tencent.mobileqq/com.tencent.mobileqq.activity.ChatActivity'] = 'weixin-chat'
-map['com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity'] = 'weixin-chat'
+map['com.tencent.mm/com.tencent.mm.ui.chatting.ChattingUI'] = 'weixin-chat'
 return map
