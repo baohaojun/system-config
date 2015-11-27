9a8752ad094735d95d5ae33f071c93f5c56018a0
Hannah von Reth <vonreth@kde.org>
2015-11-27 14:50:59 +0100

    Fix msvc build

--- autotest/snorebenchmark.cpp
+++ autotest/snorebenchmark.cpp
@@ -20,13 +20,13 @@ public:
         instance.setSettingsValue(QStringLiteral("Timeout"), 1, LOCAL_SETTING);
     }
 
-    QString htmlTestString = QStringLiteral("<i>Italic A</i><br>"
-                                            "<i>Italic B</i><br>"
-                                            "<b>Bold</b><br>"
-                                            "<u>Underline</u><br>"
-                                            "<font color=\"blue\">Font</font><br>"
-                                            "&lt;&amp;&gt;<br>"
-                                            "<a href=\"https://github.com/Snorenotify/Snorenotify\">Website</a><br>");
+    QString htmlTestString = QLatin1String("<i>Italic A</i><br>"
+                                           "<i>Italic B</i><br>"
+                                           "<b>Bold</b><br>"
+                                           "<u>Underline</u><br>"
+                                           "<font color=\"blue\">Font</font><br>"
+                                           "&lt;&amp;&gt;<br>"
+                                           "<a href=\"https://github.com/Snorenotify/Snorenotify\">Website</a><br>");
 
 private Q_SLOTS:
     void benchmarkUtilsToHtml();
@@ -85,7 +85,7 @@ void SnoreBenchmark::benchmarkNotifications()
     });
 
     Application app = SnoreCorePrivate::instance()->defaultApplication();
-//   QBENCHMARK_ONCE{
+    //   QBENCHMARK_ONCE{
     for (int i = 0; i < 100; ++i) {
         QString number = QString::number(i);
         Notification n(app, app.defaultAlert(), QLatin1String("Test ") + number, QLatin1String("Message ") + number, app.icon());
