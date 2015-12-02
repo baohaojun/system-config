d6a2ff16c3ad2f7a95362949fd9fd5e965187556
Bao Haojun <baohaojun@gmail.com>
2015-12-02 10:50:20 +0800

    auto code
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- window_post_botton.lua
+++ window_post_botton.lua
@@ -12,6 +12,7 @@ map['com.tencent.mm/com.tencent.mm.ui.chatting.ChattingUI'] = 'weixin-chat'
 map['com.tencent.mm/com.tencent.mm.ui.LauncherUI'] = 'weixin-chat'
 map['com.tencent.mobileqq/com.tencent.mobileqq.activity.aio.photo.AIOGalleryActivity'] = 'manual-post'
 map['com.tencent.mobileqq/com.tencent.mobileqq.activity.ChatActivity'] = 'weixin-chat'
+map['com.tencent.mobileqq/com.tencent.mobileqq.activity.EditActivity'] = 'manual-post'
 map['com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity'] = 'weixin-chat'
 map['com.tencent.mobileqq/com.tencent.mobileqq.activity.TroopMemberListActivity'] = 'manual-post'
 return map
