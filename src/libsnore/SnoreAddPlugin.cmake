e7dec5023010fd37bad16f5fc9c15ba67fa717ef
Hannah von Reth <vonreth@kde.org>
2015-12-02 17:15:31 +0100

    Make dependencie to QWidget optional

--- src/libsnore/SnoreAddPlugin.cmake
+++ src/libsnore/SnoreAddPlugin.cmake
@@ -11,7 +11,7 @@ function(add_snore_plugin name)
     target_link_libraries(libsnore_${ARG_TYPE}_${name} Snore::Libsnore ${ARG_LIBS})
     install(TARGETS libsnore_${ARG_TYPE}_${name} ${SNORE_PLUGIN_INSTALL_PATH})
     
-    if(ARG_SETTINGS_SOURCES)
+    if(ARG_SETTINGS_SOURCES AND Qt5Widgets_FOUND)
       add_library(libsnore_settings_${ARG_TYPE}_${name} MODULE ${ARG_SETTINGS_SOURCES} )
       target_link_libraries(libsnore_settings_${ARG_TYPE}_${name} Snore::Libsnore Snore::LibsnoreSettings ${ARG_SETTINGS_LIBS})
       install(TARGETS libsnore_settings_${ARG_TYPE}_${name} ${SNORE_PLUGIN_INSTALL_PATH})
