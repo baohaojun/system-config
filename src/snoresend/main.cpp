3aedea7933984ac0c7dca0442b23e41bec0ba4c4
Hannah von Reth <vonreth@kde.org>
2015-12-05 20:20:39 +0100

    Remove broken option from snoresend

--- src/snoresend/main.cpp
+++ src/snoresend/main.cpp
@@ -107,9 +107,6 @@ int main(int argc, char *argv[])
     QCommandLineOption _bringProcessToFront(QStringList() << QStringLiteral("bring-process-to-front"), QStringLiteral("Bring process with pid to front if notification is clicked."), QStringLiteral("pid"));
     parser.addOption(_bringProcessToFront);
 
-    QCommandLineOption _bringWindowToFront(QStringList() << QStringLiteral("bring-window-to-front"), QStringLiteral("Bring window with wid to front if notification is clicked."), QStringLiteral("wid"));
-    parser.addOption(_bringWindowToFront);
-
     parser.process(app);
     qCDebug(SNORE) << app.arguments();
     if (parser.isSet(title) && parser.isSet(message)) {
@@ -136,7 +133,7 @@ int main(int argc, char *argv[])
             parser.showHelp(-1);
         }
         Notification n(application, alert, parser.value(title), parser.value(message), icon, Notification::defaultTimeout(), static_cast<Notification::Prioritys>(prio));
-        if (parser.isSet(_bringProcessToFront) || parser.isSet(_bringWindowToFront)) {
+        if (parser.isSet(_bringProcessToFront)) {
             n.addAction(Action(1, qApp->translate("SnoreSend", "Bring to Front")));
         }
         int returnCode = -1;
@@ -150,8 +147,6 @@ int main(int argc, char *argv[])
             if (noti.closeReason() == Notification::Closed) {
                 if (parser.isSet(_bringProcessToFront)) {
                     bringToFront(parser.value(_bringProcessToFront));
-                } else if (parser.isSet(_bringWindowToFront)) {
-                    Utils::bringWindowToFront((HWND)parser.value(_bringWindowToFront).toULongLong(), true);
                 }
             }
             returnCode = noti.closeReason();
