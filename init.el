e5870030b8d5e39bc061604312ea237350e366ca
Bao Haojun <baohaojun@gmail.com>
2016-02-11 06:40:10 +0800

    init.el: fix it for M-x
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- init.el
+++ init.el
@@ -52,6 +52,7 @@
 (require 'helm-config)
 (helm-mode 1)
 
+(define-key global-map [remap execute-extended-command] 'helm-M-x)
 (define-key global-map [remap find-file] 'helm-find-files)
 (define-key global-map [remap occur] 'helm-occur)
 (define-key global-map [remap list-buffers] 'helm-buffers-list)
