7a99b5c586f35b1dd08d4fa496321f2c0713310b
Bao Haojun <baohaojun@gmail.com>
2016-02-22 10:53:45 +0800

    Add rails

---
+++ gcode/playground/rails/sample_app/test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+end
