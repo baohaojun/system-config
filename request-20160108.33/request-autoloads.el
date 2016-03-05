a6968e8fae2259b2f5dc544a8892fb1cd54b2636
Bao Haojun <baohaojun@smartisan.cn>
2016-03-05 15:44:38 +0800

    Add request

---
+++ request-20160108.33/request-autoloads.el
@@ -0,0 +1,15 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("request.el") (22234 36371 907262 25000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; request-autoloads.el ends here
---
+++ request-20160108.33/request-pkg.el
@@ -0,0 +1,1 @@
+(define-package "request" "20160108.33" "Compatible layer for URL request in Emacs" '((cl-lib "0.5")))
