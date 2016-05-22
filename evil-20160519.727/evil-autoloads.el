c46501ee9b4d7e08f39542a428b198c8db328527
Bao Haojun <baohaojun@gmail.com>
2016-05-22 16:38:05 +0800

    Add evil mode

---
+++ evil-20160519.727/evil-autoloads.el
@@ -0,0 +1,26 @@
+;;; evil-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "evil-core" "evil-core.el" (22337 28629 685653
+;;;;;;  319000))
+;;; Generated autoloads from evil-core.el
+ (autoload 'evil-mode "evil")
+
+;;;***
+
+;;;### (autoloads nil nil ("evil-command-window.el" "evil-commands.el"
+;;;;;;  "evil-common.el" "evil-digraphs.el" "evil-ex.el" "evil-integration.el"
+;;;;;;  "evil-jumps.el" "evil-macros.el" "evil-maps.el" "evil-pkg.el"
+;;;;;;  "evil-repeat.el" "evil-search.el" "evil-states.el" "evil-types.el"
+;;;;;;  "evil-vars.el" "evil.el") (22337 28629 829656 78000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-autoloads.el ends here
---
+++ goto-chg-20131228.659/goto-chg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "goto-chg" "20131228.659" "goto last change" 'nil :keywords '("convenience" "matching"))
