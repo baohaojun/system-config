3ac4aac9a25aa8eab2eb343f0070b54369e50764
Bao Haojun <baohaojun@gmail.com>
2016-05-22 19:06:29 +0800

    fix lua
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ lua-mode-20160502.1141/lua-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "lua-mode" "20160502.1141" "a major-mode for editing Lua scripts" 'nil :url "http://immerrr.github.com/lua-mode" :keywords
+  '("languages" "processes" "tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
