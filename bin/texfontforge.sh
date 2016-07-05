724fcd52cf9d68dbd3d5871bbe52f98ecaa9850a
Bao Haojun <baohaojun@gmail.com>
2016-07-05 22:09:44 +0800

    bin/org2pdf: add style choose bin/texfontforge.sh: add support for texlive 2016 (change config location) bin/wget-all: fix getopt
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/texfontforge.sh
+++ bin/texfontforge.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -e 
+set -e
+if test "$(readlink -f ~/.texlive2016/texmf-var)" != "$(readlink -f ~/.texmf-var)"; then
+    rm -rf ~/.texlive2016
+    mkdir ~/.texlive2016
+    ln -s ~/.texmf-var ~/.texlive2016/texmf-var
+    ln -s ~/.texmf-config ~/.texlive2016/texmf-config
+fi
 this_script=$(readlink -f $0)
 TMPD=tmp.$$
 #this script will generate unicode font for pdflatex usage
@@ -9,7 +15,7 @@ TMPD=tmp.$$
 
     file=$1.ttf
     if test -e $1.ttc -a ! -e $1.ttf; then
-	file=$1.ttc
+        file=$1.ttc
     fi
     cp $file ~/.texmf-var/fonts/truetype/$1.ttf;
     cd ~/.texmf-var/fonts/truetype/;
@@ -17,34 +23,34 @@ TMPD=tmp.$$
     mkdir $TMPD
     cd $TMPD
     cp /usr/share/latex-cjk-common/utils/subfonts/subfonts.pe .
-    ln -s ../$1.ttf . 
+    ln -s ../$1.ttf .
 
     kpsewhich Unicode.sfd
     fontforge -script subfonts.pe $1.ttf $1 `kpsewhich Unicode.sfd`
 
     for i in *.pfb
     do
-	echo "$(basename $i .pfb) $(basename $i .pfb) <$i" >> $1.map
+        echo "$(basename $i .pfb) $(basename $i .pfb) <$i" >> $1.map
     done
 
     mkdir -p ~/.texmf-var/fonts/map/dvips/$1/ ~/.texmf-var/fonts/{afm,type1,tfm}/$1
 
     mv $1.map ~/.texmf-var/fonts/map/dvips/$1/
     (
-	flock 9
-	cat ~/.texmf-var/fonts/map/dvips/$1/$1.map >> ~/.texmf-var/fonts/map/dvips/psfonts_t1.map
+        flock 9
+        cat ~/.texmf-var/fonts/map/dvips/$1/$1.map >> ~/.texmf-var/fonts/map/dvips/psfonts_t1.map
     ) 9< $this_script
     mv *.afm ~/.texmf-var/fonts/afm/$1
     mv *.tfm ~/.texmf-var/fonts/tfm/$1
     mv *.pfb ~/.texmf-var/fonts/type1/$1
 
     (mkdir -p ~/.texmf-var/tex/latex/cjk/utf-8;
-	cd ~/.texmf-var/tex/latex/cjk/utf-8;
-	bold="{<-> CJKb * ${1}}{\CJKbold}"
-	if test $1 = simsun; then
-	    bold="{<-> CJK * simhei}{}"
-	fi
-	cat <<End> c70$1.fd
+        cd ~/.texmf-var/tex/latex/cjk/utf-8;
+        bold="{<-> CJKb * ${1}}{\CJKbold}"
+        if test $1 = simsun; then
+            bold="{<-> CJK * simhei}{}"
+        fi
+        cat <<End> c70$1.fd
 \ProvidesFile{c70${1}.fd}
 % character set: Unicode U+0080 - U+FFFD
 % font encoding: Unicode
@@ -61,6 +67,7 @@ End
     rm $TMPD -rf
 
     texhash
+    mktexlsr
     updmap --enable Map $1.map
 )
 
