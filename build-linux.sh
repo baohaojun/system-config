bc0381316d5007d83a780b860cad73f65e13e6b1
Bao Haojun <baohaojun@gmail.com>
2016-07-11 14:19:36 +0800

    Fix build-linux on non-system-config env
    Fix #1.

--- build-linux.sh
+++ build-linux.sh
@@ -2,36 +2,73 @@
 set -e
 
 ## start code-generator "^\\s *#\\s *"
-# generate-getopts  r:release_dir
+# generate-getopts  r:release_dir=Wrench-debian b:build-dir=~/tmp/build-wrench
 ## end code-generator
 ## start generated code
-release_dir=
+
+release_dir=Wrench-debian
+build_dir=~/tmp/build-wrench
 OPTIND=1
-while getopts 'r:' opt; do
+while getopts "r:b:h" opt; do
     case "$opt" in
-        r)    release_dir=$OPTARG;;
-        *)    echo Usage Error; exit 2;;
+
+        r) release_dir=$OPTARG ;;
+        b) build_dir=$OPTARG ;;
+        h)
+            echo
+            echo
+            printf %06s '-b '
+            printf %-24s 'BUILD_DIR'
+            echo ''
+            printf %06s '-r '
+            printf %-24s 'RELEASE_DIR'
+            echo ''
+            shift
+            exit 0
+            ;;
+        *)
+            echo
+            echo
+            printf %06s '-b '
+            printf %-24s 'BUILD_DIR'
+            echo ''
+            printf %06s '-r '
+            printf %-24s 'RELEASE_DIR'
+            echo ''
+            exit 2
+            ;;
     esac
 done
-shift $(($OPTIND - 1))
+
 
 ## end generated code
 
-if test -z "$release_dir"; then
-    release_dir=Wrench-debian
-fi
-
 cd $(dirname $(readlink -f $0))
-build_dir=~/tmp/build-t1
+
 if test $# = 1 && [[ "$1" =~ debug ]]; then
     build_dir=~/tmp/build-t1-debug
+fi
+
+if type relative-link >/dev/null 2>&1; then
+    system_config=true
+else
+    system_config=false
+
+    function relative-link() {
+        true
+    }
 fi
 
 relative-link -f $build_dir/Wrench ~/system-config/bin/overide
 
 mkdir -p $build_dir
-rsync -L * $build_dir -av --exclude=release --exclude=windows --exclude=macx --exclude=emojis
-rsync release/ $build_dir -av -L --exclude=adb_usb_driver_smartisan --exclude=emojis
+if test "$system_config" = true; then
+    rsync -L * $build_dir -av --exclude=release --exclude=windows --exclude=macx --exclude=emojis
+    rsync release/ $build_dir -av -L --exclude=adb_usb_driver_smartisan --exclude=emojis
+else
+    rsync * $build_dir -a -L --exclude=windows --exclude=macx
+    rsync release/ $build_dir -a -L
+fi
 
 oldpwd=$PWD
 cd $build_dir
@@ -53,6 +90,12 @@ ln -s $oldpwd/linux/binaries/the-true-adb . -f
     if test "$DOING_WRENCH_RELEASE"; then
         mkdir -p ~/src/github/$release_dir
         command rsync -L $oldpwd/linux/binaries/* Wrench download/download $oldpwd/release/ $oldpwd/*.lua ~/src/github/$release_dir -av --delete --exclude-from=$HOME/src/github/Wrench/release-exclude.txt
+        exit
+    fi
+
+    if test "$system_config" = false; then
+        killall Wrench || true
+        ./Wrench&
         exit
     fi
 
