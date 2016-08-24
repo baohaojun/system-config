d8861d6eb5c1884b8cdaf996be6f7552f0b10b11
Bao Haojun <baohaojun@gmail.com>
2016-08-24 12:17:34 +0800

    tools/beagrep-files: export the directories tools/beagrep-files: make grep chinese work tools/beagrep-unurl-and-filter.pl: allow to use db_copy tools/beagrep: allow to debug from start-debug tools/mkbeagrepidx: record where is my top source dir
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- tools/beagrep-unurl-and-filter.pl
+++ tools/beagrep-unurl-and-filter.pl
@@ -1,8 +1,25 @@
 #!/usr/bin/env perl
 
+$db_source_dir = $ENV{source_dir};
+if (-e "$ENV{beagrep_dir}/top-source-dir") {
+    chomp($db_source_dir = qx(cat $ENV{beagrep_dir}/top-source-dir));
+}
+
+$db_copy = 0;
+
+if ($db_source_dir ne $ENV{source_dir}) {
+    $db_copy = 1;
+    $n_db_source_dir = length $db_source_dir;
+    $source_dir = $ENV{source_dir};
+}
+
 while (<>) {
     s/%([0-9a-fA-F]{2})/chr(eval("0x$1"))/eg;
     s!^file://!!;
+    if ($db_copy) {
+        $substr = substr($_, $n_db_source_dir);
+        $_ = $source_dir . $substr;
+    }
     if (($^O eq "MSWin32" or $^O eq "cygwin") && $_ =~ m,^/.:,) {
         if (m!^/.:/cygwin!i) {
             our $root;
