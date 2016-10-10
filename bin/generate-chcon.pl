98917ad431911f3aba9dd32602083c84bcab71db
Bao Haojun <baohaojun@gmail.com>
2016-10-10 14:26:38 +0800

    .after-push-hook: fix after-push-hook bin/adb-backup-system: make chcon simpler bin/adb-install-debian: fix apt-get on phone bin/firefox-open-url-pattern: should make sure browser can goto next page with firemacs (get out of the adress bar) bin/generate-chcon.pl: make the script a lot simpler bin/mm: allow to do a quick mm bin/my-adb: adb logcat :Vold,vold,VoldCmdListener '*:E' -- -b system AudioParamParser:S PhoneWindow:S
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/generate-chcon.pl
+++ bin/generate-chcon.pl
@@ -3,25 +3,11 @@
 my $paren_open = 0;
 my $last_chcon;
 while (<>) {
-    if (m,/data/debian/android/data/+(debian/|media/0),) {
-        while (<>) {
-            if (m/^$/) {
-                last;
+    if (m,^/data/debian/android/data/:, .. m,^/data/debian/android/data//,) {
+        if (m,^d,) {
+            if (m,(u:object_r:\S*)\s+(.+),) {
+                print "\nmkdir -p /data/$2; chcon $1 /data/'$2'";
             }
         }
-    } elsif (m,^(/data/debian/android/data/.*):$,) {
-        if ($paren_open) {
-            print ")\n";
-            $paren_open = 0;
-        }
-        print "(   cd $1 && pwd || exit;";
-        $paren_open = 1;
-        $last_chcon = "";
-
-    } elsif (m,(u:object_r:\S*)\s+(.+),) {
-        print "\n    chcon $1 '$2';" unless $1 eq 'u:object_r:system_data_file:s0';
     }
-}
-if ($paren_open) {
-    print ")\n";
 }
