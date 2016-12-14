d2281f8f121bc921bc94261744f0dca9f32d7ac3
Bao Haojun <baohaojun@gmail.com>
2016-12-14 22:30:02 +0800

    wrench.lua: Fix wx login mp
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- wrench.lua
+++ wrench.lua
@@ -839,7 +839,19 @@ end
 
 local open_weixin_scan = function()
    weixin_open_homepage()
-   adb_event"adb-tap 986 117 sleep .5 adb-tap 717 477"
+   adb_event"adb-tap 56 154 sleep .1 adb-tap 1002 115 sleep .5 adb-tap 717 477"
+   wx_login_page = "com.tencent.mm/com.tencent.mm.plugin.webview.ui.tools.WebViewUI"
+   for i = 1, 60 do
+      w = adb_top_window()
+      if w == "com.tencent.mm/com.tencent.mm.plugin.scanner.ui.BaseScanUI" then
+         sleep(1)
+      elseif w == wx_login_page then
+         for y = 1, 10 do
+            adb_event"sleep .1 adb-tap 516 1020"
+         end
+         break
+      end
+   end
 end
 
 local function dingding_open_homepage()
