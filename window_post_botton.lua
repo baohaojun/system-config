1f92409cc7f635e14ab700b2ebb10c99131b3176
Bao Haojun <baohaojun@gmail.com>
2016-12-30 21:53:20 +0800

    window_post_botton.lua: auto
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- window_post_botton.lua
+++ window_post_botton.lua
@@ -9,6 +9,7 @@ map['com.alibaba.android.rimet/com.alibaba.android.user.profile.activities.EditP
 map['com.alibaba.android.rimet/com.alibaba.android.user.search.activity.GlobalSearchInputActivity'] = 'manual-post'
 map['com.alibaba.android.rimet/com.alibaba.android.user.settings.activity.ChangePwdActivity'] = 'manual-post'
 map['com.alibaba.android.rimet/com.alibaba.lightapp.runtime.activity.CommonWebViewActivity'] = 'manual-post'
+map['com.amazon.kindle/com.amazon.kcp.store.LegacyStoreActivity'] = 'manual-post'
 map['com.amazon.mShop.android.shopping/com.amazon.identity.auth.device.AuthPortalUIActivity'] = 'manual-post'
 map['com.android.browser/com.android.browser.BrowserActivity'] = 'manual-post'
 map['com.android.browser/org.chromium.chrome.browser.ChromeTabbedActivity'] = 'manual-post'
