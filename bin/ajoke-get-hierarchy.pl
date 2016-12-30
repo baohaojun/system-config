484435b17b74f7eeda6b6fee072e32b4f56826a4
Bao Haojun <baohaojun@gmail.com>
2016-12-30 22:00:06 +0800

    bin/adb-stop-all-services: should stop the android first bin/ajoke-get-hierarchy.pl: add cache
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/ajoke-get-hierarchy.pl
+++ bin/ajoke-get-hierarchy.pl
@@ -1,13 +1,32 @@
 #!/usr/bin/perl
 use strict;
 use Getopt::Long;
+use Digest::MD5 qw(md5_hex);
+use File::Path qw(make_path);
+use Cwd;
 
-use BhjJava;
+use BhjJava; # will chdir to beagrep src
 sub debug(@) {
     print $debug "@_\n";
 }
 
 $ENV{GTAGS_START_FILE} = "";
+
+if ($ENV{AJOKE_NO_CACHE} ne 'true') {
+    my $md5sum = md5_hex(join("\n", @ARGV, cwd()));
+    $md5sum =~ m/^(..)(.*)/;
+    my ($md_dir, $md_file) = ($1, $2);
+    my $cache_dir = "$ENV{HOME}/.cache/system-config/ajoke-get-hierarchy.pl.cache/$md_dir";
+    make_path $cache_dir;
+    $ENV{AJOKE_NO_CACHE} = 'true';
+    if (-e "$cache_dir/$md_file") {
+        system("cat", "$cache_dir/$md_file");
+    } else {
+        system($0, @ARGV);
+    }
+    system("daemon", "-i", "-D", cwd(), "--", "update-output-cache", "$cache_dir/$md_file", "$0", @ARGV);
+    exit;
+}
 
 my $should_recurse = 1;
 if ($ENV{DO_RECURSIVE_JAVA_HIERARCHY}) {
