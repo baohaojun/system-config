e2e3b72723d5cc15cd58fcc5f4288d8cf38afc3b
Hannah von Reth <vonreth@kde.org>
2017-01-20 10:16:47 +0100

    Since Qt 5.7 Qt is no longer able to display "Segoe UI Emoji" in color
    So we always use "Segoe UI Symbol"

--- src/plugins/backends/snore/notifywidget.cpp
+++ src/plugins/backends/snore/notifywidget.cpp
@@ -35,18 +35,18 @@ NotifyWidget::NotifyWidget(int id, const ::SnorePlugin::Snore *parent) :
 {
 
 #ifdef Q_OS_WIN
-#if QT_VERSION < QT_VERSION_CHECK(5,7,0)// Qt 5.7+ is broken and can only display it in black and white.
+//#if QT_VERSION < QT_VERSION_CHECK(5,7,0)// Qt 5.7+ is broken and can only display it in black and white.
     if (QSysInfo::windowsVersion() >= QSysInfo::WV_WINDOWS8) {
         m_fontFamily = QStringLiteral("Segoe UI Symbol");
         emit fontFamilyChanged();
     }
-#if QT_VERSION >= QT_VERSION_CHECK(5,5,0)
-    if (QSysInfo::windowsVersion() >= QSysInfo::WV_WINDOWS10) {
-        m_fontFamily = QStringLiteral("Segoe UI Emoji");
-        emit fontFamilyChanged();
-    }
-#endif
-#endif
+//#if QT_VERSION >= QT_VERSION_CHECK(5,5,0)
+//    if (QSysInfo::windowsVersion() >= QSysInfo::WV_WINDOWS10) {
+//        m_fontFamily = QStringLiteral("Segoe UI Emoji");
+//        emit fontFamilyChanged();
+//    }
+//#endif
+//#endif
 #endif
     QQmlApplicationEngine *engine = new QQmlApplicationEngine(this);
     engine->rootContext()->setContextProperty(QStringLiteral("notifyWidget"), this);
