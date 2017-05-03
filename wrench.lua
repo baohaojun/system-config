2cd915cc3188836f09d0d463a76e5b38153efbef
Bao Haojun <baohaojun@gmail.com>
2017-05-03 18:49:18 +0800

    wrench.lua: wait longer
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- wrench.lua
+++ wrench.lua
@@ -1307,7 +1307,7 @@ adb_wait_file_gone = function(file)
    adb_shell(
       (
       [[
-            for x in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+            for x in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 $(seq 1 80); do
                if test -e %s; then
                   sleep .1 || busybox sleep .1;
                else
