a58ece5b2d97ab3e4b909fc1e3135f23de046a36
Bao Haojun <baohaojun@gmail.com>
2017-05-27 16:47:09 +0800

    bin/bbs-robot: do bbs every hour bin/gerrit2org-done: allow to get/set done bin/gerrit2org: Don't do gerrit2org again once captured bin/get-distributed-file: split-dir-cache? bin/run-every-N-minutes: run every so many minutes etc/gerrit-rules.pl: fix the all reviewer rule.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- etc/gerrit-rules.pl
+++ etc/gerrit-rules.pl
@@ -1,10 +1,25 @@
+% -*- Mode: Prolog -*-
+
+submit_rule(submit(ARR, CR, V)) :-
+    all_done_and_go('All-Reviewer-Required', ARR),
+    gerrit:max_with_block(-2, 2, 'Code-Review', CR),
+    gerrit:max_with_block(-1, 1, 'Verified', V).
+
+% 如果是mol线的话，就使用原来的逻辑
 submit_rule(submit(CR, V)) :-
-    all_done_and_go('Code-Review', CR),
+    gerrit:change_branch(B), regex_matches('refs/heads/mol/.*', B),
+    gerrit:max_with_block(-2, 2, 'Code-Review', CR),
+    gerrit:max_with_block(-1, 1, 'Verified', V).
+
+% 如果被 153 （cmbuild） Review +2 的话，也使用原来的逻辑（直接可以进了）
+submit_rule(submit(CR, V)) :-
+    CR = label('Code-Review', ok(user(153))),
+    gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-1, 1, 'Verified', V).
 
 all_done_and_go(Category, label(Category, ok(U))) :-
     gerrit:current_user(U),
-    findall(Score, score(Category, Score), All),
+    findall(Score, score('Code-Review', Score), All),
     all_done_list(All),
     one_plus2_list(All),
     !.
@@ -14,7 +29,6 @@ all_done_and_go(Category, label(Category, need(_))).
 score(Category, Score) :-
     gerrit:commit_label(label(Category, Score), User).
 
-% Simple Prolog routine to sum a list of integers.
 all_done_list([]).
 all_done_list([X|T]) :- X \= 0, all_done_list(T).
 
