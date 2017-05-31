dcffc51b2e6a45d7bc2a2f00ade211d9b1326970
Bao Haojun <baohaojun@gmail.com>
2017-05-31 13:34:50 +0800

    etc/gerrit-rules.pl: try to make sure no-res change works
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- etc/gerrit-rules.pl
+++ etc/gerrit-rules.pl
@@ -1,37 +1,27 @@
 % -*- Mode: Prolog -*-
 
-submit_rule(submit(ARR, CR, V)) :-
-    all_done_and_go('All-Reviewer-Required', ARR),
+submit_rule(R) :-
+    res_rule(R).
+
+submit_rule(R) :-
+    non_res_rule(R).
+
+non_res_rule(submit(R, CR, V)) :-
+    \+ contains_res_change(),
+    gerrit:current_user(U),
+    R = label('Shit', reject(U)),
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-1, 1, 'Verified', V).
 
-% 如果是mol线的话，就使用原来的逻辑
-submit_rule(submit(CR, V)) :-
-    gerrit:change_branch(B), regex_matches('refs/heads/mol/.*', B),
-    gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-    gerrit:max_with_block(-1, 1, 'Verified', V).
-
-% 如果被 153 （cmbuild） Review +2 的话，也使用原来的逻辑（直接可以进了）
-submit_rule(submit(CR, V)) :-
+res_rule(submit(CR, V)) :-
+    contains_res_change(),
     CR = label('Code-Review', ok(user(153))),
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-1, 1, 'Verified', V).
 
-all_done_and_go(Category, label(Category, ok(U))) :-
-    gerrit:current_user(U),
-    findall(Score, score('Code-Review', Score), All),
-    all_done_list(All),
-    one_plus2_list(All),
-    !.
+contains_res_change() :-
+    gerrit:commit_delta('core/res/res/.*');
+    gerrit:commit_delta('core/java/android/provider/Settings.java$').
 
-all_done_and_go(Category, label(Category, need(_))).
 
-score(Category, Score) :-
-    gerrit:commit_label(label(Category, Score), User).
-
-all_done_list([]).
-all_done_list([X|T]) :- X \= 0, all_done_list(T).
-
-one_plus2_list([]) :- false.
-one_plus2_list([X|T]) :- X = 2; one_plus2_list(T).
 
