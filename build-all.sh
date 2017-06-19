795d907a1a89b8dddfd1f89b7db2fb54c29bdacd
Bao Haojun <baohaojun@gmail.com>
2017-06-19 18:38:00 +0800

    fix ubuntu build

--- build-all.sh
+++ build-all.sh
@@ -137,10 +137,16 @@ fi
 
 if is-platform-needed ubuntu; then
     (
+        if echo $SHELLOPTS | grep -q xtrace; then
+            bashx=-x
+        else
+            bashx=
+        fi
+
         mkdir -p ~/external/cowbuilder/ubuntu-trusty-amd64/chroot/home/bhj/tmp/build-wrench
         chmod u+w ~/external/cowbuilder/ubuntu-trusty-amd64/chroot/home/bhj/tmp/build-wrench/ -R
         rm ~/external/cowbuilder/ubuntu-trusty-amd64/chroot/home/bhj/tmp/build-wrench/ -rf
-        ssh trusty "export DOING_WRENCH_RELEASE=true; PATH=~/external/local/qt5-ubuntu-1404/bin/:\$PATH; cd $PWD; ./build-linux.sh -r Wrench-ubuntu-14.04"
+        ssh trusty "export DOING_WRENCH_RELEASE=true; PATH=~/external/local/qt5-ubuntu-1404/bin/:\$PATH; cd $PWD; bash $bashx ./build-linux.sh -r Wrench-ubuntu-14.04"
         touch ~/external/cowbuilder/ubuntu-trusty-amd64/chroot/home/bhj/tmp/build-wrench/build-ok
     )&
 fi
