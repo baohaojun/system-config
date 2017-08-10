b8b958f1812e96623b4d8f8ee864dded4986d703
Bao Haojun <baohaojun@gmail.com>
2017-08-10 11:08:08 +0800

    auto
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- uptimes-20170802.426/uptimes-autoloads.el
+++ uptimes-20170802.426/uptimes-autoloads.el
@@ -3,6 +3,27 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
+;;;### (autoloads nil "uptimes" "uptimes.el" (22923 48235 859552
+;;;;;;  989000))
+;;; Generated autoloads from uptimes.el
+
+(autoload 'uptimes-save "uptimes" "\
+Write the uptimes to `uptimes-database'.
+
+\(fn)" t nil)
+
+(autoload 'uptimes "uptimes" "\
+Display the last and top `uptimes-keep-count' uptimes.
+
+\(fn)" t nil)
+
+(autoload 'uptimes-current "uptimes" "\
+Display the uptime for the current Emacs session.
+
+\(fn)" t nil)
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
