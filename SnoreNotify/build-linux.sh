2139cd54700cef98abe53d39cedd53c54e5b0253
Bao Haojun <baohaojun@smartisan.com>
2017-09-14 11:10:38 +0800

    Fix after-checkout build for no relative-link

--- SnoreNotify/build-linux.sh
+++ SnoreNotify/build-linux.sh
@@ -27,6 +27,6 @@ make -j8
 sudo make install
 (
     cd /usr/lib/x86_64-linux-gnu/qt5/mkspecs/modules
-    sudo relative-link /usr/local/mkspecs/modules/qt_Libsnore* .
+    sudo ln -s /usr/local/mkspecs/modules/qt_Libsnore* .
 )
 
