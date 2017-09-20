ebd89032b21e8b452e7a6ef36087dd56e7461fec
Bao Haojun <baohaojun@gmail.com>
2017-09-20 21:56:00 +0800

    init.el: use themes lisp/init-bhj.el: add clang to c/c++
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- lisp/init-bhj.el
+++ lisp/init-bhj.el
@@ -1,4 +1,8 @@
 (after-load 'company
   (add-hook 'sh-mode-hook
-            (lambda () (sanityinc/local-push-company-backend 'company-shell))))
+            (lambda () (sanityinc/local-push-company-backend 'company-shell)))
+  (add-hook 'c-mode-hook
+            (lambda () (sanityinc/local-push-company-backend 'company-clang)))
+  (add-hook 'c++-mode-hook
+            (lambda () (sanityinc/local-push-company-backend 'company-clang))))
 (provide 'init-bhj)
