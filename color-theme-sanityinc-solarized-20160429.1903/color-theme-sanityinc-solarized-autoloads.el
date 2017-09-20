9a8c1a68ad2ab8c0ef98097ee85a18ee8d4cd1cc
Bao Haojun <baohaojun@gmail.com>
2017-09-20 21:54:16 +0800

    Add color themes

---
+++ color-theme-sanityinc-solarized-20160429.1903/color-theme-sanityinc-solarized-autoloads.el
@@ -0,0 +1,35 @@
+;;; color-theme-sanityinc-solarized-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "color-theme-sanityinc-solarized" "color-theme-sanityinc-solarized.el"
+;;;;;;  (22978 28630 589928 51000))
+;;; Generated autoloads from color-theme-sanityinc-solarized.el
+
+(when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(autoload 'color-theme-sanityinc-solarized-dark "color-theme-sanityinc-solarized" "\
+Apply the dark solarized theme.
+
+\(fn)" t nil)
+
+(autoload 'color-theme-sanityinc-solarized-light "color-theme-sanityinc-solarized" "\
+Apply the light solarized theme.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("color-theme-sanityinc-solarized-pkg.el"
+;;;;;;  "sanityinc-solarized-dark-theme.el" "sanityinc-solarized-light-theme.el")
+;;;;;;  (22978 28630 549927 513000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; color-theme-sanityinc-solarized-autoloads.el ends here
---
+++ color-theme-sanityinc-tomorrow-20170919.1552/color-theme-sanityinc-tomorrow-pkg.el
@@ -0,0 +1,4 @@
+(define-package "color-theme-sanityinc-tomorrow" "20170919.1552" "A version of Chris Kempson's various Tomorrow themes" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
