cfd3d6f46079c64ef704aa81d0bb0032f8683313
Bao Haojun <baohaojun@gmail.com>
2017-10-16 13:00:45 +0800

    add fix-cjk-spaces
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- lisp/init-bhj.el
+++ lisp/init-bhj.el
@@ -5,4 +5,15 @@
             (lambda () (sanityinc/local-push-company-backend 'company-clang)))
   (add-hook 'c++-mode-hook
             (lambda () (sanityinc/local-push-company-backend 'company-clang))))
+
+(defun fix-cjk-spaces ()
+  "Fix cjk spaces."
+  (interactive)
+  (let ((my-point (point)))
+    (shell-command-on-region (point-min) (point-max) "fix-cjk-spaces" nil t)
+    (goto-char (min my-point (point-max)))))
+
+(add-hook 'before-save-hook #'fix-cjk-spaces)
+
+(define-key global-map (kbd "s-SPC") #'fix-cjk-spaces)
 (provide 'init-bhj)
