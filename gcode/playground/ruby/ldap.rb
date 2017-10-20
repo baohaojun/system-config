66e1be67d0c1a308a2d7174d78c716553d883d8b
Bao Haojun <baohaojun@gmail.com>
2017-10-20 18:39:40 +0800

    gcode/playground/ruby/ldap.rb: debug builder logic
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- gcode/playground/ruby/ldap.rb
+++ gcode/playground/ruby/ldap.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'net/ldap'
 
+require File::expand_path("~/src/github/smartcm/builder/app/controllers/smartcm_ldap_helper.rb")
+
+class Test
+  include SmartcmLdapHelper
+end
+if Test.new.ldap_login(ARGV[0], ARGV[1])
+  puts "builder: ok"
+else
+  puts "builder: fail"
+end
+
 ldap = Net::LDAP.new
 ldap.host = '172.16.21.3'
 ldap.port = 3268
@@ -15,7 +26,7 @@ user_email = user_email.sub(/\.cn$/, ".com")
 
 [user_email, user_email2].each { | user |
   result = ldap.bind_as(:base => "dc=smartisan, dc=cn",
-                        :filter => "(&(mail=#{user})(sAMAccountName=#{user.sub(/@.*/, "")}))",
+                        :filter => "(&(|(mail=#{user})(userPrincipalName=#{user}))(sAMAccountName=#{user.sub(/@.*/, "")}))",
                         :password => ARGV[1])
   if result
     puts "good: #{user}"
