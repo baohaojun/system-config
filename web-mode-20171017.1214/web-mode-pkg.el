ce870d34b6b5678ad823692218132a2f227be202
Bao Haojun <baohaojun@gmail.com>
2017-10-27 14:31:51 +0800

    install web-mode

---
+++ web-mode-20171017.1214/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web-mode" "20171017.1214" "major mode for editing web templates" '((emacs "23.1")) :commit "4754211f44e9fe1389e4378553ad538460282225" :url "http://web-mode.org" :keywords '("languages"))
