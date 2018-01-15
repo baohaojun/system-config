65f36e460830979117b62753d7c6e081037e1436
Bao Haojun <baohaojun@gmail.com>
2018-01-15 14:51:14 +0800

    unpack boot image.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ bin/unpack-bootimg.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+## start code-generator "^\\s *#\\s *"
+# generate-getopt b:boot-img d:unpack-dir
+## end code-generator
+## start generated code
+TEMP=$( getopt -o b:d:h \
+               --long boot-img:,unpack-dir:,help \
+               -n $(basename -- $0) -- "$@")
+boot_img=
+unpack_dir=
+eval set -- "$TEMP"
+while true; do
+    case "$1" in
+
+        -b|--boot-img)
+            boot_img=$2
+            shift 2
+            ;;
+        -d|--unpack-dir)
+            unpack_dir=$2
+            shift 2
+            ;;
+        -h|--help)
+            set +x
+            echo -e
+            echo
+            echo Options and arguments:
+            printf %06s '-b, '
+            printf %-24s '--boot-img=BOOT_IMG'
+            echo
+            printf %06s '-d, '
+            printf %-24s '--unpack-dir=UNPACK_DIR'
+            echo
+            exit
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            die "internal error"
+            ;;
+    esac
+done
+
+
+## end generated code
+
+mkdir -p $unpack_dir
+boot_img=$(readlink -f $boot_img)
+
+cd $unpack_dir
+unbootimg "$boot_img"
+
+mkdir -p ramdisk
+cd ramdisk
+cat ../ramdisk.img | gunzip |cpio -i
