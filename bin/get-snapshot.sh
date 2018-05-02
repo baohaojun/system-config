7a4a9b453e444b6f7581f59ff423f12718f4fa26
Bao Haojun <baohaojun@gmail.com>
2018-05-02 16:21:55 +0800

    bin/sparse-unsparse.xml: allow to pad or no pad (for fastboot) bin/wait-all: wait for all background processes
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/get-snapshot.sh
+++ bin/get-snapshot.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
+
+## 如果指定了 N 个文件、目录，选出里面最新的一个；
+## 如果只指定了一个参数，一般必须是一个目录，然后选出其下最新的一个文件或目录
+
+if test "$#" -gt 1; then
+    ls -t -d "$@" | head -n 1
+    exit
+fi
+
 cd ${1:-~/shots}
 
 ignore_pat='\.~.*#$'
