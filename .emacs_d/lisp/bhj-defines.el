3dcac1fd01050b5474b0cd1e4e65200c5e641d94
Bao Haojun <baohaojun@gmail.com>
2018-06-08 13:18:21 +0800

    bin/mm: Fix bug found by xiangning
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- .emacs_d/lisp/bhj-defines.el
+++ .emacs_d/lisp/bhj-defines.el
@@ -1442,16 +1442,12 @@ criteria can be provided via the optional match-string argument "
 
 (defun bhj-todo-from-mail-view-mail ()
   (interactive)
-  (let ((go-back-to-agenda nil))
+  (let ((message-id nil))
     (when (eq major-mode 'org-agenda-mode)
-      (setq go-back-to-agenda t)
       (org-agenda-goto))
     (ajoke--push-marker-ring)
-    (let ((mail (org-entry-get (point) "FROM")))
-      (setq mail (shell-quote-argument mail))
-      (shell-command-to-string (format "of %s >/dev/null 2>&1&" mail)))
-    (when go-back-to-agenda
-      (switch-window))))
+    (setq message-id (org-entry-get (point) "MESSAGE_ID"))
+    (mu4e-view-message-with-msgid message-id)))
 
 (defun bhj-todo-copy-id ()
   (interactive)
