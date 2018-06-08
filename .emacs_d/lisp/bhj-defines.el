a1575ebf67679081b7d15e9ed47f998a64561583
Bao Haojun <baohaojun@gmail.com>
2018-06-08 15:10:36 +0800

    bin/.replace-%%: fix it for %mol%
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- .emacs_d/lisp/bhj-defines.el
+++ .emacs_d/lisp/bhj-defines.el
@@ -1442,12 +1442,21 @@ criteria can be provided via the optional match-string argument "
 
 (defun bhj-todo-from-mail-view-mail ()
   (interactive)
-  (let ((message-id nil))
+  (let ((go-back-to-agenda nil)
+        (mail nil)
+        (message-id nil))
     (when (eq major-mode 'org-agenda-mode)
+      (setq go-back-to-agenda t)
       (org-agenda-goto))
     (ajoke--push-marker-ring)
-    (setq message-id (org-entry-get (point) "MESSAGE_ID"))
-    (mu4e-view-message-with-msgid message-id)))
+    (setq mail (org-entry-get (point) "FROM")
+          message-id (org-entry-get (point) "MESSAGE_ID"))
+    (if message-id
+        (mu4e-view-message-with-msgid message-id)
+      (setq mail (shell-quote-argument mail))
+      (shell-command-to-string (format "of %s >/dev/null 2>&1&" mail))
+      (when go-back-to-agenda
+        (switch-window)))))
 
 (defun bhj-todo-copy-id ()
   (interactive)
