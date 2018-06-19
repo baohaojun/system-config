8f22fe32872dfea10046b072832cd25810d18e3a
Bao Haojun <baohaojun@gmail.com>
2018-06-19 16:59:43 +0800

    Add screencap

--- droidVncServer/Android.mk
+++ droidVncServer/Android.mk
@@ -1,4 +1,28 @@
 LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    screencap.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libbinder \
+    libskia \
+    libui \
+    libgui
+
+LOCAL_MODULE := screencap-$(TARGET_ARCH).sdk$(PLATFORM_SDK_VERSION)
+LOCAL_MODULE_PATH := $(LOCAL_PATH)/
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
+
+include $(BUILD_EXECUTABLE)
+
+
 include $(CLEAR_VARS)
 
 LIBVNCSERVER_ROOT:=./LibVNCServer-0.9.9
