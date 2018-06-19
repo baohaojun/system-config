ff5f9a0ddbe0131c3c928c61e9b164ed3d25a697
Aaron Bieber <aaron@aaronbieber.com>
2018-06-19 12:03:46 -0400

    Apply GPLv3
    Fixes #6.

--- README.md
+++ README.md
@@ -43,3 +43,17 @@ Within the code editing window, you can press `C-c '` to copy the edited code
 back to its original location, or `C-c C-k` to discard your edits. In both
 situations, the new window is destroyed and you are returned to a convenient
 cursor location.
+
+## License ##
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
--- fence-edit.el
+++ fence-edit.el
@@ -2,6 +2,19 @@
 
 ;; Copyright (c) 2017 Aaron Bieber
 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;; Author: Aaron Bieber <aaron@aaronbieber.com>
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4"))
@@ -251,7 +264,7 @@ The edit buffer is expected to be the current buffer."
       (set-buffer-modified-p nil))
     (kill-buffer buffer)))
 
-(defun fence-edit-save () 
+(defun fence-edit-save ()
   "Save the original buffer with the new text."
   (interactive)
   (fence-edit--guard-edit-buffer)
