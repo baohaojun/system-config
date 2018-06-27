f0ea13504b6da067153b2fb84474519803dd5f41
Bao Haojun <baohaojun@gmail.com>
2018-06-27 20:49:29 +0800

    Try to use csv for patches.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- .emacs_d/lisp/init-mu4e.el
+++ .emacs_d/lisp/init-mu4e.el
@@ -6,5 +6,14 @@
   (let ((msg-file (mu4e-field-at-point :path)))
     (shell-command-to-string (format "of %s >/dev/null 2>&1&" (shell-quote-argument msg-file)))))
 
+(defun ajoke--mu4e-pop-mark ()
+  "Pop mark in mu4e mode."
+  (interactive)
+  (ajoke-pop-mark)
+  (delete-other-windows))
+
+(define-key mu4e-headers-mode-map [(control meta ?,)] 'ajoke--mu4e-pop-mark)
+(define-key mu4e-view-mode-map [(control meta ?,)] 'ajoke--mu4e-pop-mark)
+
 (define-key mu4e-headers-mode-map [(control c) (v)] 'bhj-mu4e-view-with-thunderbird)
 (define-key mu4e-view-mode-map [(control c) (v)] 'bhj-mu4e-view-with-thunderbird)
