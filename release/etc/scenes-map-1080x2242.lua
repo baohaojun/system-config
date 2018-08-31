786816bc009acbc83f251ab75b724028a6153a8e
Bao Haojun <baohaojun@gmail.com>
2018-08-31 18:49:48 +0800

    auto.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- release/etc/scenes-map-1080x2242.lua
+++ release/etc/scenes-map-1080x2242.lua
@@ -1,5 +1,5 @@
 local map = {}
-map['close-keyguard-dialog'] = '957 597'
+map['close-keyguard-dialog'] = '911 651'
 map['qq-close-search'] = '814 135'
 map['weibo-icon'] = '366 219'
 map['wx-close-search'] = '991 140'
