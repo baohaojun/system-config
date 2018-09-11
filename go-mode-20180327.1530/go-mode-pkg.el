27e2623ef2817b8e6bd22efb094b80f88bb5994f
Bao Haojun <baohaojun@gmail.com>
2018-09-11 09:53:26 +0800

    Switch to use tsinghua mirror for melpa

---
+++ go-mode-20180327.1530/go-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-mode" "20180327.1530" "Major mode for the Go programming language" 'nil :commit "7f87c32464d2eb22ac7a413cac741a89fbfdc740" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
---
+++ go-mode-readme.txt
@@ -0,0 +1,10 @@
+Copyright 2013 The go-mode Authors.  All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+
+Author: The go-mode Authors
+Version: 1.5.0
+Keywords: languages go
+URL: https://github.com/dominikh/go-mode.el
+
+This file is not part of GNU Emacs.
