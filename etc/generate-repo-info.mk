cae50c7d64f99b2656e328f4c92ee837a0b7fcca
Bao Haojun <baohaojun@gmail.com>
2018-12-28 14:42:27 +0800

    bin/my-rfa: make sure only 1 process to update repo info etc/generate-repo-info.mk: clock skew!
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- etc/generate-repo-info.mk
+++ etc/generate-repo-info.mk
@@ -1,3 +1,4 @@
-repo_info.sh : manifest.xml $(shell which print-repo-info-as-perl-all) $(MAKEFILE_LIST) $(shell if test -e local_manifests/local_manifist.xml; then echo local_manifests/local_manifist.xml; fi)
-	print-repo-info-as-perl-all > $@
-	chmod +x $@
+repo_info.sh : manifest.xml $(shell if test -e local_manifests/local_manifist.xml; then echo local_manifests/local_manifist.xml; fi)
+	print-repo-info-as-perl-all > $@.$$$$ && \
+	chmod +x $@.$$$$ && \
+	mv $@.$$$$ $@
