4f33f2ba0b066ff42c215dae2b6dda7762540563
Bao Haojun <baohaojun@gmail.com>
2019-03-28 12:49:54 +0800

    Fix some bugs 'cause we are using homebrew.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- etc/custom.el
+++ etc/custom.el
@@ -22,6 +22,7 @@
  '(case-fold-search t)
  '(company-idle-delay nil)
  '(compilation-message-face (quote default))
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(custom-safe-themes
    (quote
     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
@@ -277,6 +278,7 @@
  '(shell-file-name "/bin/bash")
  '(show-paren-mode t nil (paren))
  '(show-paren-style (quote parenthesis))
+ '(skk-jisyo-edit-user-accepts-editing t)
  '(skk-user-directory "~/system-config/.ddskk")
  '(starttls-use-gnutls t)
  '(text-mode-hook (quote (text-mode-hook-identify)))
