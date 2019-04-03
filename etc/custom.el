0be2585e2cc21490b6e20fd7a3503f4670c17e91
Bao Haojun <baohaojun@gmail.com>
2019-04-03 09:43:20 +0800

    bin/gerrit-set-same-group-as: add log
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- etc/custom.el
+++ etc/custom.el
@@ -22,7 +22,7 @@
  '(case-fold-search t)
  '(company-idle-delay nil)
  '(compilation-message-face (quote default))
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(custom-safe-themes
    (quote
     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
@@ -324,6 +324,7 @@
  '(weechat-color-list
    (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))
  '(whitespace-action (quote (auto-cleanup)))
+ '(whitespace-cleanup-mode-only-if-initially-clean nil)
  '(woman-manpath (quote ("/usr/man" "/usr/share/man" "/usr/local/man")))
  '(woman-use-own-frame nil)
  '(yas-also-auto-indent-first-line t)
