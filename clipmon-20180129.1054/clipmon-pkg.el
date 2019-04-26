a9c32b5caefca711c51b8b28c4ed3d6fe810cbc7
Bao Haojun <baohaojun@gmail.com>
2019-04-26 09:13:19 +0800

    Add clipmon

---
+++ clipmon-20180129.1054/clipmon-pkg.el
@@ -0,0 +1,10 @@
+(define-package "clipmon" "20180129.1054" "Clipboard monitor - watch system clipboard, add changes to kill ring/autoinsert" 'nil :keywords
+  '("convenience")
+  :authors
+  '(("Brian Burns" . "bburns.km@gmail.com"))
+  :maintainer
+  '("Brian Burns" . "bburns.km@gmail.com")
+  :url "https://github.com/bburns/clipmon")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
