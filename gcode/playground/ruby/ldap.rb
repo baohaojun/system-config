74cf039c5fb875865ffac95c2b5408de6505cc2b
Bao Haojun <baohaojun@gmail.com>
2019-06-11 22:33:50 +0800

    Can do ldap in localhost slapd.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- gcode/playground/ruby/ldap.rb
+++ gcode/playground/ruby/ldap.rb
@@ -1,21 +1,10 @@
 require 'rubygems'
 require 'net/ldap'
 
-require File::expand_path("~/src/github/smartcm/builder/app/controllers/smartcm_ldap_helper.rb")
-
-class Test
-  include SmartcmLdapHelper
-end
-if Test.new.ldap_login(ARGV[0], ARGV[1])
-  puts "builder: ok"
-else
-  puts "builder: fail"
-end
-
 ldap = Net::LDAP.new
 ldap.host = ENV['scm_ldap_host']
 ldap.port = ENV['scm_ldap_port']
-ldap.auth ENV['scm_ldap_user'], ENV['scm_ldap_password']
+# ldap.auth ENV['scm_ldap_user'], ENV['scm_ldap_password']
 
 user_email = String.new(ARGV[0])
 
@@ -25,8 +14,8 @@ user_email = user_email.sub(/\.cn$/, ".com")
 
 
 [user_email, user_email2].each { | user |
-  result = ldap.bind_as(:base => "DC=chj,DC=local",
-                        :filter => "(sAMAccountName=#{user.sub(/@.*/, "")})",
+  result = ldap.bind_as(:base => "DC=nodomain",
+                        :filter => "(cn=#{user.sub(/@.*/, "")})",
                         :password => ARGV[1])
   if result
     puts "good: #{user}"
