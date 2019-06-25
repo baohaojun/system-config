ab818b96fccfff27e93b90ec172d29e585391f17
Bao Haojun <baohaojun@gmail.com>
2019-06-25 18:15:49 +0800

    .emacs_d/lisp/bhj-set-key.el: allow to post with f12
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- .emacs_d/lisp/bhj-set-key.el
+++ .emacs_d/lisp/bhj-set-key.el
@@ -129,6 +129,6 @@
 (define-key global-map (kbd "<S-escape>") esc-map)
 (define-key global-map (kbd "<XF86Search>") 's-dicts)
 (define-key global-map (kbd "<C-M-mouse-1>") 'netease-music-jump-into)
-(define-key global-map (kbd "M-RET") 'bhj-wrench-post)
 (define-key global-map (kbd "ESC <menu>") 'bhj-wrench-post)
+(define-key global-map (kbd "<f12>") 'bhj-wrench-post)
 (define-key global-map (kbd "ESC <C-return>") 'bhj-wrench-post)
