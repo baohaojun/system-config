248eb76ef755e0705e0fa329338353ff84171e2d
Bao Haojun <baohaojun@gmail.com>
2019-07-15 08:45:17 +0800

    gcode/scim-cs/ime-py/wubi_extra.py: Fix the period
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- gcode/scim-cs/ime-py/wubi_extra.py
+++ gcode/scim-cs/ime-py/wubi_extra.py
@@ -17,9 +17,10 @@ g_quail_map = {
 }
 
 _char_map = {}
-for c in ':', '.', ',', '(', ')', '{', '}', '[', ']', '?':
+for c in ':', ',', '(', ')', '{', '}', '[', ']', '?':
     _char_map[c] = chr(ord(u'Ａ') - ord('A') + ord(c))
 
+_char_map['.'] = "。"
 _char_map['`'] = "‘"
 _char_map['\''] = "’"
 _char_map["''"] = "”"
@@ -32,17 +33,17 @@ _char_map['``['] = '『'
 _char_map['\'\']'] = '』'
 
 for c in _char_map:
-    g_quail_map[c] = _char_map[c]
-    g_quail_map[c + ' '] = _char_map[c]
+    g_quail_map[c] = (_char_map[c],)
+    g_quail_map[c + ' '] = (_char_map[c],)
     for ex_c in ',', '.', ':':
-        g_quail_map[c + ex_c] = _char_map[c] + _char_map[ex_c]
-        g_quail_map[c + ex_c + ' '] = _char_map[c] + _char_map[ex_c]
+        g_quail_map[c + ex_c] = (_char_map[c] + _char_map[ex_c],)
+        g_quail_map[c + ex_c + ' '] = (_char_map[c] + _char_map[ex_c],)
 
-        g_quail_map[ex_c + c] = _char_map[ex_c] + _char_map[c]
-        g_quail_map[ex_c + c + ' '] = _char_map[ex_c] + _char_map[c]
+        g_quail_map[ex_c + c] = (_char_map[ex_c] + _char_map[c],)
+        g_quail_map[ex_c + c + ' '] = (_char_map[ex_c] + _char_map[c],)
 
 for c in range(ord('A'), ord('Z') + 1):
-    g_quail_map[chr(c) + "  "] = chr(ord(u'Ａ') - ord('A') + c)
+    g_quail_map[chr(c) + "  "] = (chr(ord(u'Ａ') - ord('A') + c),)
 
 _extra_map = {}
 _extra_map['`\''] = ('‘’',)
@@ -50,7 +51,9 @@ _extra_map['``\''] = ('“”',)
 _extra_map['`]'] = ('「」',)
 _extra_map['[\''] = ('「」',)
 _extra_map['``]'] = ('『』',)
+_extra_map['`]]'] = ('『』',)
 _extra_map['[[\''] = ('『』',)
+_extra_map['[\'\''] = ('『』',)
 
 for c in _extra_map:
     g_quail_map[c] = _extra_map[c]
