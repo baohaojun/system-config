26bbed18714a1708fd3ee5c201721f2b92962656
Bao Haojun <baohaojun@gmail.com>
2019-08-01 23:56:21 +0800

    .bashrc-remote: allow to set it bin/illPass: fix pass input bin/logout-by-killing: restart the right dm bin/toggle-dvp-us.sh: remove history lib/jc/build: allow to build synchronously
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/toggle-dvp-us.sh
+++ bin/toggle-dvp-us.sh
@@ -1,91 +1,11 @@
 #!/bin/bash
 
 set -x
-exec > ~/tmp/am.log 2>&1
+exec >> ~/tmp/am.log 2>&1
+(
+    date; echo $0
+) >> ~/tmp/am.history
 set -e
-## start code-generator "^\\s *#\\s *"
-# generate-getopt -P @udev
-## end code-generator
-## start generated code
-TEMP=$(POSIXLY_CORRECT=true getopt -o h \
-                      --long udev,help,no-udev \
-                      -n $(basename -- $0) -- "$@")
-declare udev=false
-eval set -- "$TEMP"
-while true; do
-    case "$1" in
-
-        --udev|--no-udev)
-            if test "$1" = --no-udev; then
-                udev=false
-            else
-                udev=true
-            fi
-            shift
-
-            ;;
-        -h|--help)
-            set +x
-            echo -e
-            echo
-            echo Options and arguments:
-            printf "%06s" " "
-            printf %-24s '--[no-]udev'
-            echo
-            exit
-            shift
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            die "internal error: $(. bt; echo; bt | indent-stdin)"
-            ;;
-    esac
-done
-
-## end generated code
-
-if test "$udev" = true; then
-    at now <<<'myscr --bg bash -c "$(
-cat <<'\''EOF2a3e34760a92'\''
-# {%sh-mode%}
-set -x
-exec > ~/tmp/am-udev.log 2>&1
-for x in $(seq 1 25); do
-    if setxkbmap -print|grep -q us:2; then
-        (
-            for x in $(seq 1 10); do
-                am dvp;
-                if (
-                    exit_val=0
-                    for x in $(seq 1 5); do
-                        sleep .2
-                        if setxkbmap -print | grep -q us:2 ||
-                                test "$(cat ~/.local-config/etc/xmodmap.$(get-mac))" != "$(xmodmap)"; then
-                            exit_val=1
-                            break
-                        fi
-                    done
-                    exit ${exit_val}
-                ); then
-                    exit 0
-                fi
-                sleep .3
-            done
-        )
-       exit
-    fi
-    sleep .2
-done
-# {%/sh-mode%}
-
-EOF2a3e34760a92
-
-)"'
-    exit
-fi
 
 if [[ "$(tty)" =~ /dev/tty ]]; then
     if test "$(cat ~/.config/system-config/am-keyboard)" = en; then
