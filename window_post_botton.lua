22d9693c0e249aad4ba4d9a42d07e29ca0573048
Bao Haojun <baohaojun@gmail.com>
2020-01-17 11:40:38 +0800

    auto.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- window_post_botton.lua
+++ window_post_botton.lua
@@ -6,6 +6,7 @@ map['com.alibaba.android.rimet/com.alibaba.lightapp.runtime.activity.CommonWebVi
 map['com.amazon.kindle/com.amazon.kcp.oob.MainActivity'] = 'No-Post'
 map['com.amazon.kindle/com.amazon.kcp.reader.StandAloneBookReaderActivity'] = 'No-Post'
 map['com.android.contacts/com.android.contacts.activities.DialtactsActivity'] = 'No-Post'
+map['com.instagram.android/com.instagram.nux.activity.SignedOutFragmentActivity'] = 'No-Post'
 map['com.netease.onmyoji/com.netease.onmyoji.Client'] = 'Enter-Key'
 map['com.sina.weibo/com.sina.weibo.weiyou.DMSingleChatActivity'] = 'Find-Button'
 map['com.tencent.mm/com.tencent.mm.plugin.fts.ui.FTSMainUI'] = 'Enter-Key'
