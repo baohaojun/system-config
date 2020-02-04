3edd0456606865b8ec3fdfe85a692362becc6098
Bao Haojun <baohaojun@gmail.com>
2020-02-04 23:33:13 +0800

    bin/se: allow to specify the jira env lib/jkd.user/go-admin-page: allow to edit an issue's workflow lib/jkd/dump-screen: allow the tabs to be sorted lib/jkd/rest: allow to hint more error msg when failed
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ etc/windows/crontab/ut.bat
@@ -0,0 +1,3 @@
+set path=c:\cygwin64\bin;%path%
+echo %path%
+C:\cygwin64\bin\bash.exe -c "set -x; (set -x ;net stop w32time; sleep 1; net start w32time; sleep 1; w32tm /config /update ; w32tm /resync /rediscover) 2>&1 |iconv -f gbk -t utf-8 |tee ~/time.txt"
