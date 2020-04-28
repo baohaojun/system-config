dcfdf5875cab3c539a962c50be288638003f00c0
Bao Haojun <baohaojun@gmail.com>
2020-04-28 21:24:03 +0800

    auto.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- window_post_botton.lua
+++ window_post_botton.lua
@@ -1,6 +1,7 @@
 local map = {}
 map['cn.gov.tax.its/cn.gov.tax.its.MainActivity'] = 'No-Post'
 map['com.aibang.bjtraffic/com.uicps.activity.UICPSWebActivity'] = 'No-Post'
+map['com.alibaba.android.rimet/com.alibaba.alimei.mail.activity.MailDetailActivity'] = 'No-Post'
 map['com.alibaba.android.rimet/com.alibaba.android.dingtalkim.activities.AddRobotActivity'] = 'No-Post'
 map['com.alibaba.android.rimet/com.alibaba.android.dingtalkim.activities.ChatMsgActivity'] = 'Find-Button'
 map['com.alibaba.android.rimet/com.alibaba.android.dingtalkim.activities.ConfirmCreatingConversationActivity'] = 'No-Post'
