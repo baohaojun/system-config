563c7b62ecb23c154d0a4a12bf3d3be784310f2a
Bao Haojun <baohaojun@gmail.com>
2020-05-01 13:57:22 +0800

    build for freebsd

--- glue/thread-glue.c
+++ glue/thread-glue.c
@@ -27,23 +27,22 @@
 
 #include <sys/types.h>
 #include <unistd.h>
-#include <linux/unistd.h>
 #include <errno.h>
 
 #ifdef __NR_gettid
 static pid_t gettid (void)
 {
-	return syscall(__NR_gettid);
+        return syscall(__NR_gettid);
 }
 #else
 static pid_t gettid (void)
 {
-	return 0;
+        return 0;
 }
 #endif
 
 pid_t
 wrap_gettid (void)
 {
-	return gettid ();
+        return gettid ();
 }
