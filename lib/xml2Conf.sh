f09d9a1dd131c4faa26dfe882c593325891d0ce3
Bao Haojun <baohaojun@gmail.com>
2020-05-02 06:48:53 +0800

    Fix it for freebsd

--- lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/bhj/external/local/lib"
-XML2_LIBS="-lxml2"
-XML2_INCLUDEDIR="-I/home/bhj/external/local/include/libxml2"
-MODULE_VERSION="xml2-2.7.8"
-
--- lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/bhj/external/local/lib"
-XSLT_LIBS="-lxslt  -L/home/bhj/external/local/lib -lxml2"
-XSLT_INCLUDEDIR="-I/home/bhj/external/local/include"
-MODULE_VERSION="xslt-1.1.26"
