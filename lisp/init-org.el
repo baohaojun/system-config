953cfeb27276edf67d0a303531f3eabba8a11d84
Bao Haojun <baohaojun@gmail.com>
2020-05-27 09:29:26 +0800

    change gtd keyword
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -183,7 +183,7 @@ typical word processor."
 ;;; To-do settings
 
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+      (quote ((sequence "TODO(t)" "DOING(n)" "|" "DONE(d!/!)")
               (sequence "PROJECT(p)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
               (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")))
       org-todo-repeat-to-state "NEXT")
