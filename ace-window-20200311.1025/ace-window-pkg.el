69bdf0331a0fc965dc01a12d6140a6bbe2c3ce91
Bao Haojun <baohaojun@gmail.com>
2020-06-01 13:24:54 +0800

    add lsp-java

---
+++ ace-window-20200311.1025/ace-window-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/bhj/.emacs.d/elpa-27.0/ace-window-20200311.1025/ace-window.el  -*- no-byte-compile: t -*-
+(define-package "ace-window" "20200311.1025" "Quickly switch windows." '((avy "0.5.0")) :commit "7003c88cd9cad58dc35c7cd13ebc61c355fb5be7" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")
---
+++ pfuture-20200425.1357/pfuture-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/bhj/.emacs.d/elpa-27.0/pfuture-20200425.1357/pfuture.el  -*- no-byte-compile: t -*-
+(define-package "pfuture" "20200425.1357" "a simple wrapper around asynchronous processes" '((emacs "25.2")) :commit "d7926de3ba0105a36cfd00811fd6278aea903eef" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/pfuture")
