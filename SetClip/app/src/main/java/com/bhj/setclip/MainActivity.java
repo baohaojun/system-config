0b9e5936cc8d7ec3d175482336fc8d7520db6da0
Bao Haojun <baohaojun@gmail.com>
2020-06-02 10:54:42 +0800

    auto.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- SetClip/app/src/main/java/com/bhj/setclip/MainActivity.java
+++ SetClip/app/src/main/java/com/bhj/setclip/MainActivity.java
@@ -118,6 +118,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
