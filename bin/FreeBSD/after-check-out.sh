da9869b9fd9c0d2e9c489146ce3f0ff540d4d169
Bao Haojun <baohaojun@gmail.com>
2020-06-25 23:21:26 +0800

    bin/.gitx: allow to check bin/FreeBSD/config/setup/05-build-ss.obfs: fix build on freebsd
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- bin/FreeBSD/after-check-out.sh
+++ bin/FreeBSD/after-check-out.sh
@@ -1,60 +1,19 @@
 #!/usr/bin/env bash
 
-~/system-config/bin/after-co-ln-s.sh
-. ~/system-config/.bashrc
-touch ~/.cache/system-config/.where ~/.cache/system-config/.where.lock
+if yes-or-no-p "update fstab"; then
+cat <<EOF | sudo tee -a /etc/fstab
 
-umask 0022
-
-if test $(whoami) = bhj; then
-    sudo perl -npe 's/^XKBVARIANT=.*/XKBVARIANT="dvp"/;' -i /etc/default/keyboard
-    cat <<EOF | sudo tee /etc/locale.gen
-zh_CN.GBK GBK
-zh_CN.UTF-8 UTF-8
-en_US.UTF-8 UTF-8
-ja_JP.UTF-8 UTF-8
+proc                    /proc           procfs  rw              0       0
+# End auto-admin addition
+linprocfs   /compat/linux/proc  linprocfs       rw      0       0
+linsysfs    /compat/linux/sys   linsysfs        rw      0       0
+tmpfs    /compat/linux/dev/shm  tmpfs   rw,mode=1777    0       0
 EOF
-    sudo locale-gen
-    echo LANG=en_US.UTF-8 |sudo tee /etc/default/locale
-fi
-sudo touch /etc/console-setup/* || true
-sudo touch /etc/default/* || true # setupcon may fail when the timestamp of
-                                  # these files are messed up by debian
-                                  # installation (time zone or ntp not available
-                                  # because we are not connected to the
-                                  # Internet).
-sudo setupcon
-sudo usermod -a -G fuse $USER
 
-sudo perl -npe 's/ main$/ main contrib non-free/' -i /etc/apt/sources.list
-
-. ~/system-config/bin/Linux/download-external.sh
-download_external >/dev/null 2>&1 &
-
-set -e
-export PATH=~/system-config/bin/Linux/config:~/system/bin/Linux:~/system/bin:$PATH
-
-#update the system
-upd_system
-
-sudo usermod -a -G dialout $(whoami) || true
-sudo perl -npe 's/^#user_allow_other/user_allow_other/' -i /etc/fuse.conf
-mkdir -p ~/src/github
-emacs-install-packages || true
-if test ! -d ~/src/github/presentations/; then
-    git clone https://github.com/baohaojun/presentations ~/src/github/presentations
 fi
 
-if test ! -d /usr/local/share/info; then
-    sudo mkdir -p /usr/local/share/info
+if yes-or-no-p "enable linux"; then
+    sudo kldload linux
+    sudo kldload linux64
+    sudo sysrc 'linux_enable="YES"'
 fi
-
-(mozroots --import --sync&)
-#编译一些软件
-do_compile
-
-if test -e ~/.local-config/bin/bhj-after-check-out.sh; then
-    ~/.local-config/bin/bhj-after-check-out.sh
-fi
-
-echo 'Full config OK'
