7d7f7a359f318d414886bdf908833804a28d74e4
Bao Haojun <baohaojun@gmail.com>
2020-10-15 18:27:54 +0800

    lisp/init-bhj.el: allow to work in my daily.org
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- lisp/init-bhj.el
+++ lisp/init-bhj.el
@@ -11,6 +11,31 @@
   (add-hook 'c++-mode-hook
             (lambda () (sanityinc/local-push-company-backend 'company-clang))))
 
+(defun bhj-point-visible-p ()
+  "Return t if current char is visible."
+  (eq nil (delete-if ; delete the nils
+           #'listp
+           (mapcar (lambda (overlay)
+                     (overlay-get overlay 'invisible))
+                   (overlays-at (point))))))
+
+(defun bhj-next-visible-char ()
+  "Return the next visible char starting from (point).
+Returns (point) if current-char is visible."
+  (save-excursion
+    (while (and (not (bhj-point-visible-p))
+                (not (= (point) (point-max))))
+      (goto-char (next-overlay-change (point))))
+    (point)))
+
+(defun bhj-next-invisible-char ()
+  "Return the next invisible char starting from (point).
+Returns (point) if current-char is visible."
+  (save-excursion
+    (while (and (bhj-point-visible-p)
+                (not (= (point) (point-max))))
+      (goto-char (next-overlay-change (point))))
+    (point)))
 (defun fix-cjk-spaces ()
   "Fix cjk spaces."
   (interactive)
@@ -20,7 +45,7 @@
           (is-eol (looking-at "$"))
           (my-start (point-min))
           (my-end (point-max)))
-      (when (region-active-p)
+      (when (region-active-p) ; for changing 1 space
         (setq my-start (min (mark) (point))
               my-end (max (mark) (point)))
         (let ((region-text (buffer-substring-no-properties (mark) (point))))
@@ -33,9 +58,25 @@
             (delete-region (mark) (point))
             (insert " ")
             (throw 'single-char-done nil)))))
+
       (save-mark-and-excursion
         (save-restriction
-          (shell-command-on-region my-start my-end "fix-cjk-spaces" nil t)))
+          (if (buffer-narrowed-p)
+              (save-restriction
+                (narrow-to-region my-start my-end)
+                (setq my-start (point-min)
+                      my-end (point-max))
+                (while (< my-start my-end)
+                  (goto-char my-start)
+                  (goto-char (bhj-next-visible-char))
+                  (setq my-end (bhj-next-invisible-char))
+                  (shell-command-on-region (point) my-end "fix-cjk-spaces" nil t)
+                  (goto-char my-end)
+                  (goto-char (bhj-next-visible-char))
+                  (setq my-start (point)
+                        my-end (point-max))))
+            (shell-command-on-region my-start my-end "fix-cjk-spaces" nil t))
+          ))
       (if is-eol
           (progn
             (goto-char (point-min))
