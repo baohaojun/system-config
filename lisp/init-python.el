aa0dfb8161527f3ff49324c5d6c11fc2c2941f22
Bao Haojun <baohaojun@gmail.com>
2020-12-27 21:53:11 +0800

    auto.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -9,17 +9,9 @@
 
 (require-package 'pip-requirements)
 
-(when (maybe-require-package 'anaconda-mode)
+(when (maybe-require-package 'elpy)
   (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (after-load 'anaconda-mode
-    (define-key anaconda-mode-map (kbd "M-?") nil))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (after-load 'python
-        (push 'company-anaconda company-backends)))))
-
+    (add-hook 'python-mode-hook 'elpy-mode)))
 
 (provide 'init-python)
 ;;; init-python.el ends here
