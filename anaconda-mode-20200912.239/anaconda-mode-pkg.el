1e4a94f8984868a38fc09972b36eabfa19a532aa
Bao Haojun <baohaojun@gmail.com>
2020-12-27 21:51:00 +0800

    upgrade anaconda, but we are using elpy now

---
+++ anaconda-mode-20200912.239/anaconda-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/bhj/.emacs.d/elpa-27.0/anaconda-mode-20200912.239/anaconda-mode.el  -*- no-byte-compile: t -*-
+(define-package "anaconda-mode" "20200912.239" "Code navigation, documentation lookup and completion for Python" '((emacs "25.1") (pythonic "0.1.0") (dash "2.6.0") (s "1.9") (f "0.16.2")) :commit "39b1cf88c8c459901630d248d6135d8644075648" :authors '(("Artem Malyshev" . "proofit404@gmail.com")) :maintainer '("Artem Malyshev" . "proofit404@gmail.com") :url "https://github.com/proofit404/anaconda-mode")
