3c5ede7282858490f3838a24dbaf37a94ba48a1a
Bao Haojun <baohaojun@gmail.com>
2020-12-30 21:45:42 +0800

    Revert "auto."
    This reverts commit aa0dfb8161527f3ff49324c5d6c11fc2c2941f22.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -9,9 +9,17 @@
 
 (require-package 'pip-requirements)
 
-(when (maybe-require-package 'elpy)
+(when (maybe-require-package 'anaconda-mode)
   (after-load 'python
-    (add-hook 'python-mode-hook 'elpy-mode)))
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+  (after-load 'anaconda-mode
+    (define-key anaconda-mode-map (kbd "M-?") nil))
+  (when (maybe-require-package 'company-anaconda)
+    (after-load 'company
+      (after-load 'python
+        (push 'company-anaconda company-backends)))))
+
 
 (provide 'init-python)
 ;;; init-python.el ends here
