967652400ef8fecde2a003cb4d9e7b6e3329f3a3
Bao Haojun <baohaojun@gmail.com>
2021-03-04 22:26:38 +0800

    auto

---
+++ parsec-20180730.16/parsec-autoloads.el
@@ -0,0 +1,22 @@
+;;; parsec-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "parsec" "parsec.el" (0 0 0 0))
+;;; Generated autoloads from parsec.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parsec" '("parsec-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; parsec-autoloads.el ends here
---
+++ parsec-20180730.16/parsec-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/bhj/.emacs.d/elpa-27.1/parsec-20180730.16/parsec.el  -*- no-byte-compile: t -*-
+(define-package "parsec" "20180730.16" "Parser combinator library" '((emacs "24") (cl-lib "0.5")) :commit "2cbbbc2254aa7bcaa4fb5e07c8c1bf2f381dba26" :authors '(("Junpeng Qiu" . "qjpchmail@gmail.com")) :maintainer '("Junpeng Qiu" . "qjpchmail@gmail.com") :keywords '("extensions") :url "https://github.com/cute-jumper/parsec.el")
