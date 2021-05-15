cbfe4f82a438e5f24b1817add434f328d1b79990
Bao Haojun <baohaojun@gmail.com>
2021-05-15 15:59:50 +0800

    auto
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- init.el
+++ init.el
@@ -22,15 +22,6 @@
 (defconst *is-a-mac* (eq system-type 'darwin))
 
 ;;----------------------------------------------------------------------------
-;; Adjust garbage collection thresholds during startup, and thereafter
-;;----------------------------------------------------------------------------
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'emacs-startup-hook
-            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
-
-;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
 (require 'init-utils)
