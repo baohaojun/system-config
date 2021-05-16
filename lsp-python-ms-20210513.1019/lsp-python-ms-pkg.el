4151b0ea049e94cf8ca9e4e1438ff856775bfe3a
Bao Haojun <baohaojun@gmail.com>
2021-05-16 21:28:12 +0800

    remove ms python lsp

--- lsp-python-ms-20210513.1019/lsp-python-ms-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from lsp-python-ms.el  -*- no-byte-compile: t -*-
-(define-package "lsp-python-ms" "20210513.1019" "The lsp-mode client for Microsoft python-language-server" '((emacs "25.1") (lsp-mode "6.1")) :commit "5856c08d6393c10951f39b993a8d7bf2d506b44f" :authors '(("Charl Botha")) :maintainer '("Andrew Christianson, Vincent Zhang") :keywords '("languages" "tools") :url "https://github.com/emacs-lsp/lsp-python-ms")
