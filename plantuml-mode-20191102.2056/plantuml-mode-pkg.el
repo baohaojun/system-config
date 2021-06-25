5b2fa12ce3841a75b1449e4284e8328bbab7b735
Bao Haojun <baohaojun@gmail.com>
2021-06-25 09:04:36 +0800

    auto
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

---
+++ plantuml-mode-20191102.2056/plantuml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from plantuml-mode.el  -*- no-byte-compile: t -*-
+(define-package "plantuml-mode" "20191102.2056" "Major mode for PlantUML" '((dash "2.0.0") (emacs "25.0")) :commit "ea45a13707abd2a70df183f1aec6447197fc9ccc" :authors '(("Zhang Weize (zwz)")) :maintainer '("Carlo Sciolla (skuro)") :keywords '("uml" "plantuml" "ascii"))
